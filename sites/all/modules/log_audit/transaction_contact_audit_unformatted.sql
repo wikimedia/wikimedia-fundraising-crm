89b7984d46a8172bba170bea0192fc0fad6f7522
Ryan Faulkner <rfaulk@users.mediawiki.org>
2011-05-15 15:06:51 +0000

    missing transaction auditing via the paypal ipn logs. the query searches for transactions for which there is missing country data in civi and the resulting records are to be comapared against the logs. the results of the audit are returned in the form of decoded JSON.

---
+++ sites/all/modules/log_audit/transaction_contact_audit_unformatted.sql
@@ -0,0 +1,25 @@
+
+
+select 
+
+substring(contribution_address.trxn_id,18,17) as id,
+contribution_address.total_amount as amount
+
+
+from
+
+(select
+civicrm.civicrm_contribution.total_amount, 
+civicrm.civicrm_contribution.trxn_id, 
+civicrm.civicrm_contribution.contact_id as contribution_contact_id, 
+civicrm.civicrm_address.contact_id as address_contact_id,
+civicrm.civicrm_address.country_id as country_id 
+
+from civicrm.civicrm_contribution left join civicrm.civicrm_address on civicrm.civicrm_contribution.contact_id = civicrm.civicrm_address.contact_id
+
+where DATE_FORMAT(receive_date, '%sY%sm%sd%sH%si%ss') >= '%s' and DATE_FORMAT(receive_date, '%sY%sm%sd%sH%si%ss') < '%s'
+and civicrm.civicrm_contribution.trxn_id REGEXP 'RECURRING PAYPAL'
+) as contribution_address
+
+
+where contribution_address.country_id is NULL;
