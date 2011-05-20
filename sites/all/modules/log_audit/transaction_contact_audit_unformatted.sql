c0e83e5f4c31baca96caa9f200ae34c0f7d52b95
Arthur Richards <awjrichards@users.mediawiki.org>
2011-05-20 19:52:43 +0000

    Fixed misspelling of var name ; working on fixing Faulkner's query

--- sites/all/modules/log_audit/transaction_contact_audit_unformatted.sql
+++ sites/all/modules/log_audit/transaction_contact_audit_unformatted.sql
@@ -1,5 +1,3 @@
-
-
 select 
 
 substring(contribution_address.trxn_id,18,17) as trxn_id,
@@ -7,7 +5,6 @@ substring(contribution_address.trxn_id,18,17) as trxn_id,
 from
 
 (select
-civicrm.civicrm_contribution.total_amount, 
 civicrm.civicrm_contribution.trxn_id, 
 civicrm.civicrm_contribution.contact_id as contribution_contact_id, 
 civicrm.civicrm_address.contact_id as address_contact_id,
@@ -15,7 +12,7 @@ civicrm.civicrm_address.country_id as country_id
 
 from civicrm.civicrm_contribution left join civicrm.civicrm_address on civicrm.civicrm_contribution.contact_id = civicrm.civicrm_address.contact_id
 
-where DATE_FORMAT(receive_date, '%sY%sm%sd%sH%si%ss') >= '%s' and DATE_FORMAT(receive_date, '%sY%sm%sd%sH%si%ss') < '%s'
+where DATE_FORMAT(receive_date, '%%Y%%m%%d%%H%%i%%s') >= %d and DATE_FORMAT(receive_date, '%%Y%%m%%d%%H%%i%%s') <= %d
 and civicrm.civicrm_contribution.trxn_id REGEXP 'RECURRING PAYPAL'
 ) as contribution_address
 
