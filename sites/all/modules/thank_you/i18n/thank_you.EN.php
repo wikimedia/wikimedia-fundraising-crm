75f03f692e101aea253c6bf8a379942eca2b2331
Peter Gehres <pgehres@users.mediawiki.org>
2011-10-25 05:50:04 +0000

    Adding translated thank-you email functionality as well as en defaults

---
+++ sites/all/modules/thank_you/i18n/thank_you.EN.php
@@ -0,0 +1,42 @@
+<?php
+
+$messages['en'] = array(
+	"thank_you_from_name" => "Sue Gardner",
+	"thank_you_to_name" => "{contact.display_name}",
+	"thank_you_to_name_secondary" => "Wikimedian",
+	"thank_you_subject" => "Thank you from the Wikimedia Foundation"
+);
+$messages['en']['thank_you_body'] =<<<'EOD'
+Dear {contact.first_name},
+
+Thank you for your gift of {contribution.source} to the Wikimedia Foundation, received on {contribution.date}. I’m very grateful for your support.
+
+Your donation celebrates everything Wikipedia and its sister sites stand for: the power of information to help people live better lives, and the importance of sharing, freedom, learning and discovery. Thank you so much for helping to keep these projects freely available for their more than 400 million monthly readers around the world.
+
+Your money supports technology and people. The Wikimedia Foundation develops and improves the technology behind Wikipedia and nine other projects, and sustains the infrastructure that keeps them up and running. The Foundation has a staff of about fifty, which provides technical, administrative, legal and outreach support for the global community of volunteers who write and edit Wikipedia.
+
+Many people love Wikipedia, but a surprising number don't know it's run by a non-profit. Please help us spread the word by telling a few of your friends.
+
+And again, thank you for supporting free knowledge.
+
+Sincerely Yours,
+
+
+Sue Gardner
+Executive Director
+
+* To donate: http://donate.wikimedia.org
+* To visit our Blog: http://blog.wikimedia.org
+* To follow us on Twitter: http://twitter.com/wikimedia
+* To follow us on Facebook: http://www.facebook.com/wikipedia
+
+
+This letter can serve as a record for tax purposes. No goods or
+services were provided, in whole or in part, for this contribution.
+The Wikimedia Foundation, Inc. is a non-profit charitable corporation
+with 501(c)(3) tax exempt status in the United States.
+Our address is 149 New Montgomery, 3rd Floor, San Francisco, CA, 94105.
+Tax-exempt number: 20-0049703
+EOD;
+
+
---
+++ sites/all/modules/thank_you/i18n/thank_you.i18n.php
@@ -0,0 +1,21 @@
+<?php
+
+# intialize the messages variable, if needed
+if ( !isset( $messages ) ){
+	$messages = array();
+}
+
+# whitelist of enabled language translations for emails
+$languages_enabled = array(
+	'en' => 'thank_you.EN.php',
+);
+
+/*
+ * Work through each of the enabled languages and include if
+ * the i18n file exists
+ */
+foreach ( $languages_enabled as $lang => $file ) {
+	if ( file_exists( dirname(__FILE__) . '/' . $file ) ) {
+		require_once dirname(__FILE__) . '/' . $file;
+	}
+}
