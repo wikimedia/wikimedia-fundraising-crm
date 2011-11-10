da7c7dc6a7724aea14e0f9249b909fbf82d09ae7
Peter Gehres <pgehres@users.mediawiki.org>
2011-11-10 02:37:51 +0000

    Adding el, es_ES, fr, gl, id, it languages for the thank you email

---
+++ sites/all/modules/thank_you/i18n/thank_you.id.php
@@ -0,0 +1,67 @@
+<?php
+
+$TYmsgs['id'] = array(
+	"thank_you_from_name" => "Sue Gardner",
+	"thank_you_to_name" => "{contact.display_name}",
+	"thank_you_to_name_secondary" => "teman dari Wikimedia Foundation",
+	"thank_you_subject" => "Terima kasih dari Wikimedia Foundation",
+	"thank_you_unsubscribe_title" => "Berhenti berlangganan Wikimedia Foundation",
+	"thank_you_unsubscribe_button" => "Berhenti berlangganan",
+//	"thank_you_unsubscribe_confirm" => "",
+//	"thank_you_unsubscribe_warning" => "",
+	"thank_you_unsubscribe_success" => "Anda telah berhasil menghapus diri Anda dari milis kami.",
+	"thank_you_unsubscribe_delay" => "Mohon berikan sampai dengan empat (4) hari supaya perubahan terjadi. Kami mohon maaf untuk surel apapun yang Anda dapatkan pada saat itu. Jika Anda memiliki pertanyaan, mohon hubungi <a href='mailto:donations@wikimedia.org'>donations@wikimedia.org</a>.",
+	"thank_you_unsubscribe_fail" => "Ada kesalahan dalam pemrosesan permohonan Anda, mohon hubungi <a href='mailto:donations@wikimedia.org'>donations@wikimedia.org</a>.",
+);
+$TYmsgs['id']['thank_you_body_plaintext'] =<<<'EOD'
+Hai {contact.first_name},
+
+Anda begitu hebat, terima kasih banyak karena telah menyumbang untuk Yayasan Wikimedia!
+
+Beginilah cara kami membayar tagihan kami -- mereka seperti Anda, menyumbangkan lima dolar, dua puluh dolar, seratus dolar. Sumbangan favoritku tahun lalu adalah lima pound dari seorang gadis kecil di Inggris, yang telah membujuk orang tuanya untuk mengizinkannya menyumbangkan uang jajannya. Orang-orang seperti Anda, dan juga gadis tersebut, yang memungkinkan Wikipedia untuk terus memberikan akses bebas dan mudah terhadap informasi netral untuk semua orang di seluruh dunia. Untuk semua orang yang membantu menyumbang, dan semua orang yang belum mampu membantu. Terima kasih banyak.
+
+Mudah saja untuk mengabaikan permohonan kami, dan saya senang Anda tidak melakukannya. Dari saya, dan puluhan ribu relawan yang menulis untuk Wikipedia: Terima Kasih karena telah membantu kami menjadikan dunia ini lebih baik. Kami akan menggunakan sumbangan Anda secara hati-hati, dan saya berterima kasih atas kepercayaan Anda terhadap kami.
+
+Terima kasih,
+Sue Gardner
+Direktur Eksekutif Yayasan Wikimedia
+
+---
+
+Untuk catatan: Sumbangan Anda pada tanggal {contribution.date} sebesar {contribution.source}.
+
+Surat ini digunakan sebagai catatan sumbangan Anda. Tidak ada barang atau jasa yang disediakan, secara keseluruhan atau sebagian, untuk kontribusi ini. Wikimedia Foundation, Inc. adalah sebuah perusahaan amal nirlaba dengan status bebas pajak 501(c)(3) di Amerika Serikat. Kami beralamat di 149 New Montgomery, 3rd Floor, San Francisco, CA, 94105. Nomor bebas pajak A.S.: 20-0049703
+
+---
+
+Opsi bebas kontak:
+Kami ingin Anda, selaku penyumbang, terus diberitahu mengenai aktivitas komunitas dan penggalangan dana kami. Jika Anda tidak ingin menerima surel seperti itu dari kami, klik tautan di bawah dan kami akan menghapus Anda dari daftar kontak:
+
+{unsubscribe_link}
+EOD;
+
+$TYmsgs['id']['thank_you_body_html'] =<<<'EOD'
+<p>Hai {contact.first_name},</p>
+
+<p>Anda begitu hebat, terima kasih banyak karena telah menyumbang untuk Yayasan Wikimedia!</p>
+
+<p>Beginilah cara kami membayar tagihan kami -- mereka seperti Anda, menyumbangkan lima dolar, dua puluh dolar, seratus dolar. Sumbangan favoritku tahun lalu adalah lima pound dari seorang gadis kecil di Inggris, yang telah membujuk orang tuanya untuk mengizinkannya menyumbangkan uang jajannya. Orang-orang seperti Anda, dan juga gadis tersebut, yang memungkinkan Wikipedia untuk terus memberikan akses bebas dan mudah terhadap informasi netral untuk semua orang di seluruh dunia. Untuk semua orang yang membantu menyumbang, dan semua orang yang belum mampu membantu. Terima kasih banyak.</p>
+
+<p>Mudah saja untuk mengabaikan permohonan kami, dan saya senang Anda tidak melakukannya. Dari saya, dan puluhan ribu relawan yang menulis untuk Wikipedia: Terima Kasih karena telah membantu kami menjadikan dunia ini lebih baik. Kami akan menggunakan sumbangan Anda secara hati-hati, dan saya berterima kasih atas kepercayaan Anda terhadap kami.</p>
+
+<p>Terima kasih,<br /><br />
+<b>Sue Gardner</b><br />
+Direktur Eksekutif Yayasan Wikimedia</p>
+
+<p>Untuk catatan: Sumbangan Anda pada tanggal {contribution.date} sebesar {contribution.source}.</p>
+
+<p>Surat ini digunakan sebagai catatan sumbangan Anda. Tidak ada barang atau jasa yang disediakan, secara keseluruhan atau sebagian, untuk kontribusi ini. Wikimedia Foundation, Inc. adalah sebuah perusahaan amal nirlaba dengan status bebas pajak 501(c)(3) di Amerika Serikat. Kami beralamat di 149 New Montgomery, 3rd Floor, San Francisco, CA, 94105. Nomor bebas pajak A.S.: 20-0049703</p>
+
+<div style="padding:0 10px 5px 10px; border:1px solid black;">
+<p><i>Opsi bebas kontak:</i></p>
+<p>Kami ingin Anda, selaku penyumbang, terus diberitahu mengenai aktivitas komunitas dan penggalangan dana kami. Jika Anda tidak ingin menerima surel seperti itu dari kami, klik tautan di bawah dan kami akan menghapus Anda dari daftar kontak:</p>
+<a style="padding-left: 25px;" href="{unsubscribe_link}">Berhenti berlangganan</a>
+</div>
+EOD;
+
+
---
+++ sites/all/modules/thank_you/i18n/thank_you.it.php
@@ -0,0 +1,65 @@
+<?php
+
+$TYmsgs['it'] = array(
+	"thank_you_from_name" => "Sue Gardner",
+	"thank_you_to_name" => "{contact.display_name}",
+	"thank_you_to_name_secondary" => "amico della Wikimedia Foundation",
+	"thank_you_subject" => "Un Grazie dalla Wikimedia Foundation",
+	"thank_you_unsubscribe_title" => "Cancellazione dalla Wikimedia Foundation",
+	"thank_you_unsubscribe_button" => "Annulla l'iscrizione",
+//	"thank_you_unsubscribe_confirm" => "",
+//	"thank_you_unsubscribe_warning" => "",
+	"thank_you_unsubscribe_success" => "Sei stato rimosso con successo dalla nostra mailing list",
+	"thank_you_unsubscribe_delay" => "Attendi fino a 4 quattro giorni perché le modifiche abbiano effetto. Ci scusiamo per qualsiasi e-mail che riceverete durante questo periodo. Se hai delle domande contatta <a href='mailto:donations@wikimedia.org'>donations@wikimedia.org</a>.",
+	"thank_you_unsubscribe_fail" => "C'è stato un errore di elaborazione della tua richiesta, contatta <a href='mailto:donations@wikimedia.org'>donations@wikimedia.org</a>.",
+);
+$TYmsgs['it']['thank_you_body_plaintext'] =<<<'EOD'
+Gentile {contact.first_name},
+
+Sei grande! Grazie davvero per aver donato alla Wikimedia Foundation!
+
+È in questo modo che paghiamo le bollette: grazie a persone come te che donano cinque, venti, cento dollari. La mia donazione preferita dell'anno scorso è stata quella di una bambina dall'Inghilterra, che aveva convinto i genitori a lasciarla donare la propria paghetta di cinque sterline. È gente come te, partecipando come quella ragazzina, che rende possibile per Wikipedia continuare a fornire un accesso libero e semplice a informazioni imparziali per ciascuno in tutto il mondo: sia per coloro che aiutano a pagarla, sia per chi non può permettersi di aiutarla. Un grande ringraziamento!
+
+So che è facile ignorare i nostri appelli: sono contento che tu non lo abbia fatto. Da me e dalle decine di migliaia di volontari che scrivono Wikipedia: grazie per averci aiutato a rendere il mondo un luogo migliore. Useremo il tuo denaro con cura, e ti ringrazio di nuovo per la fiducia che hai riposto in noi.
+
+Grazie,
+Sue Gardner
+Direttore Esecutivo della Wikimedia Foundation
+
+---
+
+Per tua archiviazione: la tua donazione del {contribution.date} è stata di {contribution.source}.
+
+Questa lettera può servire come prova della donazione. Nè beni nè servizi sono stati forniti, in tutto o in parte, per questo contributo. La Wikimedia Foundation, Inc. è una società di beneficenza no-profit che gode quindi della 501(c)(3) che dà diritto all'esenzione fiscale della donazione per i residenti negli Stati Uniti. Il nostro indirizzo è 149 New Montgomery, 3rd Floor, San Francisco, CA, 94105. U.S. Numero per l'esenzione fiscale: 20-0049703
+
+---
+
+Opzione Opt-out:
+L'opzione "Opt-out" è l'atto di scegliere di non essere coinvolto in un accordo. Per questo ti ricordiamo che ti invieremo un'email l'anno prossimo di questo periodo per rinnovare la tua donazione. Se preferisci non ricevere da noi un promemoria tramite email, clicca qui sotto e ti toglieremo dalla lista:
+
+{unsubscribe_link}
+EOD;
+
+$TYmsgs['it']['thank_you_body_html'] =<<<'EOD'
+<p>Gentile {contact.first_name},</p>
+
+<p>Sei grande! Grazie davvero per aver donato alla Wikimedia Foundation!</p>
+
+<p>È in questo modo che paghiamo le bollette: grazie a persone come te che donano cinque, venti, cento dollari. La mia donazione preferita dell'anno scorso è stata quella di una bambina dall'Inghilterra, che aveva convinto i genitori a lasciarla donare la propria paghetta di cinque sterline. È gente come te, partecipando come quella ragazzina, che rende possibile per Wikipedia continuare a fornire un accesso libero e semplice a informazioni imparziali per ciascuno in tutto il mondo: sia per coloro che aiutano a pagarla, sia per chi non può permettersi di aiutarla. Un grande ringraziamento!</p>
+
+<p>So che è facile ignorare i nostri appelli: sono contento che tu non lo abbia fatto. Da me e dalle decine di migliaia di volontari che scrivono Wikipedia: grazie per averci aiutato a rendere il mondo un luogo migliore. Useremo il tuo denaro con cura, e ti ringrazio di nuovo per la fiducia che hai riposto in noi.</p>
+
+<p>Grazie,<br /><br />
+<b>Sue Gardner</b><br />
+Direttore Esecutivo della Wikimedia Foundation</p>
+
+<p>Per tua archiviazione: la tua donazione del {contribution.date} è stata di {contribution.source}.</p>
+
+<p>Questa lettera può servire come prova della donazione. Nè beni nè servizi sono stati forniti, in tutto o in parte, per questo contributo. La Wikimedia Foundation, Inc. è una società di beneficenza no-profit che gode quindi della 501(c)(3) che dà diritto all'esenzione fiscale della donazione per i residenti negli Stati Uniti. Il nostro indirizzo è 149 New Montgomery, 3rd Floor, San Francisco, CA, 94105. U.S. Numero per l'esenzione fiscale: 20-0049703</p>
+
+<div style="padding:0 10px 5px 10px; border:1px solid black;">
+<p><i>Opzione Opt-out:</i></p>
+<p>L'opzione "Opt-out" è l'atto di scegliere di non essere coinvolto in un accordo. Per questo ti ricordiamo che ti invieremo un'email l'anno prossimo di questo periodo per rinnovare la tua donazione. Se preferisci non ricevere da noi un promemoria tramite email, clicca qui sotto e ti toglieremo dalla lista:</p>
+<a style="padding-left: 25px;" href="{unsubscribe_link}">Annulla l'iscrizione</a>
+</div>
+EOD;
