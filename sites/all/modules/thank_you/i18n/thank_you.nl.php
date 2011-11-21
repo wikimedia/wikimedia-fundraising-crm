f918234639014ea39bb11afb7cdc439428ca840d
Peter Gehres <pgehres@users.mediawiki.org>
2011-11-21 07:10:33 +0000

    Adding ja, nl, pt, ru, zh-hans and enabling es and zh

---
+++ sites/all/modules/thank_you/i18n/thank_you.nl.php
@@ -0,0 +1,70 @@
+<?php
+
+$TYmsgs['nl'] = array(
+	"thank_you_from_name" => "Sue Gardner",
+	"thank_you_to_name" => "{contact.display_name}",
+	"thank_you_to_name_secondary" => "vriend van de Wikimedia Foundation",
+	"thank_you_subject" => "Dank u wel van de Wikimedia Foundation",
+	"thank_you_unsubscribe_title" => "Uitschrijven van Wikimedia Foundation",
+	"thank_you_unsubscribe_button" => "Uitschrijven",
+//	"thank_you_unsubscribe_confirm" => "",
+//	"thank_you_unsubscribe_warning" => "",
+	"thank_you_unsubscribe_success" => "U bent niet langer ingeschreven op onze mailinglijst",
+	"thank_you_unsubscribe_delay" => "Het duurt maximaal vier (4) dagen om deze wijzigingen door te voeren. In het geval u in de tussentijd nog e-mailberichten van ons ontvangt, bieden wij daar bij voorbaat onze excuses voor aan. Neem alstublieft contact op met <a href='mailto:donations@wikimedia.org'>donations@wikimedia.org</a> als u nog vragen hebt",
+	"thank_you_unsubscribe_fail" => "Er is een fout opgetreden tijdens de verwerking. Neem alstublieft contact op met <a href='mailto:donations@wikimedia.org'>donations@wikimedia.org</a>.",
+);
+$TYmsgs['nl']['thank_you_body_plaintext'] =<<<'EOD'
+Beste {contact.first_name},
+
+Hartelijk dank voor uw donatie aan de Wikimedia Foundation!
+
+Op deze manier betalen we onze rekeningen: door mensen als u, die vijf, twintig of honderd euro geven. Mijn favoriete donatie van het afgelopen jaar was vijf pond van een klein Engels meisje. Ze had haar ouders overtuigd, zodat ze haar zakgeld mocht doneren. Mensen zoals u en haar maken het voor Wikipedia mogelijk om door te gaan met het beschikbaar stellen van gratis, eenvoudig te vinden en onbevooroordeelde informatie, voor iedereen ter wereld. Voor iedereen die helpt meebetalen en ook voor iedereen die zich dit niet kan veroorloven. Hartelijk bedankt.
+
+Ik weet dat het gemakkelijk is om ons verzoek om donaties te negeren, maar ik ben blij dat u dat niet heeft gedaan. Ik zeg dit niet alleen namens mijzelf, maar ook namens de tienduizenden vrijwilligers die Wikipedia helpen schrijven: dank u dat u ons wilt helpen de wereld een beetje beter te maken. We zullen uw geld zorgvuldig gebruiken.
+
+Dank u voor uw vertrouwen in ons.
+
+Bedankt,
+Sue Gardner
+Directeur van de Wikimedia Foundation
+
+---
+
+Voor uw administratie: Uw donatie op {contribution.date} bedroeg {contribution.source} .
+Deze e-mail dient als bewijs van uw donatie. Er zijn, geheel of gedeeltelijk, geen goederen of diensten geleverd voor deze bijdrage. De Wikimedia Foundation is een non-profit organisatie, afhankelijk van giften, en heeft een 201(c)(3) belastingvrijstelling in de Verenigde Staten. Ons adres is 149 New Montgomery, 3rd Floor, San Francisco, CA, 94105. Belastingvrijstellingsnummer in de Verenigde Staten: 20-0049703
+
+---
+
+Uitvinkoptie:
+Wij willen u als donateur graag op de hoogte te houden van de activiteiten van de Wikipediagemeenschap en de fondsenwerving. Als u dergelijke e-mails niet op prijs stelt, klik dan hieronder en we verwijderen uw e-mailadres van de maillijst.
+{unsubscribe_link}
+EOD;
+
+$TYmsgs['nl']['thank_you_body_html'] =<<<'EOD'
+<p>Beste {contact.first_name},</p>
+
+<p>Hartelijk dank voor uw donatie aan de Wikimedia Foundation!</p>
+
+<p>Op deze manier betalen we onze rekeningen: door mensen als u, die vijf, twintig of honderd euro geven. Mijn favoriete donatie van het afgelopen jaar was vijf pond van een klein Engels meisje. Ze had haar ouders overtuigd, zodat ze haar zakgeld mocht doneren. Mensen zoals u en haar maken het voor Wikipedia mogelijk om door te gaan met het beschikbaar stellen van gratis, eenvoudig te vinden en onbevooroordeelde informatie, voor iedereen ter wereld. Voor iedereen die helpt meebetalen en ook voor iedereen die zich dit niet kan veroorloven. Hartelijk bedankt.</p>
+
+<p>Ik weet dat het gemakkelijk is om ons verzoek om donaties te negeren, maar ik ben blij dat u dat niet heeft gedaan. Ik zeg dit niet alleen namens mijzelf, maar ook namens de tienduizenden vrijwilligers die Wikipedia helpen schrijven: dank u dat u ons wilt helpen de wereld een beetje beter te maken. We zullen uw geld zorgvuldig gebruiken.</p>
+
+<p>Dank u voor uw vertrouwen in ons.</p>
+
+<p>Bedankt,<br />
+<br />
+<b>Sue Gardner</b><br />
+Directeur van de Wikimedia Foundation</p>
+
+<p>Voor uw administratie: Uw donatie op {contribution.date} bedroeg {contribution.source}.</p>
+
+<p>Deze e-mail dient als bewijs van uw donatie. Er zijn, geheel of gedeeltelijk, geen goederen of diensten geleverd voor deze bijdrage. De Wikimedia Foundation is een non-profit organisatie, afhankelijk van giften, en heeft een 201(c)(3) belastingvrijstelling in de Verenigde Staten. Ons adres is 149 New Montgomery, 3rd Floor, San Francisco, CA, 94105. Belastingvrijstellingsnummer in de Verenigde Staten: 20-0049703</p>
+
+<div style="padding:0 10px 5px 10px; border:1px solid black;">
+<p><i>Uitvinkoptie:</i></p>
+<p>Wij willen u als donateur graag op de hoogte te houden van de activiteiten van de Wikipediagemeenschap en de fondsenwerving. Als u dergelijke e-mails niet op prijs stelt, klik dan hieronder en we verwijderen uw e-mailadres van de maillijst.</p>
+<a style="padding-left: 25px;" href="{unsubscribe_link}">Uitschrijven</a>
+</div>
+EOD;
+
+
