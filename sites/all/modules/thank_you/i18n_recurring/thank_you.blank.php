ec8b217eaeae52f2816540e94f14dace5a76ad4e
Peter Gehres <pgehres@users.mediawiki.org>
2012-04-13 19:39:40 +0000

    Adding parallel thank you message generation for recurring contributions. Terrible violation of DRY, but right now its simplest and does not require re-testing of normal thank you messages.

---
+++ sites/all/modules/thank_you/i18n_recurring/thank_you.blank.php
@@ -0,0 +1,44 @@
+<?php
+
+$TYmsgs[''] = array(
+	"thank_you_from_name" => "Sue Gardner",
+	"thank_you_to_name" => "{contact.display_name}",
+	"thank_you_to_name_secondary" => "",
+	"thank_you_subject" => "",
+	"thank_you_unsubscribe_title" => "",
+	"thank_you_unsubscribe_button" => "",
+	"thank_you_unsubscribe_confirm" => "",
+	"thank_you_unsubscribe_warning" => "",
+	"thank_you_unsubscribe_success" => "",
+	"thank_you_unsubscribe_delay" => "",
+	"thank_you_unsubscribe_fail" => "",
+);
+$TYmsgs['']['thank_you_body_plaintext'] =<<<'EOD'
+EOD;
+
+$TYmsgs['']['thank_you_body_html'] =<<<'EOD'
+<p></p>
+
+<p></p>
+
+<p></p>
+
+<p></p>
+
+<p><br />
+<br />
+<b></b><br />
+</p>
+
+<p></p>
+
+<p></p>
+
+<div style="padding:0 10px 5px 10px; border:1px solid black;">
+<p><i></i></p>
+<p></p>
+<a style="padding-left: 25px;" href="{unsubscribe_link}">Opt out</a>
+</div>
+EOD;
+
+
