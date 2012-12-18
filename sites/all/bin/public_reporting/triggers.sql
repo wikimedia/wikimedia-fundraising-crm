b9d6d598519bf36949fdb794980265888ccbb77a
Adam Wight <adamw@users.mediawiki.org>
2012-12-18 20:19:18 +0000

    public_reporting is aware of finance_only

--- sites/all/bin/public_reporting/triggers.sql
+++ sites/all/bin/public_reporting/triggers.sql
@@ -4,36 +4,60 @@ DROP TRIGGER IF EXISTS public_reporting_insert;
 DROP TRIGGER IF EXISTS public_reporting_update;
 DROP TRIGGER IF EXISTS public_reporting_contact_update;
 DROP TRIGGER IF EXISTS public_reporting_note_update;
+DROP TRIGGER IF EXISTS public_reporting_custom_insert;
+DROP TRIGGER IF EXISTS public_reporting_custom_update;
 DELIMITER //
-CREATE TRIGGER public_reporting_insert AFTER INSERT ON civicrm_contribution FOR EACH ROW
+CREATE TRIGGER public_reporting_update AFTER UPDATE ON civicrm_contribution FOR EACH ROW
 BEGIN
-    IF SUBSTRING( NEW.source, 1, 3 ) != 'RFD' AND NEW.total_amount > 0 THEN -- don't trigger for refunds
-        INSERT INTO {pr_db}{public_reporting}
-            ( contribution_id, converted_amount, original_currency, original_amount, received )
-            VALUES (
-                NEW.id,
-                NEW.total_amount,
-                SUBSTRING( NEW.source, 1, 3 ),
-                CONVERT( SUBSTRING( NEW.source, 5 ), DECIMAL( 20, 2 ) ),
-                UNIX_TIMESTAMP( NEW.receive_date )
+    IF NOT (SELECT finance_only = 1 FROM wmf_contribution_extra WHERE entity_id = NEW.id) THEN
+        REPLACE INTO {pr_db}{public_reporting}
+            SET
+                contribution_id = NEW.id,
+                converted_amount = NEW.total_amount,
+                original_currency = SUBSTRING( NEW.source, 1, 3 ),
+                original_amount = CONVERT( SUBSTRING( NEW.source, 5 ), DECIMAL( 20, 2 ) ),
+                received = UNIX_TIMESTAMP( NEW.receive_date );
+    END IF;
+END
+//
+CREATE TRIGGER public_reporting_custom_insert AFTER INSERT ON wmf_contribution_extra FOR EACH ROW
+BEGIN
+    IF NEW.finance_only = 1 THEN
+        DELETE from {pr_db}{public_reporting}
+            WHERE {pr_db}{public_reporting}.contribution_id = NEW.entity_id;
+    ELSE
+        REPLACE INTO {pr_db}{public_reporting}
+            (contribution_id, converted_amount, original_currency, original_amount, received)
+            (SELECT
+                    id,
+                    total_amount,
+                    SUBSTRING( source, 1, 3 ),
+                    CONVERT( SUBSTRING( source, 5 ), DECIMAL( 20, 2 ) ),
+                    UNIX_TIMESTAMP( receive_date )
+                FROM civicrm_contribution
+                WHERE id = NEW.entity_id
             );
     END IF;
 END
 //
-CREATE TRIGGER public_reporting_update AFTER UPDATE ON civicrm_contribution FOR EACH ROW
+CREATE TRIGGER public_reporting_custom_update AFTER UPDATE ON wmf_contribution_extra FOR EACH ROW
 BEGIN
-    IF SUBSTRING(NEW.source, 1, 3) = 'RFD' OR NEW.total_amount <= 0 THEN -- trigger for refunds
+    IF NEW.finance_only = 1 THEN
         DELETE from {pr_db}{public_reporting}
-            WHERE {pr_db}{public_reporting}.contribution_id = NEW.id;
+            WHERE {pr_db}{public_reporting}.contribution_id = NEW.entity_id;
     ELSE
-        UPDATE {pr_db}{public_reporting} pr
-            SET
-                pr.converted_amount = NEW.total_amount,
-                pr.original_currency = SUBSTRING( NEW.source, 1, 3 ),
-                pr.original_amount = CONVERT( SUBSTRING( NEW.source, 5 ), DECIMAL( 20, 2 ) ),
-                pr.received = UNIX_TIMESTAMP( NEW.receive_date )
-        WHERE pr.contribution_id = NEW.id;
-  END IF;
+        REPLACE INTO {pr_db}{public_reporting}
+            (contribution_id, converted_amount, original_currency, original_amount, received)
+            (SELECT
+                    id,
+                    total_amount,
+                    SUBSTRING( source, 1, 3 ),
+                    CONVERT( SUBSTRING( source, 5 ), DECIMAL( 20, 2 ) ),
+                    UNIX_TIMESTAMP( receive_date )
+                FROM civicrm_contribution
+                WHERE id = NEW.entity_id
+            );
+    END IF;
 END
 //
 DELIMITER ;
