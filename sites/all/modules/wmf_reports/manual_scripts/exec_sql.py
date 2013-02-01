dc4ab2e8ac65b00d4da829cf2c30ce6b105ab738
Adam Wight <adamw@users.mediawiki.org>
2013-02-01 02:07:33 +0000

    stupid way to order the columns

--- sites/all/modules/wmf_reports/manual_scripts/exec_sql.py
+++ sites/all/modules/wmf_reports/manual_scripts/exec_sql.py
@@ -12,8 +12,30 @@ def exec_file(path=None):
         params={ 'scratch': config.scratch_db_prefix }
     )
 
+columns = [
+    "first_name",
+    "last_name",
+    "organization_name",
+    "phone",
+    "email",
+    "address",
+    "supplemental_address",
+    "city",
+    "state",
+    "country",
+    "zip",
+    "contributions",
+    "note",
+    "do_not_email",
+    "do_not_phone",
+    "civi_id",
+]
+
 if __name__ == "__main__":
+    separator = ","
     results = exec_file(sys.argv[1])
-    print "\t".join(results[0].keys())
+    if not columns:
+        columns = results[0].keys()
+    print separator.join(columns)
     for row in results:
-        print "\t".join([str(v) for v in row.values()])
+        print separator.join([str(row[key]) for key in columns])
