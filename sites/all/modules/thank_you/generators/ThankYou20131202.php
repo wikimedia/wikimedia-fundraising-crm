f36e3db8ee9f529b367faa7b3f6718b51ed99afb
Adam Roses Wight <awight@wikimedia.org>
2014-02-21 17:54:42 -0800

    Thank You sending decoupled from Civi records
    A new test GUI allows every template parameter to be set directly.
    
    Change-Id: I6409578a517d4d432e273f8f98f52fee3b02521f

--- sites/all/modules/thank_you/generators/ThankYou20131202.php
+++ sites/all/modules/thank_you/generators/ThankYou20131202.php
@@ -6,13 +6,13 @@ class ThankYou20131202 extends RenderTranslatedPage {
 		$this->proto_file = __DIR__ . '/../templates/html/thank_you.$1.html';
 
 		$this->substitutions = array(
-			'/\[given name\]/' => '{{contact.first_name}}',
-			'/\[first name\]/' => '{{contact.first_name}}',
-			'/\[family name\]/' => '{{contact.last_name}}',
-			'/\[last name\]/' => '{{contact.last_name}}',
-			'/\[date\]/' => '{{contribution.receive_date}}',
-			'/\[amount\]/' => '{{contribution.contribution_source|l10n_currency(locale)}}',
-			'/\[contributionId\]/' => 'CNTCT-{{contribution.contact_id}}',
+			'/\[given name\]/' => '{{ first_name }}',
+			'/\[first name\]/' => '{{ first_name }}',
+			'/\[family name\]/' => '{{ last_name }}',
+			'/\[last name\]/' => '{{ last_name }}',
+			'/\[date\]/' => '{{ receive_date }}',
+			'/\[amount\]/' => '{{ (currency ~ " " ~ amount) | l10n_currency(locale) }}',
+			'/\[contributionId\]/' => '{{ transaction_id }}',
 
 			'/\[ifRecurring\]/' => '{% if recurring %}',
 			'/\[endifRecurring\]/' => '{% endif %}',
@@ -29,8 +29,8 @@ class ThankYou20131202 extends RenderTranslatedPage {
 			// TODO: DO WE HAVE TRANSLATIONS FOR THE 5-YEAR, STRATEGIC PLAN
 			'/\[#strategic ((?:(?!\]).)*)\]/' => '<a href="https://wikimediafoundation.org/wiki/Wikimedia_Movement_Strategic_Plan_Summary">$1</a>',
 			'/\[#shop ((?:(?!\]).)*)\]/' => '<a href="https://shop.wikimedia.org">$1</a>',
-			'/\[#unsubscribe ((?:(?!\]).)*)\]/' => '<a href="{{unsubscribe_link|raw}}">$1</a>',
-			'/\[#recurringCancel ((?:(?!\]).)*)\]/' => '<a href="https://wikimediafoundation.org/wiki/Special:LandingCheck?landing_page=Cancel_or_change_recurring_payments&basic=true&country={{ country }}&language={{ language }}">$1</a>',
+			'/\[#unsubscribe ((?:(?!\]).)*)\]/' => '<a href="{{ unsubscribe_link | raw }}">$1</a>',
+			'/\[#recurringCancel ((?:(?!\]).)*)\]/' => '<a href="https://wikimediafoundation.org/wiki/Special:LandingCheck?landing_page=Cancel_or_change_recurring_payments&basic=true&language={{ locale }}">$1</a>',
 			'/\[#translate ((?:(?!\]).)*)\]/' => '<a href="https://meta.wikimedia.org/w/index.php?title=Special:Translate&group=page-Fundraising%2FTranslation%2FThank_you_email_20131202">$1</a>',
 			'/\[#donate ((?:(?!\]).)*)\]/' => '<a href="https://donate.wikimedia.org/">$1</a>',
 		);
