1d8a10cdeb26a2e258976d94840fdbecc7f03c10
Adam Roses Wight <awight@wikimedia.org>
2014-05-30 15:29:11 -0700

    Campaign-match notifier
    Deployment:
    * drush en wmf_campaigns
    
    Change-Id: I6c10c686f5d299865db83828928ee5b2c04e5304

---
+++ sites/all/modules/wmf_campaigns/WmfCampaign.php
@@ -0,0 +1,32 @@
+<?php
+
+class WmfCampaign {
+    protected $key;
+    protected $notification_email;
+
+    protected function __construct() {}
+
+    public static function fromKey( $key ) {
+        $result = db_select( 'wmf_campaigns_campaign' )
+            ->fields( 'wmf_campaigns_campaign' )
+            ->condition( 'campaign_key', $key )
+            ->execute()
+            ->fetchAssoc();
+        return WmfCampaign::fromDbRecord( $result );
+    }
+
+    protected static function fromDbRecord( $record ) {
+        $camp = new WmfCampaign();
+        $camp->key = $record['campaign_key'];
+        $camp->notification_email = $record['notification_email'];
+        return $camp;
+    }
+
+    public function getKey() {
+        return $this->key;
+    }
+
+    public function getNotificationEmail() {
+        return $this->notification_email;
+    }
+}
