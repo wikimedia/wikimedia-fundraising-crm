4c6d7534964dbbda44c198b45ffe98a47e04c17e
Adam Roses Wight <awight@wikimedia.org>
2014-07-16 18:09:24 -0700

    Simplify main message processing function
    This improves the calling semantics and debug logging, all information is
    contained in the message.  It also makes the main function usable for
    different scenarios which currently tap directly into and repeat lower-level
    logic.
    
    Change-Id: I0a02bc94ba17b32f0e5a22015d0efb416f4fe26e

---
+++ sites/all/modules/wmf_civicrm/tests/phpunit/ImportMessageTest.php
@@ -0,0 +1,187 @@
+<?php
+
+class ImportMessageTest extends BaseWmfDrupalPhpUnitTestCase {
+    public static function getInfo() {
+        return array(
+            'name' => 'Import Message',
+            'group' => 'Pipeline',
+            'description' => 'Attempt contribution message import.',
+        );
+    }
+
+    public function setUp() {
+        parent::setUp();
+
+        $api = civicrm_api_classapi();
+
+        // TODO: clean up the fixtures
+        $contact_params = array(
+            'contact_type' => 'Individual',
+            'first_name' => 'Test',
+            'last_name' => 'Es',
+
+            'version' => 3,
+        );
+        $api->Contact->Create( $contact_params );
+        $this->contact_id = $api->id;
+
+        $this->recur_amount = '1.23';
+
+        $contribution_params = array(
+            'contact_id' => $this->contact_id,
+            'amount' => $this->recur_amount,
+            'currency' => 'USD',
+            'frequency_unit' => 'month',
+            'frequency_interval' => '1',
+            'installments' => '0',
+            'start_date' => wmf_common_date_unix_to_civicrm( time() ),
+            'create_date' => wmf_common_date_unix_to_civicrm( time() ),
+            'cancel_date' => null,
+            'processor_id' => 1,
+            'cycle_day' => '1',
+            'next_sched_contribution' => null,
+            'trxn_id' => 'RECURRING TEST_GATEWAY 123-1 ' . time(),
+
+            'version' => 3,
+        );
+        $api->ContributionRecur->Create( $contribution_params );
+        $this->contribution_recur_id = $api->id;
+    }
+
+    /**
+     * @XXX doesn't stupid work cos of member vars: dataProvider messageProvider
+     */
+    public function testMessageInsert() {
+        foreach ( $this->messageProvider() as $test ) {
+            list( $msg, $expected_contribution ) = $test;
+
+            // FIXME
+            $this->run_random_id = mt_rand();
+            $msg['gateway_txn_id'] = $this->run_random_id;
+
+            $contribution = wmf_civicrm_contribution_message_import( $msg );
+
+            // Synthesize trxn_id so it matches the random id we just used
+            $expected_transaction = new WmfTransaction();
+            $expected_transaction->gateway = $msg['gateway'];
+            $expected_transaction->gateway_txn_id = $msg['gateway_txn_id'];
+            $expected_transaction->recurring = $msg['recurring'];
+            $expected_transaction->recur_sequence = ( isset( $msg['effort_id'] ) ? $msg['effort_id'] : null );
+            $expected_contribution['trxn_id'] = $expected_transaction->get_unique_id();
+            $this->stripTrxnIdTimestamp( $expected_contribution );
+
+            $this->stripUniques( $contribution );
+
+            // Strip contact_id if we are have no expectation
+            if ( empty( $expected_contribution['contact_id'] ) ) {
+                unset( $contribution['contact_id'] );
+            }
+
+            $this->assertEquals( $expected_contribution, $contribution );
+        }
+    }
+
+    /**
+     * Remove unique stuff which cannot be expected
+     */
+    function stripUniques( &$contribution ) {
+        $isNumber = array(
+            'id',
+            'receive_date',
+        );
+        foreach ( $isNumber as $field ) {
+            $this->assertGreaterThan( 0, $contribution[$field] );
+            unset( $contribution[$field] );
+        }
+
+        $this->stripTrxnIdTimestamp( $contribution );
+    }
+
+    function stripTrxnIdTimestamp( &$contribution ) {
+        $parts = explode( ' ', $contribution['trxn_id'] );
+        array_pop( $parts );
+        $contribution['trxn_id'] = implode( ' ', $parts );
+    }
+
+    public function messageProvider() {
+        return array(
+            array(
+                // Normal contribution
+                array(
+                    'email' => 'nobody@wikimedia.org',
+                    'gross' => '1.23',
+                    'currency' => 'USD',
+                    'payment_method' => 'cc',
+                    'gateway' => 'test_gateway',
+                ),
+                array(
+                    'contribution_type_id' => '5',
+                    'contribution_page_id' => '',
+                    'payment_instrument_id' => '1',
+                    'non_deductible_amount' => '',
+                    'total_amount' => '1.23',
+                    'fee_amount' => '0',
+                    'net_amount' => '1.23',
+                    'invoice_id' => '',
+                    'currency' => 'USD',
+                    'cancel_date' => '',
+                    'cancel_reason' => '',
+                    'receipt_date' => '',
+                    'thankyou_date' => '',
+                    'source' => 'USD 1.23',
+                    'amount_level' => '',
+                    'contribution_recur_id' => '',
+                    'honor_contact_id' => '',
+                    'is_test' => '',
+                    'is_pay_later' => '',
+                    'contribution_status_id' => '',
+                    'honor_type_id' => '',
+                    'address_id' => '',
+                    'check_number' => 'null',
+                    'campaign_id' => '',
+                ),
+            ),
+
+            // Recurring contribution
+            array(
+                array(
+                    'email' => 'nobody@wikimedia.org',
+                    'gross' => $this->recur_amount,
+                    'currency' => 'USD',
+                    'payment_method' => 'cc',
+                    'gateway' => 'test_gateway',
+                    'contact_id' => $this->contact_id,
+                    'contribution_recur_id' => $this->contribution_recur_id,
+                    'effort_id' => 2,
+                ),
+                array(
+                    'contact_id' => strval( $this->contact_id ),
+                    'contribution_type_id' => '5',
+                    'contribution_page_id' => '',
+                    'payment_instrument_id' => '1',
+                    'non_deductible_amount' => '',
+                    'total_amount' => $this->recur_amount,
+                    'fee_amount' => '0',
+                    'net_amount' => $this->recur_amount,
+                    'invoice_id' => '',
+                    'currency' => 'USD',
+                    'cancel_date' => '',
+                    'cancel_reason' => '',
+                    'receipt_date' => '',
+                    'thankyou_date' => '',
+                    'source' => 'USD ' . $this->recur_amount,
+                    'amount_level' => '',
+                    'contribution_recur_id' => strval( $this->contribution_recur_id ),
+                    'honor_contact_id' => '',
+                    'is_test' => '',
+                    'is_pay_later' => '',
+                    'contribution_status_id' => '',
+                    'honor_type_id' => '',
+                    'address_id' => '',
+                    'check_number' => 'null',
+                    'campaign_id' => '',
+                ),
+            ),
+        );
+    }
+}
