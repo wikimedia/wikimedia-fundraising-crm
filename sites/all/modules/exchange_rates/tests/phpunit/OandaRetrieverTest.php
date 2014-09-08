59d82fb76233bcd90f996f0bc3c3f43b6d0a7a18
Elliott Eggleston <ejegg@ejegg.com>
2014-09-08 14:53:52 -0700

    Add OANDA and ECB importers, update with fallthrough
    exchange_rates_cron can now go through a list of retrievers, stopping
    when it gets a good result.  The OANDA retriever uses OANDA's new API.
    Users can configure which data point to retrieve (high/low/average bid,
    or the midpoint between average bid and average ask).
    The fallback ECB update logic has been adapted into a standalone
    retriever.  A few tests for the OANDA retriever are included.
    
    Change-Id: I4dad864e210480fd963a2bdaec854567de632a3d

---
+++ sites/all/modules/exchange_rates/tests/phpunit/OandaRetrieverTest.php
@@ -0,0 +1,95 @@
+<?php
+namespace exchange_rates;
+
+use \BaseWmfDrupalPhpUnitTestCase;
+
+/**
+ * Test OandaRetriever
+ */
+class OandaRetrieverTestCase extends BaseWmfDrupalPhpUnitTestCase {
+
+	/**
+	 * @expectedException exchange_rates\ExchangeRateUpdateException
+	 */
+	public function testExceptionOnBadHttpResponseCode() {
+		$retriever = new OandaRetriever(
+			function( $url, $options ) {
+				return (object) array( 'code' => 404 );
+			}, 
+			'key',
+			'bid'
+		);
+		
+		$retriever->updateRates( array() );
+	}
+
+	/**
+	 * @expectedException exchange_rates\ExchangeRateUpdateException
+	 */
+	public function testExceptionOnBadHttpResponseBody() {
+		$retriever = new OandaRetriever(
+			function( $url, $options ) {
+				return (object) array(
+					'code' => 200,
+					'data' => '{this is not good JSON!}',
+					'headers' => array(),
+				);
+			}, 
+			'key',
+			'bid'
+		);
+		$retriever->updateRates( array() );
+	}
+
+	public function testNormalRetrieval() {
+		$that = $this;
+		$retriever = new OandaRetriever(
+			function( $url, $options ) use ( $that ) {
+				$that->assertEquals( 'Bearer mzplx', $options['headers']['Authorization'] );
+				$urlParts = parse_url( $url );
+				$that->assertEquals( 'https', $urlParts['scheme'] );
+				$that->assertEquals( 'web-services.oanda.com', $urlParts['host'] );
+				$that->assertEquals( '/rates/api/v1/rates/USD.json', $urlParts['path'] );
+				$jsonResponse = '{
+   "base_currency" : "USD",
+   "meta" : {
+      "effective_params" : {
+         "date" : "2014-01-01",
+         "decimal_places" : "all",
+         "fields" : [
+            "midpoint"
+         ],
+         "quote_currencies" : [
+            "EUR",
+            "GBP"
+         ]
+      },
+      "request_time" : "2014-03-30T19:04:25+0000",
+      "skipped_currencies" : []
+   },
+   "quotes" : {
+      "EUR" : {
+         "date" : "2014-01-01T21:00:00+0000",
+         "midpoint" : "0.72527"
+      },
+      "GBP" : {
+         "date" : "2014-01-01T21:00:00+0000",
+         "midpoint" : "0.60365"
+      }
+   }
+}';
+				return (object)array(
+					'code' => 200,
+					'data' => $jsonResponse,
+					'headers' => array( 'x-rate-limit-remaining' => 144 )
+				);
+			}, 
+			'mzplx',
+			'midpoint'
+		);
+		$result = $retriever->updateRates( array( 'EUR', 'GBP' ) );
+		$this->assertEquals( 0.72527, $result->rates['EUR']['value'] );
+		$this->assertEquals( 0.60365, $result->rates['GBP']['value'] );
+		$this->assertEquals( 144, $result->quotesRemaining );
+	}
+}
