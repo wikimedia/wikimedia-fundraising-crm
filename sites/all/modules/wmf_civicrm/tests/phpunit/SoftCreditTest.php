53a86251f8dcca80aa8ef5a3f7ece87232341542
Adam Roses Wight <awight@wikimedia.org>
2015-06-12 17:51:36 -0700

    Implement soft credit import
    Bug: T88836
    Change-Id: I68611df9c0d16b7a94514e08cff8a089ec35b17e

---
+++ sites/all/modules/wmf_civicrm/tests/phpunit/SoftCreditTest.php
@@ -0,0 +1,61 @@
+<?php
+
+class SoftCreditTest extends BaseWmfDrupalPhpUnitTestCase {
+    public static function getInfo() {
+        return array(
+            'name' => 'Soft Credit',
+            'group' => 'Pipeline',
+            'description' => 'Ensure we record soft credits.',
+        );
+    }
+
+    public function testSoftCredit() {
+        $fixtures = CiviFixtures::instance();
+
+        $msg = array(
+            'currency' => 'USD',
+            'date' => time(),
+            'email' => 'nobody@wikimedia.org',
+            'gateway' => 'test_gateway',
+            'gateway_txn_id' => mt_rand(),
+            'gross' => '1.23',
+            'payment_method' => 'cc',
+
+            'soft_credit_to' => $fixtures->org_contact_name,
+        );
+
+        $contribution = wmf_civicrm_contribution_message_import( $msg );
+
+        $api = civicrm_api_classapi();
+        $api->Contribution->Get( array(
+            'id' => $contribution['id'],
+            'return' => array(
+                'soft_credit_to' => 1,
+            ),
+
+            'version' => 3,
+        ) );
+
+        $this->assertEquals( $fixtures->org_contact_id, $api->values[0]->soft_credit_to );
+    }
+
+    /**
+     * @expectedException WmfException
+     * @expectedExceptionMessage Bad soft credit target
+     */
+    public function testBadSoftCreditTarget() {
+        $msg = array(
+            'currency' => 'USD',
+            'date' => time(),
+            'email' => 'nobody@wikimedia.org',
+            'gateway' => 'test_gateway',
+            'gateway_txn_id' => mt_rand(),
+            'gross' => '1.23',
+            'payment_method' => 'cc',
+
+            'soft_credit_to' => 'Not a thing',
+        );
+
+        $contribution = wmf_civicrm_contribution_message_import( $msg );
+    }
+}
