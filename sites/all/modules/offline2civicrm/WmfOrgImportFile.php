93cd628805d38d5271a6130c7d2bc740e2b6468c
Adam Roses Wight <awight@wikimedia.org>
2015-06-22 15:01:33 -0700

    Provide Generic Org import
    Change-Id: I227226f7847f9ae223fe0fa6c8b0e2d1babbba89

---
+++ sites/all/modules/offline2civicrm/WmfOrgImportFile.php
@@ -0,0 +1,50 @@
+<?php
+
+class WmfOrgImportFile extends ChecksFile {
+    /** Make sure the file schema is not damaged. */
+    protected function getRequiredColumns() {
+        return array(
+            'Additional Address 1',
+            'Additional Address 2',
+            'Assistant Contact',
+            'Assistant Name',
+            'Check Number',
+            'City',
+            'Contribution Type',
+            'Country',
+            'Description of Stock',
+            'Direct Mail Appeal',
+            'Do Not Email',
+            'Do Not Mail',
+            'Do Not Phone',
+            'Do Not SMS',
+            'Do Not Solicit',
+            'Email',
+            'External Batch Number',
+            'Gift Source',
+            'Groups',
+            'Is Opt Out',
+            'Name',
+            'Notes',
+            'No Thank You',
+            'Organization Name',
+            'Original Amount',
+            'Original Currency',
+            'Payment Instrument',
+            'Phone',
+            'Postal Code',
+            'Postmark Date',
+            'Received Date',
+            'Relationship Type',
+            'Restrictions',
+            'Soft Credit To',
+            'State',
+            'Street Address',
+            'Tags',
+            'Target Contact',
+            'Thank You Letter Date',
+            'Title',
+            'Transaction ID',
+        );
+    }
+}
