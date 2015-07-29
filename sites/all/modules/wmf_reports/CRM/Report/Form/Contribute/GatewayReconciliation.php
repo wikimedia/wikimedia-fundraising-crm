ee71e1371d7b143321bf60f8c6e81c6597dc739f
Adam Roses Wight <awight@wikimedia.org>
2015-07-29 01:37:00 -0700

    Add Square gateway to GatewayReconciliation
    TODO: function to return this reference list.
    
    Bug: T107224
    Change-Id: I8e06ff08ee1bd59610e39949dbece8702748f91b

--- sites/all/modules/wmf_reports/CRM/Report/Form/Contribute/GatewayReconciliation.php
+++ sites/all/modules/wmf_reports/CRM/Report/Form/Contribute/GatewayReconciliation.php
@@ -17,6 +17,7 @@ class CRM_Report_Form_Contribute_GatewayReconciliation extends CRM_Report_Form {
             'GLOBALCOLLECT' => 'GlobalCollect',
             'JPMORGAN' => 'JP Morgan',
             'PAYPAL' => 'PayPal',
+            'SQUARE' => 'Square',
             'WORLDPAY' => 'Worldpay',
         );
 
