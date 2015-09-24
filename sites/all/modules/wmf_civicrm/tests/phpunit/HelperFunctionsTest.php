6ca9074dceb447dbdfaa7672af84c3d0049c05dd
eileen <emcnaughton@wikimedia.org>
2015-09-25 10:54:36 +1200

    T99836 Civi46: ensure that preferred language exists when creating a contact
    Change-Id: I400577ae622d24d7dedfa28dfa5767bbaeaade0f

--- sites/all/modules/wmf_civicrm/tests/phpunit/HelperFunctionsTest.php
+++ sites/all/modules/wmf_civicrm/tests/phpunit/HelperFunctionsTest.php
@@ -6,15 +6,6 @@
 class HelperFunctionsTest extends BaseWmfDrupalPhpUnitTestCase {
 
     /**
-     * Prepare for test.
-     *
-     * @throws \Exception
-     */
-    public function setUp() {
-        parent::setUp();
-    }
-
-    /**
      * Test wmf_civicrm_tag_contact_for_review.
      *
      * Maintenance note: the civicrm entity_tag get api returns an odd syntax.
@@ -33,6 +24,26 @@ class HelperFunctionsTest extends BaseWmfDrupalPhpUnitTestCase {
         wmf_civicrm_tag_contact_for_review($contact);
         $entityTags = civicrm_api3('EntityTag', 'get', array('entity_id' => $contact['id']));
         $this->assertArrayHasKey(civicrm_api3('Tag', 'getvalue', array('name' => 'Review', 'return' => 'id')), $entityTags['values']);
+    }
+
+    /**
+     * Test wmf_ensure_language_exists
+     *
+     * Maintenance note: the civicrm entity_tag get api returns an odd syntax.
+     *
+     * If that ever gets fixed it may break this test - but only the test would
+     * need to be altered to adapt.
+     *
+     * @throws \CiviCRM_API3_Exception
+     */
+    public function testEnsureLanguageExists() {
+        civicrm_intialize();
+        wmf_civicrm_ensure_language_exists('en_IL');
+        $languages = civicrm_api3('OptionValue', 'get', array(
+            'option_group_name' => 'languages',
+            'name' => 'en_IL',
+        ));
+        $this->assertEquals(1, $languages['count']);
     }
 
 }
