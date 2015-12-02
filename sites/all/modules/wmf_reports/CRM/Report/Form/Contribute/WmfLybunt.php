af316c431bd43e8850666fef0cf403a56c637047
XenoRyet <dkozlowski@wikimedia.org>
2015-12-02 15:49:41 -0800

    Add Default Order to WMF LYBUNT template.
    Make the report sort by sum of last year's donations descending.
    
    Bug: T119911
    Change-Id: I9c3974a5e2d0e090ea823fcacd992ad1f9aff558

--- sites/all/modules/wmf_reports/CRM/Report/Form/Contribute/WmfLybunt.php
+++ sites/all/modules/wmf_reports/CRM/Report/Form/Contribute/WmfLybunt.php
@@ -355,6 +355,10 @@ class CRM_Report_Form_Contribute_WmfLybunt extends CRM_Report_Form_Contribute_Ly
     }
   }
 
+    function orderBy() {
+      $this->_orderBy = "ORDER BY civicrm_contribution_total_amount DESC";
+  }
+  
   function alterDisplay(&$rows) {
     // Prevent parent behavior
   }
