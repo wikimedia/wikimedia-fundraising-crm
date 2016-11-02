e2a97fffeef4a35e09e97050f9279a1f7918adc1
Elliott Eggleston <ejegg@ejegg.com>
2016-11-02 13:53:00 -0500

    Fix regex typo
    https://xkcd.com/1638/
    
    Change-Id: I219605f88be3da09a0a4dee13263dc77f6709812

--- sites/all/modules/wmf_common/WmfQueueConsumer.php
+++ sites/all/modules/wmf_common/WmfQueueConsumer.php
@@ -116,7 +116,7 @@ abstract class WmfQueueConsumer extends BaseQueueConsumer {
 	}
 
 	protected function logMessage( $message ) {
-		$className = preg_replace( '/.*\\/', '', get_called_class() );
+		$className = preg_replace( '/.*\\\/', '', get_called_class() );
 		$formattedMessage = print_r( $message, true );
 		watchdog( $className, $formattedMessage, NULL, WATCHDOG_INFO );
 	}
