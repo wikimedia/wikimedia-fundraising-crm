e288e712e8aa40b21bad28a6f7d68de837366e83
Elliott Eggleston <ejegg@ejegg.com>
2016-11-18 18:12:51 -0600

    return value for TestMailer::send
    Configurable, so we can test success and failure.
    
    Change-Id: If4bedd42acc70fe14fb36868164973aa5c167ba6

--- sites/all/modules/wmf_communication/tests/TestMailer.php
+++ sites/all/modules/wmf_communication/tests/TestMailer.php
@@ -3,15 +3,22 @@ namespace wmf_communication;
 
 class TestMailer implements IMailer {
     static protected $mailings;
+    static protected $success = true;
 
     static public function setup() {
         Mailer::$defaultSystem = 'test';
 
         self::$mailings = array();
+        self::$success = true;
+    }
+
+    static public function setSuccess( $success ) {
+        self::$success = $success;
     }
 
     public function send( $email ) {
         self::$mailings[] = $email;
+        return self::$success;
     }
 
     static public function countMailings() {
