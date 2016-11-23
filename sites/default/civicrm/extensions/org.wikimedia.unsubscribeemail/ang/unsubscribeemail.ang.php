a696034c7ac40467d3b2e89351256e2e3e2e1d60
eileen <emcnaughton@wikimedia.org>
2016-11-24 10:56:31 +1300

    Add extension with screen to do data entry on unsubscribing emails.
    Although the request was to restore an unsubscribe link I could not determine what had been there previously to restore.
    
    I opted in the end to create a screen to do the unsubscribe in a CiviCRM extension (which we can publish & share).
    
    Currently the extension is on my git repo - https://github.com/eileenmcnaughton/org.wikimedia.unsubscribeemail
    
    Not sure if it is important to move to wmf repo or not.
    
    Michael has tested this on staging and says it will allow them to do sets of unsubscribes more efficiently.
    
    Note that to install this it is necessary to:
    1) set the directory path for CiviCRM extensions
    2) set the url path for CiviCRM extension (for ajax reasons)
    3) enable the extension
    4) add the link to the page to the drupal block (I focussed on this rather than a menu entry in CiviCRM
     as it seemed to reflect DS flow.
    
    These things can probably all be done by update script
    as a follow up.
    
    Bug: T147571
    Change-Id: I321cc684d5cd4ce70a53d1b32b7094d0e6c1ed7b

---
+++ sites/default/civicrm/extensions/org.wikimedia.unsubscribeemail/ang/unsubscribeemail.ang.php
@@ -0,0 +1,24 @@
+<?php
+// This file declares an Angular module which can be autoloaded
+// in CiviCRM. See also:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+
+return array (
+  'js' => 
+  array (
+    0 => 'ang/unsubscribeemail.js',
+    1 => 'ang/unsubscribeemail/*.js',
+    2 => 'ang/unsubscribeemail/*/*.js',
+  ),
+  'css' => 
+  array (
+    0 => 'ang/unsubscribeemail.css',
+  ),
+  'partials' => 
+  array (
+    0 => 'ang/unsubscribeemail',
+  ),
+  'settings' => 
+  array (
+  ),
+);
---
+++ sites/default/civicrm/extensions/org.wikimedia.unsubscribeemail/ang/unsubscribeemail/UnsubCntrl.js
@@ -0,0 +1,129 @@
+(function (angular, $, _) {
+
+  angular.module('unsubscribeemail').config(function ($routeProvider) {
+      $routeProvider.when('/email/unsubscribe', {
+        controller: 'UnsubscribeemailUnsubCntrl',
+        templateUrl: '~/unsubscribeemail/UnsubCntrl.html',
+      });
+    }
+  );
+
+  // The controller uses *injection*. This default injects a few things:
+  //   $scope -- This is the set of variables shared between JS and HTML.
+  //   crmApi, crmStatus, crmUiHelp -- These are services provided by civicrm-core .
+  //   myContact -- The current contact, defined above in config().
+  angular.module('unsubscribeemail').controller('UnsubscribeemailUnsubCntrl', function ($scope, crmApi, crmStatus, crmUiHelp) {
+    // The ts() and hs() functions help load strings for this module.
+    var ts = $scope.ts = CRM.ts('unsubscribeemail');
+    var hs = $scope.hs = crmUiHelp({file: 'CRM/unsubscribeemail/UnsubCntrl'}); // See: templates/CRM/unsubscribeemail/UnsubCntrl.hlp
+    $scope.unsubscribeContacts = {};
+    $scope.unsubscribeEmails = {};
+    $scope.nothingToUnsubscribe = true;
+
+    $scope.find = function find(emailEntered) {
+      $scope.nothingToUnsubscribe = true;
+      if ($scope.unsubscribeContacts.length) {
+        var messages = {start: ts('Refreshing results ...'), success: ts('Refresh complete')};
+      }
+      else {
+        var messages = {start: ts('Finding matchs...'), success: ts('Search complete')}
+      }
+      crmApi('Contact', 'get', {
+          email: emailEntered,
+          sequential: 1,
+          return: 'id, display_name, contact_type, is_opt_out',
+        },
+        messages
+      )
+        .then(function (apiResult) {
+            var contactResults = apiResult['values'];
+            $scope.unsubscribeContacts = apiResult['values'];
+            for (var id in contactResults) {
+              if (contactResults[id]['is_opt_out'] === '0') {
+                contactResults[id].do_opt_out = true;
+                $scope.nothingToUnsubscribe = false;
+              }
+              else {
+                contactResults[id].do_opt_out = false;
+              }
+              contactResults[id].url = CRM.url('civicrm/contact/view', {
+                'cid': contactResults[id].id,
+                'reset': 1
+              });
+            }
+            $scope.unsubscribeContacts = contactResults;
+          }
+        );
+      $scope.unsubscribeEmails = {};
+
+      crmApi('Email', 'get', {
+        email: emailEntered,
+        is_primary: 0,
+        sequential: 1,
+        'return': ['id', 'is_bulkmail', 'contact_id', 'email', 'contact_id.display_name'],
+      }, messages)
+        .then(function (apiEmailResult) {
+          var emailResults = apiEmailResult['values'];
+          for (var id in emailResults) {
+            emailResults[id].contact_id_display_name = emailResults[id]['contact_id.display_name'];
+            emailResults[id].url = CRM.url('civicrm/contact/view', {
+              'cid': emailResults[id].contact_id,
+              'reset': 1
+            });
+            if (emailResults[id]['is_bulkmail'] > 0) {
+              emailResults[id].do_opt_out = true;
+              $scope.nothingToUnsubscribe = false;
+            }
+            else {
+              emailResults[id].do_opt_out = false;
+            }
+          }
+          $scope.unsubscribeEmails = emailResults;
+        });
+    };
+
+    $scope.unsubscribe = function unsubscribe(enteredEmail) {
+      var requests = [];
+      for (var id in $scope.unsubscribeContacts) {
+        if ($scope.unsubscribeContacts[id].do_opt_out) {
+          requests.push(['Contact', 'create', {
+            id: $scope.unsubscribeContacts[id].id,
+            is_opt_out: 1,
+          }]);
+        }
+      }
+      for (var id in $scope.unsubscribeEmails) {
+        if ($scope.unsubscribeEmails[id].do_opt_out) {
+          requests.push(['Email', 'create', {
+            id: $scope.unsubscribeEmails[id].id,
+            is_bulkmail: 0,
+          }]);
+        }
+      }
+      crmApi(requests, {success: ts('Unsubscribed')}).then(function () {
+          $scope.nothingToUnsubscribe = true;
+          for (var id in $scope.unsubscribeEmails) {
+            if ($scope.unsubscribeEmails[id]['do_opt_out'] == 1) {
+              $scope.unsubscribeEmails[id]['is_bulkmail'] = 0;
+              $scope.unsubscribeEmails[id]['do_opt_out'] = 0;
+            }
+            else {
+              $scope.nothingToUnsubscribe = false;
+            }
+          }
+          for (var id in $scope.unsubscribeContacts) {
+            if ($scope.unsubscribeContacts[id]['do_opt_out'] == 1) {
+              $scope.unsubscribeContacts[id]['is_opt_out'] = 1;
+              $scope.unsubscribeContacts[id]['do_opt_out'] = 0;
+            }
+            else {
+              $scope.nothingToUnsubscribe = false;
+            }
+          }
+        }
+      );
+
+    };
+  });
+
+})(angular, CRM.$, CRM._);
