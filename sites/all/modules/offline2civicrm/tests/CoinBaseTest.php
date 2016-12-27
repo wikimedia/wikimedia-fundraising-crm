e4893eaed91af8aa31e3ddf30d163fb2a88bcb55
eileen <emcnaughton@wikimedia.org>
2016-12-28 10:50:18 +1300

    Add preliminary CoinBase Test.
    Bug: T153791
    Change-Id: Ie7e0931986e92159c2fdb323b994245669a49f90

---
+++ sites/all/modules/offline2civicrm/tests/CoinBaseTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @group Import
+ * @group Offline2Civicrm
+ */
+class CoinBaseTest extends BaseChecksFileTest {
+    protected $epochtime;
+
+    function setUp() {
+        parent::setUp();
+
+        $this->epochtime = wmf_common_date_parse_string('2016-09-15');
+        $this->setExchangeRates($this->epochtime, array('USD' => 1, 'BTC' => 3));
+        $this->gateway = 'coinbase';
+    }
+
+    function testImport() {
+        civicrm_initialize();
+        $this->trxn_id = 'Pluto';
+        $this->doCleanUp();
+
+        $importer = new CoinbaseFile( __DIR__ . "/data/coinbase.csv" );
+        $importer->import();
+
+        $contribution = wmf_civicrm_get_contributions_from_gateway_id($this->gateway, $this->trxn_id);
+        $this->assertEquals( 1, count( $contribution ) );
+        $this->assertEquals( 'COINBASE PLUTO', $contribution[0]['trxn_id'] );
+    }
+
+}
