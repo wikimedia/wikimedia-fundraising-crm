c5ee15575bf10384161b8b8db4a243e6afac692b
Elliott Eggleston <ejegg@ejegg.com>
2017-04-20 16:52:34 -0700

    Fix civibuild options for updated amp
    Depends on amp pull request https://github.com/amp-cli/amp/pull/46
    
    Change-Id: I87d66b3bf8a28814b2f69bd7b52739fd2f7462ef

--- bin/ci-populate-dbs.sh
+++ bin/ci-populate-dbs.sh
@@ -7,13 +7,14 @@ echo "Populating databases with the prefix '${CIVICRM_SCHEMA_PREFIX}'"
 
 export PRECREATED_DSN_PATTERN="mysql://${CIVICRM_MYSQL_USERNAME}:${CIVICRM_MYSQL_PASSWORD}@${CIVICRM_MYSQL_CLIENT}/${CIVICRM_SCHEMA_PREFIX}{{db_seq}}"
 export AMPHOME="${WORKSPACE}/.amp-${BUILD_NUMBER}"
+export NO_SAMPLE_DATA=1
 
 #FIXME: --web-root="$WORKSPACE/src/crm"
 
 $WORKSPACE/src/wikimedia/fundraising/civicrm-buildkit/bin/civi-download-tools
 
 $WORKSPACE/src/wikimedia/fundraising/civicrm-buildkit/bin/amp config:set \
-	--mysql_type=precreated \
+	--db_type=mysql_precreated \
 	--httpd_type=none \
 	--perm_type=none
 
