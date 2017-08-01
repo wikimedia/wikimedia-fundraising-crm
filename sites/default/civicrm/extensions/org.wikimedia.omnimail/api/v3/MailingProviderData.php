105027eb07a7b0c5fc0828611d22c38e1b41bbed
eileen <emcnaughton@wikimedia.org>
2017-08-01 15:37:43 +1200

    Omnimail MailingProviderData.get: extend specificity on id.
    We are using 3 fields to form a unique id on get but the first contact I checked had
    2 actions at exactly the same time on the same mailing. One was clickstream and the other
    opt out. Probably this means the contact clicked on a link to opt out and will be a common pattern
    
    Bug: T161760
    
    Change-Id: I225f5fc33e77b55569e190557730e5c7238c1688

--- sites/default/civicrm/extensions/org.wikimedia.omnimail/api/v3/MailingProviderData.php
+++ sites/default/civicrm/extensions/org.wikimedia.omnimail/api/v3/MailingProviderData.php
@@ -43,7 +43,7 @@ function civicrm_api3_mailing_provider_data_delete($params) {
  */
 function civicrm_api3_mailing_provider_data_get($params) {
   $sql = CRM_Utils_SQL_Select::fragment();
-  $sql->select('CONCAT(contact_identifier, mailing_identifier, recipient_action_datetime) as id');
+  $sql->select('CONCAT(contact_identifier, mailing_identifier, recipient_action_datetime, event_type) as id');
   return civicrm_api3_create_success(_civicrm_api3_basic_get('CRM_Omnimail_BAO_MailingProviderData', $params, FALSE, 'MailingProviderData', $sql, FALSE), $params, 'MailingProviderData', 'get');
 }
 
