5a1cc7e3ba20c5d711f330a7c1eacc78050cf949
Elliott Eggleston <ejegg@ejegg.com>
2017-08-29 13:10:26 -0500

    Fix c_t.country population, add test
    Oops, the last code was never inserting because the
    pseudoConstant::countryIsoCode function expected the numeric ID,
    not the ISO code.
    
    Bug: T171658
    Change-Id: Ie7de69e0722e5bc23a8c1439c74338debfa6214b

--- sites/all/modules/offline2civicrm/tests/ChecksFileTest.php
+++ sites/all/modules/offline2civicrm/tests/ChecksFileTest.php
@@ -25,4 +25,43 @@ class ChecksFileTest extends BaseChecksFileTest {
         $importer = new ChecksFileProbe( "no URI" );
         $output = $importer->_parseRow( $data );
     }
+
+	/**
+	 * Populate contribution_tracking.country
+	 */
+	function testImportCountry() {
+		// A few kinds of empty.
+		$data = array(
+			'Check Number' => mt_rand(),
+			'City' => 'blah city',
+			'Country' => 'AR',
+			'Email' => 'email@phony.com',
+			'External Batch Number' => mt_rand(),
+			'First Name' => 'Test_first_name',
+			'Gift Source' => 'Community GIft',
+			'Last Name' => 'Test_last_name',
+			'Original Amount' => '123',
+			'Original Currency' => 'USD',
+			'Payment Instrument' => 'Trilogy',
+			'Postal Code' => '90210',
+			'Postmark Date' => '2012-02-02',
+			'Received Date' => '2017-07-07',
+			'State' => 'CA',
+			'Street Address' => '123 Sunset Boulevard',
+			'Transaction ID' => mt_rand(),
+		);
+
+		$importer = new ChecksFileProbe( "no URI" );
+		$message = $importer->_parseRow( $data );
+		$importer->doImport( $message );
+		$contribution = $this->callAPISuccessGetSingle(
+			'Contribution', array( 'trxn_id' => "GENERIC_IMPORT {$data['Transaction ID']}" )
+		);
+		$ct = db_select( 'contribution_tracking', 'contribution_tracking' )
+			->fields( 'contribution_tracking' )
+			->condition( 'contribution_id', $contribution['id'] )
+			->execute()
+			->fetchAssoc();
+		$this->assertEquals( 'AR', $ct['country'] );
+	}
 }
