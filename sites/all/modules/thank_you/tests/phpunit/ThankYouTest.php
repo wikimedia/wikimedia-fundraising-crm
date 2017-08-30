5337413498974e9c8295c7fbb55e0a5d004d6a96
Elliott Eggleston <ejegg@ejegg.com>
2017-08-30 17:49:02 -0500

    Add unit tests for Thank You mailer
    And move a test for a helper function into the new class.
    
    Change-Id: If2046ab1ab6632eb9cfa4bb36d28b24b22d21bab

---
+++ sites/all/modules/thank_you/tests/phpunit/ThankYouTest.php
@@ -0,0 +1,162 @@
+<?php
+
+use wmf_communication\TestMailer;
+
+/**
+ * @group ThankYou
+ */
+class ThankYouTest extends BaseWmfDrupalPhpUnitTestCase {
+
+	/**
+	 * Id of the contribution created in the setup function.
+	 *
+	 * @var int
+	 */
+	protected $contribution_id;
+	protected $contact_id;
+	protected $old_civimail;
+	protected $old_civimail_rate;
+	protected $message;
+
+	public function setUp() {
+		if ( !defined( 'WMF_UNSUB_SALT' ) ) {
+			define( 'WMF_UNSUB_SALT', 'abc123' );
+		}
+		parent::setUp();
+		civicrm_initialize();
+		TestMailer::setup();
+		$this->message = array(
+			'city' => 'Somerville',
+			'country' => 'US',
+			'currency' => 'USD',
+			'date' => time(),
+			'email' => 'generousdonor@example.org',
+			'first_name' => 'Test',
+			'last_name' => 'Es',
+			'language' => 'en',
+			'gateway' => 'test_gateway',
+			'gateway_txn_id' => mt_rand(),
+			'gross' => '1.23',
+			'payment_method' => 'cc',
+			'postal_code' => '02144',
+			'state_province' => 'MA',
+			'street_address' => '1 Davis Square',
+		);
+		$this->old_civimail = variable_get( 'thank_you_add_civimail_records', 'false' );
+		$this->old_civimail_rate = variable_get( 'thank_you_civimail_rate', 1.0 );
+
+		$contribution = wmf_civicrm_contribution_message_import( $this->message );
+
+		$this->contact_id = $contribution['contact_id'];
+		$this->contribution_id = $contribution['id'];
+	}
+
+	public function tearDown() {
+		parent::cleanUpContact( $this->contact_id );
+		variable_set( 'thank_you_add_civimail_records', $this->old_civimail );
+		variable_get( 'thank_you_civimail_rate', $this->old_civimail_rate );
+		parent::tearDown();
+	}
+
+	/**
+	 * FIXME: This test has to be first, because we have to make sure
+	 *  those tags exist before anyone calls the helper function, which
+	 *  stashes stuff in a function-level staticvar
+	 *
+	 * @throws \CiviCRM_API3_Exception
+	 */
+	public function testGetEntityTagDetail() {
+		$tag1 = $this->ensureTagExists( 'smurfy' );
+		$tag2 = $this->ensureTagExists( 'smurfalicious' );
+
+		$this->callAPISuccess(
+			'EntityTag',
+			'create',
+			array(
+				'entity_id' => $this->contribution_id,
+				'entity_table' => 'civicrm_contribution',
+				'tag_id' => 'smurfy'
+			)
+		);
+		$this->callAPISuccess(
+			'EntityTag',
+			'create',
+			array(
+				'entity_id' => $this->contribution_id,
+				'entity_table' => 'civicrm_contribution',
+				'tag_id' => 'smurfalicious'
+			)
+		);
+
+		$smurfiestTags = wmf_thank_you_get_tag_names( $this->contribution_id );
+		$this->assertEquals( array( 'smurfy', 'smurfalicious' ), $smurfiestTags );
+
+		$this->callAPISuccess( 'Tag', 'delete', array( 'id' => $tag1 ) );
+		$this->callAPISuccess( 'Tag', 'delete', array( 'id' => $tag2 ) );
+	}
+
+	public function testSendThankYou() {
+		variable_set( 'thank_you_add_civimail_records', 'false' );
+		$result = thank_you_for_contribution( $this->contribution_id );
+		$this->assertTrue( $result );
+		$this->assertEquals( 1, TestMailer::countMailings() );
+		$sent = TestMailer::getMailing( 0 );
+		$this->assertEquals( $this->message['email'], $sent['to_address'] );
+		$this->assertEquals(
+			"{$this->message['first_name']} {$this->message['last_name']}",
+			$sent['to_name']
+		);
+		$expectedBounce = 'bounce-' .
+			str_replace( '@', '=', $this->message['email'] ) .
+			'@donate.wikimedia.org';
+		$this->assertEquals( $expectedBounce, $sent['reply_to'] );
+		$this->assertRegExp( '/\$ 1.23/', $sent['html'] );
+	}
+
+	public function testSendThankYouAddCiviMailActivity() {
+		variable_set( 'thank_you_add_civimail_records', 'true' );
+		variable_set( 'thank_you_civimail_rate', 1.0 );
+		$result = thank_you_for_contribution( $this->contribution_id );
+		$this->assertTrue( $result );
+		$activity = civicrm_api3(
+			'Activity',
+			'getSingle',
+			array(
+				'contact_id' => $this->contact_id,
+				'activity_type_id' => CRM_Core_PseudoConstant::getKey(
+					'CRM_Activity_BAO_Activity',
+					'activity_type_id',
+					'Email'
+				)
+			)
+		);
+		$this->assertEquals( 1, TestMailer::countMailings() );
+		$sent = TestMailer::getMailing( 0 );
+		$this->assertEquals( $activity['details'], $sent['html'] );
+	}
+
+	/**
+	 * Helper function to protect test against cleanup issues.
+	 *
+	 * @param string $name
+	 * @return int
+	 */
+	public function ensureTagExists( $name ) {
+		$tags = $this->callAPISuccess( 'EntityTag', 'getoptions', array(
+			'field' => 'tag_id'
+		) );
+		if ( in_array( $name, $tags['values'] ) ) {
+			return array_search( $name, $tags['values'] );
+		}
+		$tag = $this->callAPISuccess(
+			'Tag',
+			'create',
+			array(
+				'used_for' => 'civicrm_contribution',
+				'name' => $name
+			)
+		);
+		$this->callAPISuccess( 'Tag', 'getfields', array( 'cache_clear' => 1 ) );
+		return $tag['id'];
+	}
+}
--- sites/all/modules/wmf_civicrm/tests/phpunit/HelperFunctionsTest.php
+++ sites/all/modules/wmf_civicrm/tests/phpunit/HelperFunctionsTest.php
@@ -48,58 +48,6 @@ class HelperFunctionsTest extends BaseWmfDrupalPhpUnitTestCase {
     $this->assertEquals(FALSE, $paymentMethodID);
   }
 
-    /**
-     * Test wmf custom api entity get detail.
-     *
-     * @todo consider moving test to thank_you module or helper function out of there.
-     *
-     * @throws \CiviCRM_API3_Exception
-     */
-    public function testGetEntityTagDetail() {
-        civicrm_initialize();
-        $contact = $this->callAPISuccess('Contact', 'create', array(
-            'first_name' => 'Papa',
-            'last_name' => 'Smurf',
-            'contact_type' => 'Individual',
-        ));
-        $contribution = $this->callAPISuccess('Contribution', 'create', array(
-            'contact_id' => $contact['id'],
-            'total_amount' => 40,
-            'financial_type_id' => 'Donation',
-        ));
-
-        $tag1 = $this->ensureTagExists('smurfy');
-        $tag2 = $this->ensureTagExists('smurfalicious');
-
-        $this->callAPISuccess('EntityTag', 'create', array('entity_id' => $contribution['id'], 'entity_table' => 'civicrm_contribution', 'tag_id' => 'smurfy'));
-        $this->callAPISuccess('EntityTag', 'create', array('entity_id' => $contribution['id'], 'entity_table' => 'civicrm_contribution', 'tag_id' => 'smurfalicious'));
-
-        $smurfiestTags = wmf_thank_you_get_tag_names($contribution['id']);
-        $this->assertEquals(array('smurfy', 'smurfalicious'), $smurfiestTags);
-
-        $this->callAPISuccess('Tag', 'delete', array('id' => $tag1));
-        $this->callAPISuccess('Tag', 'delete', array('id' => $tag2));
-    }
-
-    /**
-     * Helper function to protect test against cleanup issues.
-     *
-     * @param string $name
-     * @return int
-     */
-    public function ensureTagExists($name) {
-        $tags = $this->callAPISuccess('EntityTag', 'getoptions', array('field' => 'tag_id'));
-        if (in_array($name, $tags['values'])) {
-            return array_search($name, $tags['values']);
-        }
-        $tag = $this->callAPISuccess('Tag', 'create', array(
-            'used_for' => 'civicrm_contribution',
-            'name' => $name
-        ));
-        $this->callAPISuccess('Tag', 'getfields', array('cache_clear' => 1));
-        return $tag['id'];
-    }
-
     public function testParseWatchdogLog() {
         $logLine = "Apr 21 17:00:02 mach1001 drupal: RecurringQueueConsumer|1492791234|127.0.0.1|https://example.wikimedia.org/index.php||1||Array#012(#012    [date] => 1492791234#012    [txn_type] => subscr_payment#012    [gateway_txn_id] => 1X123456TJ0987654#012    [currency] => EUR#012    [contribution_tracking_id] => 47012345#012    [email] => donor@generous.com#012    [first_name] => DONNY#012    [last_name] => DONOR#012    [street_address] => 123 GOLDFISH POND RD#012    [city] => LONDON#012    [state_province] => #012    [country] => GB#012    [postal_code] => 1000#012    [gross] => 10.00#012    [fee] => 0.62#012    [order_id] => 47012345#012    [recurring] => 1#012    [subscr_id] => S-9TN12345BR9987654#012    [middle_name] => #012    [gateway] => paypal#012    [source_name] => SmashPig#012    [source_type] => listener#012    [source_host] => mach1001#012    [source_run_id] => 113106#012    [source_version] => 200f63eedb05f5e6665d9837bba97e7e7237a41d#012    [source_enqueued_time] => 1492793225#012)";
         $parsed = wmf_civicrm_parse_watchdog_array( $logLine );
