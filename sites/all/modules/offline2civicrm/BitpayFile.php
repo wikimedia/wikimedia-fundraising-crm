b2fff080f3afb84c956c5cd7a7b12737c53c8aad
eileen <emcnaughton@wikimedia.org>
2018-05-02 17:42:08 +1200

    Add bitpay import
    This is the minimal import that should seemingly work, although it would need to be honed
    based on the actual data filled one.
    
    Some questions outstanding (unsure whether to resolve now or later)
    
    1) do we want any defaults for utm_medium / utm_source / utm_campaign?
        These can be added to the csv by the importer but Stripe, for example, used MGEventEmail
    
    2) Currencies - the data sample shows us receiving in EUR & getting settlement in Euro. I changed the line I am checking to settlement in USD - can we expect this to be the case? Otherwise I guess ... complexity.
    
    3) Fees - the data sample has some rows showing fees. I have assumed we will simply no import these as
    I didn't spot a precedent for this
    
    4) Refunds - the data sample has some rows showing refunds. We don't have a prededent for this -
    do we need to handle it
    
    5) Deduping - after discussing with Elliot I have not done any dedupe handling (although I guess the deduper will dedupe some after import) Elliott suggested the data quality might be pretty poor in terms of names etc anyway.
    
    6) Sample data "Satoshi Nakamoto" - that one was in there when I got the sample - nice touch.
    
    Bug: T192953
    Change-Id: I21122cf7444caefb0fd21625169cdf8236562f72

---
+++ sites/all/modules/offline2civicrm/BitpayFile.php
@@ -0,0 +1,83 @@
+<?php
+
+class BitpayFile extends ChecksFile {
+  protected function getRequiredColumns() {
+    return array(
+      'date',
+      'time',
+      'payout amount',
+      'payout currency',
+      'invoice price',
+      'invoice currency',
+    );
+  }
+
+  /**
+   * Do any final transformation on a normalized and default-laden queue message.
+   *
+   * @param array $msg
+   *
+   * @throws \WmfException
+   */
+  protected function mungeMessage(&$msg) {
+    list($msg['first_name'], $msg['last_name']) = wmf_civicrm_janky_split_name(CRM_Utils_Array::value('full_name', $msg));
+    parent::mungeMessage($msg);
+  }
+
+  protected function getFieldMapping() {
+    return [
+      'payout currency' => 'currency',
+      'invoice currency' => 'original_currency',
+      'date' => 'date',
+      'buyerEmail' => 'email',
+      'buyerName' => 'full_name',
+      'buyerState' => 'state_province',
+      'buyerZip' => 'postal_code',
+      'buyerAddress1' => 'street_address',
+      'buyerAddress2' => 'supplemental_address_1',
+      'buyerCity' => 'city',
+      'buyerCountry' => 'country',
+      'payout amount' => 'gross',
+      'invoice price' => 'original_gross',
+      'invoice id' => 'gateway_txn_id',
+      'utm_source' => 'utm_source',
+      'utm_campaign' => 'utm_campaign',
+      'utm_medium' => 'utm_medium',
+    ];
+    // unmapped fields are
+    // 'order id',,'tx type', 'exchange rate (EUR)','description', 'buyerPhone'
+  }
+
+  protected function getDefaultValues() {
+    return array_merge(parent::getDefaultValues(), array(
+        'gateway' => 'bitpay',
+        'payment_instrument' => 'Bitcoin',
+        'payment_method' => 'bitcoin',
+        'contact_source' => 'Bitpay import',
+      )
+    );
+  }
+
+  /**
+   * Read a row and transform into normalized queue message form
+   *
+   * @param $data
+   *
+   * @return array queue message format
+   *
+   * @throws \EmptyRowException
+   * @throws \WmfException
+   */
+  protected function parseRow($data) {
+    if ($data['tx type'] === 'Invoice Refund') {
+      throw new WmfException(WmfException::INVALID_MESSAGE, 'Refunds not currently handled. Please log a Phab if required');
+    }
+    if ($data['tx type'] !== 'sale') {
+      throw new EmptyRowException();
+    }
+
+
+    return parent::parseRow($data);
+  }
+
+}
