40bdc10b71b8b79a7ea0d567be7d712865ea9e9f
Elliott Eggleston <ejegg@ejegg.com>
2018-11-05 18:38:15 -0500

    Fix large donation threshold comparison
    Was just checking for min threshold
    
    Bug: T208817
    Change-Id: I0bceb59a8c1fe2b791685b168261a31718af59f9

--- sites/all/modules/large_donation/tests/LargeDonationTest.php
+++ sites/all/modules/large_donation/tests/LargeDonationTest.php
@@ -7,6 +7,12 @@ use wmf_communication\TestMailer;
  */
 class LargeDonationTest extends BaseWmfDrupalPhpUnitTestCase {
 
+  protected $threshold;
+
+  protected $threshold_high;
+
+  protected $contact_id;
+
   function setUp() {
     parent::setUp();
     civicrm_initialize();
@@ -14,6 +20,7 @@ class LargeDonationTest extends BaseWmfDrupalPhpUnitTestCase {
     TestMailer::setup();
 
     $this->threshold = 100;
+    $this->threshold_high = 1000;
 
     db_delete('large_donation_notification')
       ->execute();
@@ -22,6 +29,14 @@ class LargeDonationTest extends BaseWmfDrupalPhpUnitTestCase {
       ->fields(array(
         'addressee' => 'notifee@localhost.net',
         'threshold' => $this->threshold,
+        'financial_types_excluded' => CRM_Core_PseudoConstant::getKey('CRM_Contribute_BAO_Contribution', 'financial_type_id', 'Endowment Gift')
+      ))
+      ->execute();
+
+    db_insert('large_donation_notification')
+      ->fields(array(
+        'addressee' => 'highrollingnotifee@localhost.net',
+        'threshold' => $this->threshold_high,
         'financial_types_excluded' => CRM_Core_PseudoConstant::getKey('CRM_Contribute_BAO_Contribution', 'financial_type_id', 'Endowment Gift')
       ))
       ->execute();
@@ -72,7 +87,31 @@ class LargeDonationTest extends BaseWmfDrupalPhpUnitTestCase {
     $mailing = TestMailer::getMailing(0);
     $this->assertEquals(1, preg_match("/{$amount}/", $mailing['html']),
       'Found amount in the notification email body.');
-    }
+    $this->assertEquals('notifee@localhost.net', $mailing['to']);
+  }
+
+  function testAboveHighThreshold() {
+    $amount = $this->threshold_high + 0.01;
+    $this->callAPISuccess('Contribution', 'create', array(
+      'contact_id' => $this->contact_id,
+      'financial_type_id' => 'Cash',
+      'currency' => 'USD',
+      'payment_instrument' => 'Credit Card',
+      'total_amount' => $amount,
+      'trxn_id' => 'TEST_GATEWAY ' . mt_rand(),
+      'source' => 'EUR 2020',
+    ));
+
+    $this->assertEquals(2, TestMailer::countMailings());
+
+    $mailing = TestMailer::getMailing(0);
+    $mailing2 = TestMailer::getMailing(1);
+    $this->assertEquals([
+      'notifee@localhost.net', 'highrollingnotifee@localhost.net'
+    ], [
+      $mailing['to'], $mailing2['to']
+    ]);
+  }
 
   /**
    * Test no mailing is sent for this smaller type.
