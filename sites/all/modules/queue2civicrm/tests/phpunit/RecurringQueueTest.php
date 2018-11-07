94326356f0782b5c7e3cd6b59e0aa1974f0d2c3b
Elliott Eggleston <ejegg@ejegg.com>
2018-11-07 18:56:23 -0500

    Reformate RecurringQueueTest to drupal style
    Change-Id: I48bcfee6aa7a2fe28e8ebcfd03cc5846312f7d6b

--- sites/all/modules/queue2civicrm/tests/phpunit/RecurringQueueTest.php
+++ sites/all/modules/queue2civicrm/tests/phpunit/RecurringQueueTest.php
@@ -1,4 +1,5 @@
 <?php
+
 use queue2civicrm\recurring\RecurringQueueConsumer;
 
 /**
@@ -6,215 +7,222 @@ use queue2civicrm\recurring\RecurringQueueConsumer;
  */
 class RecurringQueueTest extends BaseWmfDrupalPhpUnitTestCase {
 
-	/**
-	 * @var RecurringQueueConsumer
-	 */
-	protected $consumer;
+  /**
+   * @var RecurringQueueConsumer
+   */
+  protected $consumer;
 
-	protected $contributions = array();
-	protected $ctIds = array();
+  protected $contributions = [];
 
-	public function setUp() {
-		parent::setUp();
-		$this->consumer = new RecurringQueueConsumer(
-			'recurring'
-		);
-	}
+  protected $ctIds = [];
 
-	// TODO: other queue import tests need to clean up like this!
-	public function tearDown() {
-		foreach ( $this->ctIds as $ctId ) {
-			db_delete( 'contribution_tracking' )
-				->condition( 'id', $ctId )
-				->execute();
-		}
-		foreach( $this->contributions as $contribution ) {
-			if ( !empty( $contribution['contribution_recur_id'] ) ) {
-				CRM_Core_DAO::executeQuery(
-					"
-				DELETE FROM civicrm_contribution_recur
-				WHERE id = %1",
-					array( 1 => array( $contribution['contribution_recur_id'], 'Positive' ) )
-				);
-			}
-			CRM_Core_DAO::executeQuery(
-				"
-			DELETE FROM civicrm_contribution
-			WHERE id = %1",
-				array( 1 => array( $contribution['id'], 'Positive' ) )
-			);
-			CRM_Core_DAO::executeQuery(
-				"
-			DELETE FROM civicrm_contact
-			WHERE id = %1",
-				array( 1 => array( $contribution['contact_id'], 'Positive' ) )
-			);
-		}
-		parent::tearDown();
-	}
+  public function setUp() {
+    parent::setUp();
+    $this->consumer = new RecurringQueueConsumer(
+      'recurring'
+    );
+  }
 
-	protected function addContributionTracking( $ctId ) {
-		$this->ctIds[] = $ctId;
-		db_insert( 'contribution_tracking' )
-			->fields( array( 'id' => $ctId ) )
-			->execute();
-	}
+  // TODO: other queue import tests need to clean up like this!
+  public function tearDown() {
+    foreach ($this->ctIds as $ctId) {
+      db_delete('contribution_tracking')
+        ->condition('id', $ctId)
+        ->execute();
+    }
+    foreach ($this->contributions as $contribution) {
+      if (!empty($contribution['contribution_recur_id'])) {
+        CRM_Core_DAO::executeQuery(
+          "
+        DELETE FROM civicrm_contribution_recur
+        WHERE id = %1",
+          [1 => [$contribution['contribution_recur_id'], 'Positive']]
+        );
+      }
+      CRM_Core_DAO::executeQuery(
+        "
+      DELETE FROM civicrm_contribution
+      WHERE id = %1",
+        [1 => [$contribution['id'], 'Positive']]
+      );
+      CRM_Core_DAO::executeQuery(
+        "
+      DELETE FROM civicrm_contact
+      WHERE id = %1",
+        [1 => [$contribution['contact_id'], 'Positive']]
+      );
+    }
+    parent::tearDown();
+  }
 
-	protected function importMessage( TransactionMessage $message ) {
-		$payment_time = $message->get( 'date' );
-		exchange_rate_cache_set( 'USD', $payment_time, 1 );
-		$currency = $message->get( 'currency' );
-		if ( $currency !== 'USD' ) {
-			exchange_rate_cache_set( $currency, $payment_time, 3 );
-		}
-		$this->consumer->processMessage( $message->getBody() );
-		$contributions = wmf_civicrm_get_contributions_from_gateway_id(
-			$message->getGateway(),
-			$message->getGatewayTxnId()
-		);
-		$this->contributions[] = $contributions[0];
-		return $contributions;
-	}
+  protected function addContributionTracking($ctId) {
+    $this->ctIds[] = $ctId;
+    db_insert('contribution_tracking')
+      ->fields(['id' => $ctId])
+      ->execute();
+  }
 
-	public function testCreateDistinctContributions() {
-		civicrm_initialize();
-		$subscr_id = mt_rand();
-		$values = $this->processRecurringSignup( $subscr_id );
+  protected function importMessage(TransactionMessage $message) {
+    $payment_time = $message->get('date');
+    exchange_rate_cache_set('USD', $payment_time, 1);
+    $currency = $message->get('currency');
+    if ($currency !== 'USD') {
+      exchange_rate_cache_set($currency, $payment_time, 3);
+    }
+    $this->consumer->processMessage($message->getBody());
+    $contributions = wmf_civicrm_get_contributions_from_gateway_id(
+      $message->getGateway(),
+      $message->getGatewayTxnId()
+    );
+    $this->contributions[] = $contributions[0];
+    return $contributions;
+  }
 
-		$message = new RecurringPaymentMessage( $values );
-		$message2 = new RecurringPaymentMessage( $values );
+  public function testCreateDistinctContributions() {
+    civicrm_initialize();
+    $subscr_id = mt_rand();
+    $values = $this->processRecurringSignup($subscr_id);
 
-		$msg = $message->getBody();
-		$this->addContributionTracking( $msg['contribution_tracking_id'] );
+    $message = new RecurringPaymentMessage($values);
+    $message2 = new RecurringPaymentMessage($values);
 
-		$contributions = $this->importMessage( $message );
-		$ctRecord = db_select( 'contribution_tracking', 'ct' )
-			->fields( 'ct' )
-			->condition( 'id', $msg['contribution_tracking_id'], '=' )
-			->execute()
-			->fetchAssoc();
+    $msg = $message->getBody();
+    $this->addContributionTracking($msg['contribution_tracking_id']);
 
-		$this->assertEquals(
-			$contributions[0]['id'],
-			$ctRecord['contribution_id']
-		);
-		$contributions2 = $this->importMessage( $message2 );
+    $contributions = $this->importMessage($message);
+    $ctRecord = db_select('contribution_tracking', 'ct')
+      ->fields('ct')
+      ->condition('id', $msg['contribution_tracking_id'], '=')
+      ->execute()
+      ->fetchAssoc();
 
-		$ctRecord2 = db_select( 'contribution_tracking', 'ct' )
-			->fields( 'ct' )
-			->condition( 'id', $msg['contribution_tracking_id'], '=' )
-			->execute()
-			->fetchAssoc();
+    $this->assertEquals(
+      $contributions[0]['id'],
+      $ctRecord['contribution_id']
+    );
+    $contributions2 = $this->importMessage($message2);
 
-		// The ct_id record should still link to the first contribution
-		$this->assertEquals(
-			$contributions[0]['id'],
-			$ctRecord2['contribution_id']
-		);
-		$recur_record = wmf_civicrm_get_recur_record( $subscr_id );
+    $ctRecord2 = db_select('contribution_tracking', 'ct')
+      ->fields('ct')
+      ->condition('id', $msg['contribution_tracking_id'], '=')
+      ->execute()
+      ->fetchAssoc();
 
-		$this->assertNotEquals( false, $recur_record );
+    // The ct_id record should still link to the first contribution
+    $this->assertEquals(
+      $contributions[0]['id'],
+      $ctRecord2['contribution_id']
+    );
+    $recur_record = wmf_civicrm_get_recur_record($subscr_id);
 
-		$this->assertEquals( 1, count( $contributions ) );
-		$this->assertEquals( $recur_record->id, $contributions[0]['contribution_recur_id'] );
-		$this->assertEquals( 1, count( $contributions2 ) );
-		$this->assertEquals( $recur_record->id, $contributions2[0]['contribution_recur_id'] );
+    $this->assertNotEquals(FALSE, $recur_record);
 
-		$this->assertEquals( $contributions[0]['contact_id'], $contributions2[0]['contact_id'] );
-		$addresses = $this->callAPISuccess(
-			'Address',
-			'get',
-			array( 'contact_id' => $contributions2[0]['contact_id'] )
-		);
-		$this->assertEquals( 1, $addresses['count'] );
-		// The address comes from the recurring_payment.json not the recurring_signup.json as it
-		// has been overwritten. This is perhaps not a valid scenario in production but it is
-		// the scenario the code works to. In production they would probably always be the same.
-		$this->assertEquals( '1211122 132 st', $addresses['values'][$addresses['id']]['street_address'] );
+    $this->assertEquals(1, count($contributions));
+    $this->assertEquals($recur_record->id, $contributions[0]['contribution_recur_id']);
+    $this->assertEquals(1, count($contributions2));
+    $this->assertEquals($recur_record->id, $contributions2[0]['contribution_recur_id']);
 
-		$emails = $this->callAPISuccess( 'Email', 'get', array( 'contact_id' => $contributions2[0]['contact_id'] ) );
-		$this->assertEquals( 1, $addresses['count'] );
-		$this->assertEquals( 'test+fr@wikimedia.org', $emails['values'][$emails['id']]['email'] );
-	}
+    $this->assertEquals($contributions[0]['contact_id'], $contributions2[0]['contact_id']);
+    $addresses = $this->callAPISuccess(
+      'Address',
+      'get',
+      ['contact_id' => $contributions2[0]['contact_id']]
+    );
+    $this->assertEquals(1, $addresses['count']);
+    // The address comes from the recurring_payment.json not the recurring_signup.json as it
+    // has been overwritten. This is perhaps not a valid scenario in production but it is
+    // the scenario the code works to. In production they would probably always be the same.
+    $this->assertEquals('1211122 132 st', $addresses['values'][$addresses['id']]['street_address']);
 
-	public function testNormalizedMessages() {
-		civicrm_initialize();
-		$subscr_id = mt_rand();
-		$values = $this->processRecurringSignup( $subscr_id );
+    $emails = $this->callAPISuccess('Email', 'get', ['contact_id' => $contributions2[0]['contact_id']]);
+    $this->assertEquals(1, $addresses['count']);
+    $this->assertEquals('test+fr@wikimedia.org', $emails['values'][$emails['id']]['email']);
+  }
 
-		$message = new RecurringPaymentMessage( $values );
+  public function testNormalizedMessages() {
+    civicrm_initialize();
+    $subscr_id = mt_rand();
+    $values = $this->processRecurringSignup($subscr_id);
 
-		$this->addContributionTracking( $message->get( 'contribution_tracking_id' ) );
+    $message = new RecurringPaymentMessage($values);
 
-		$contributions = $this->importMessage( $message );
+    $this->addContributionTracking($message->get('contribution_tracking_id'));
 
-		$recur_record = wmf_civicrm_get_recur_record( $subscr_id );
-		$this->assertNotEquals( false, $recur_record );
+    $contributions = $this->importMessage($message);
 
-		$this->assertEquals( 1, count( $contributions ) );
-		$this->assertEquals( $recur_record->id, $contributions[0]['contribution_recur_id'] );
+    $recur_record = wmf_civicrm_get_recur_record($subscr_id);
+    $this->assertNotEquals(FALSE, $recur_record);
 
-		$addresses = $this->callAPISuccess(
-			'Address',
-			'get',
-			array( 'contact_id' => $contributions[0]['contact_id'] )
-		);
-		$this->assertEquals( 1, $addresses['count'] );
+    $this->assertEquals(1, count($contributions));
+    $this->assertEquals($recur_record->id, $contributions[0]['contribution_recur_id']);
 
-		$emails = $this->callAPISuccess( 'Email', 'get', array( 'contact_id' => $contributions[0]['contact_id'] ) );
-		$this->assertEquals( 1, $addresses['count'] );
-		$this->assertEquals( 'test+fr@wikimedia.org', $emails['values'][$emails['id']]['email'] );
-	}
+    $addresses = $this->callAPISuccess(
+      'Address',
+      'get',
+      ['contact_id' => $contributions[0]['contact_id']]
+    );
+    $this->assertEquals(1, $addresses['count']);
 
-	/**
-	 *  Test that a blank address is not written to the DB.
-	 */
-	public function testBlankEmail() {
-		civicrm_initialize();
-		$subscr_id = mt_rand();
-		$values = $this->processRecurringSignup( $subscr_id );
+    $emails = $this->callAPISuccess('Email', 'get', ['contact_id' => $contributions[0]['contact_id']]);
+    $this->assertEquals(1, $addresses['count']);
+    $this->assertEquals('test+fr@wikimedia.org', $emails['values'][$emails['id']]['email']);
+  }
 
-		$message = new RecurringPaymentMessage( $values );
-		$messageBody = $message->getBody();
+  /**
+   *  Test that a blank address is not written to the DB.
+   */
+  public function testBlankEmail() {
+    civicrm_initialize();
+    $subscr_id = mt_rand();
+    $values = $this->processRecurringSignup($subscr_id);
 
-		$addressFields = array( 'city', 'country', 'state_province', 'street_address', 'postal_code' );
-		foreach ( $addressFields as $addressField ) {
-			$messageBody[$addressField] = '';
-		}
+    $message = new RecurringPaymentMessage($values);
+    $messageBody = $message->getBody();
 
-		$this->addContributionTracking( $messageBody['contribution_tracking_id'] );
+    $addressFields = [
+      'city',
+      'country',
+      'state_province',
+      'street_address',
+      'postal_code',
+    ];
+    foreach ($addressFields as $addressField) {
+      $messageBody[$addressField] = '';
+    }
 
-		$this->consumer->processMessage( $messageBody );
+    $this->addContributionTracking($messageBody['contribution_tracking_id']);
 
-		$contributions = wmf_civicrm_get_contributions_from_gateway_id(
-			$message->getGateway(),
-			$message->getGatewayTxnId()
-		);
-		$this->contributions[] = $contributions[0];
-		$addresses = $this->callAPISuccess(
-			'Address',
-			'get',
-			array( 'contact_id' => $contributions[0]['contact_id'], 'sequential' => 1 )
-		);
-		$this->assertEquals( 1, $addresses['count'] );
-		// The address created by the sign up (Lockwood Rd) should not have been overwritten by the blank.
-		$this->assertEquals( '5109 Lockwood Rd', $addresses['values'][0]['street_address'] );
-	}
+    $this->consumer->processMessage($messageBody);
 
-	/**
-	 * @expectedException WmfException
-	 * @expectedExceptionCode WmfException::MISSING_PREDECESSOR
-	 */
-	public function testMissingPredecessor() {
-		$message = new RecurringPaymentMessage(
-			array(
-				'subscr_id' => mt_rand(),
+    $contributions = wmf_civicrm_get_contributions_from_gateway_id(
+      $message->getGateway(),
+      $message->getGatewayTxnId()
+    );
+    $this->contributions[] = $contributions[0];
+    $addresses = $this->callAPISuccess(
+      'Address',
+      'get',
+      ['contact_id' => $contributions[0]['contact_id'], 'sequential' => 1]
+    );
+    $this->assertEquals(1, $addresses['count']);
+    // The address created by the sign up (Lockwood Rd) should not have been overwritten by the blank.
+    $this->assertEquals('5109 Lockwood Rd', $addresses['values'][0]['street_address']);
+  }
+
+  /**
+   * @expectedException WmfException
+   * @expectedExceptionCode WmfException::MISSING_PREDECESSOR
+   */
+  public function testMissingPredecessor() {
+    $message = new RecurringPaymentMessage(
+      [
+        'subscr_id' => mt_rand(),
         'email' => 'notinthedb@example.com',
-			)
-		);
+      ]
+    );
 
-		$this->importMessage( $message );
-	}
+    $this->importMessage($message);
+  }
 
   /**
    * Deal with a bad situation caused by PayPal's botched subscr_id migration.
@@ -225,33 +233,33 @@ class RecurringQueueTest extends BaseWmfDrupalPhpUnitTestCase {
     $email = 'test_recur_' . mt_rand() . '@example.org';
     // Set up an old-style PayPal recurring subscription with S-XXXX subscr_id
     $subscr_id = 'S-' . mt_rand();
-    $values = $this->processRecurringSignup( $subscr_id, [
+    $values = $this->processRecurringSignup($subscr_id, [
       'gateway' => 'paypal',
-      'email' => $email
-    ] );
+      'email' => $email,
+    ]);
 
     // Import an initial payment with consistent gateway and subscr_id
     $values['email'] = $email;
     $values['gateway'] = 'paypal';
-    $oldStyleMessage = new RecurringPaymentMessage( $values );
-    $this->addContributionTracking( $oldStyleMessage->get( 'contribution_tracking_id' ) );
-    $this->importMessage( $oldStyleMessage );
+    $oldStyleMessage = new RecurringPaymentMessage($values);
+    $this->addContributionTracking($oldStyleMessage->get('contribution_tracking_id'));
+    $this->importMessage($oldStyleMessage);
 
     // New payment comes in with subscr ID format that we associate
     // with paypal_ec, so we mis-tag the gateway.
     $new_subscr_id = 'I-' . mt_rand();
     $values['subscr_id'] = $new_subscr_id;
     $values['gateway'] = 'paypal_ec';
-    $newStyleMessage = new RecurringPaymentMessage( $values );
+    $newStyleMessage = new RecurringPaymentMessage($values);
 
-    $this->consumer->processMessage( $newStyleMessage->getBody() );
+    $this->consumer->processMessage($newStyleMessage->getBody());
     // It should be imported as a paypal donation, not paypal_ec
     $contributions = wmf_civicrm_get_contributions_from_gateway_id(
       'paypal',
       $newStyleMessage->getGatewayTxnId()
     );
     // New record should have created a new contribution
-    $this->assertEquals( 1, count( $contributions ) );
+    $this->assertEquals(1, count($contributions));
 
     // Add the contribution to our tearDown array, since we didn't go through
     // $this->importMessage for this one.
@@ -264,42 +272,43 @@ class RecurringQueueTest extends BaseWmfDrupalPhpUnitTestCase {
       INNER JOIN civicrm_email e on ccr.contact_id = e.contact_id
       WHERE e.email = '$email'
     "));
-    $this->assertEquals( 1, count( $recur_records ) );
+    $this->assertEquals(1, count($recur_records));
 
     // ...and it should be associated with the contribution
-    $this->assertEquals( $recur_records[0]['id'], $contributions[0]['contribution_recur_id'] );
+    $this->assertEquals($recur_records[0]['id'], $contributions[0]['contribution_recur_id']);
 
     // Finally, we should have stuck the new ID in the processor_id field
-    $this->assertEquals( $new_subscr_id, $recur_records[0]['processor_id']);
+    $this->assertEquals($new_subscr_id, $recur_records[0]['processor_id']);
   }
 
-	/**
-	 * @expectedException WmfException
-	 * @expectedExceptionCode WmfException::INVALID_RECURRING
-	 */
-	public function testNoSubscrId() {
-		$message = new RecurringPaymentMessage(
-			array(
-				'subscr_id' => null,
-			)
-		);
+  /**
+   * @expectedException WmfException
+   * @expectedExceptionCode WmfException::INVALID_RECURRING
+   */
+  public function testNoSubscrId() {
+    $message = new RecurringPaymentMessage(
+      [
+        'subscr_id' => NULL,
+      ]
+    );
 
-		$this->importMessage( $message );
-	}
+    $this->importMessage($message);
+  }
 
-	/**
-	 * Process the original recurring sign up message.
-	 *
-	 * @param string $subscr_id
-	 * @return array
-	 */
-	private function processRecurringSignup( $subscr_id, $overrides = [] ) {
-		$values = $overrides + array( 'subscr_id' => $subscr_id );
-		$signup_message = new RecurringSignupMessage( $values );
-		$subscr_time = $signup_message->get( 'date' );
-		exchange_rate_cache_set( 'USD', $subscr_time, 1 );
-		exchange_rate_cache_set( $signup_message->get( 'currency' ), $subscr_time, 2 );
-		$this->consumer->processMessage( $signup_message->getBody() );
-		return $values;
-	}
+  /**
+   * Process the original recurring sign up message.
+   *
+   * @param string $subscr_id
+   *
+   * @return array
+   */
+  private function processRecurringSignup($subscr_id, $overrides = []) {
+    $values = $overrides + ['subscr_id' => $subscr_id];
+    $signup_message = new RecurringSignupMessage($values);
+    $subscr_time = $signup_message->get('date');
+    exchange_rate_cache_set('USD', $subscr_time, 1);
+    exchange_rate_cache_set($signup_message->get('currency'), $subscr_time, 2);
+    $this->consumer->processMessage($signup_message->getBody());
+    return $values;
+  }
 }
