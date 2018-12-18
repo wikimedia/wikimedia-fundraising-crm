1ab451613dfd5c6d237804d6695c945152aef987
eileen <emcnaughton@wikimedia.org>
2018-12-18 14:24:01 +1300

    Update Civi thank you letter (offline_receipt_message) for Endowment
    Bug: T211797
    Change-Id: I37aa79a15cc0c7de875d3a8ccdce00ed41a27ca7

--- sites/all/modules/wmf_civicrm/update_receipt.php
+++ sites/all/modules/wmf_civicrm/update_receipt.php
@@ -7,10 +7,12 @@ function _wmf_civicrm_update_offline_receipt() {
     civicrm_initialize();
     $msg_html = file_get_contents(__DIR__ . '/templates/offline_receipt.html');
     $msg_text = file_get_contents(__DIR__ . '/templates/offline_receipt.txt');
+    $msg_subject = file_get_contents(__DIR__ . '/templates/offline_receipt_subject.txt');
     CRM_Core_DAO::executeQuery("
   UPDATE civicrm_msg_template
   SET msg_html = '$msg_html'
   , msg_text = '$msg_text'
+  , msg_subject = '$msg_subject'
   WHERE msg_title = 'Contributions - Receipt (off-line)' AND is_default = 1
 
   ");
