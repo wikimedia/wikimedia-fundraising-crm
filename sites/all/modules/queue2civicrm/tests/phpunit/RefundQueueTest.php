99373dc574cb5f552683d2d913ce86f49dad73df
eileen <emcnaughton@wikimedia.org>
2019-02-07 11:09:00 +1300

    Apply drupal formatting to RefundQueueTest class
    Change-Id: I524b2749c72e00a1ee103daff9aecf69953a20b0

--- sites/all/modules/queue2civicrm/tests/phpunit/RefundQueueTest.php
+++ sites/all/modules/queue2civicrm/tests/phpunit/RefundQueueTest.php
@@ -1,4 +1,5 @@
 <?php
+
 use queue2civicrm\refund\RefundQueueConsumer;
 
 /**
@@ -6,104 +7,105 @@ use queue2civicrm\refund\RefundQueueConsumer;
  */
 class RefundQueueTest extends BaseWmfDrupalPhpUnitTestCase {
 
-	/**
-	 * @var RefundQueueConsumer
-	 */
-	protected $consumer;
+  /**
+   * @var RefundQueueConsumer
+   */
+  protected $consumer;
 
-	public function setUp() {
-		parent::setUp();
-		$this->consumer = new RefundQueueConsumer(
-			'refund'
-		);
-	}
+  public function setUp() {
+    parent::setUp();
+    $this->consumer = new RefundQueueConsumer(
+      'refund'
+    );
+  }
 
-	public function testRefund() {
-		$donation_message = new TransactionMessage();
-		$refund_message = new RefundMessage(
-			array(
-				'gateway' => $donation_message->getGateway(),
-				'gateway_parent_id' => $donation_message->getGatewayTxnId(),
-				'gateway_refund_id' => mt_rand(),
-				'gross' => $donation_message->get( 'original_gross' ),
-				'gross_currency' => $donation_message->get( 'original_currency' ),
-			)
-		);
+  public function testRefund() {
+    $donation_message = new TransactionMessage();
+    $refund_message = new RefundMessage(
+      [
+        'gateway' => $donation_message->getGateway(),
+        'gateway_parent_id' => $donation_message->getGatewayTxnId(),
+        'gateway_refund_id' => mt_rand(),
+        'gross' => $donation_message->get('original_gross'),
+        'gross_currency' => $donation_message->get('original_currency'),
+      ]
+    );
 
-		exchange_rate_cache_set( 'USD', $donation_message->get( 'date' ), 1 );
-		exchange_rate_cache_set( $donation_message->get( 'currency' ), $donation_message->get( 'date' ), 3 );
+    exchange_rate_cache_set('USD', $donation_message->get('date'), 1);
+    exchange_rate_cache_set($donation_message->get('currency'), $donation_message->get('date'), 3);
 
-		$message_body = $donation_message->getBody();
-		wmf_civicrm_contribution_message_import( $message_body );
-		$contributions = wmf_civicrm_get_contributions_from_gateway_id(
-			$donation_message->getGateway(),
-			$donation_message->getGatewayTxnId()
-		);
-		$this->assertEquals( 1, count( $contributions ) );
+    $message_body = $donation_message->getBody();
+    wmf_civicrm_contribution_message_import($message_body);
+    $contributions = wmf_civicrm_get_contributions_from_gateway_id(
+      $donation_message->getGateway(),
+      $donation_message->getGatewayTxnId()
+    );
+    $this->assertEquals(1, count($contributions));
 
-		$this->consumer->processMessage( $refund_message->getBody() );
-		$contributions = wmf_civicrm_get_contributions_from_gateway_id(
-			$refund_message->getGateway(),
-			$refund_message->getGatewayTxnId()
-		);
-		$this->assertEquals( 1, count( $contributions ) );
-	}
+    $this->consumer->processMessage($refund_message->getBody());
+    $contributions = wmf_civicrm_get_contributions_from_gateway_id(
+      $refund_message->getGateway(),
+      $refund_message->getGatewayTxnId()
+    );
+    $this->assertEquals(1, count($contributions));
+  }
 
-	/**
-	 * @expectedException WmfException
-	 * @expectedExceptionCode WmfException::MISSING_PREDECESSOR
-	 */
-	public function testRefundNoPredecessor() {
-		$refund_message = new RefundMessage();
+  /**
+   * @expectedException WmfException
+   * @expectedExceptionCode WmfException::MISSING_PREDECESSOR
+   */
+  public function testRefundNoPredecessor() {
+    $refund_message = new RefundMessage();
 
-		$this->consumer->processMessage( $refund_message->getBody() );
-	}
+    $this->consumer->processMessage($refund_message->getBody());
+  }
 
-	/**
-	 * Test refunding a mismatched amount.
-	 *
-	 * Note that we were checking against an exception - but it turned out the exception
-	 * could be thrown in this fn $this->queueConsumer->processMessage if the exchange rate does not
-	 * exist - which is not what we are testing for.
-	 */
-	public function testRefundMismatched() {
-		$this->setExchangeRates( 1234567, array( 'USD' => 1, 'PLN' => 0.5 ) );
-		$donation_message = new TransactionMessage(
-			array(
-				'gateway' => 'test_gateway',
-				'gateway_txn_id' => mt_rand(),
-			)
-		);
-		$refund_message = new RefundMessage(
-			array(
-				'gateway' => 'test_gateway',
-				'gateway_parent_id' => $donation_message->getGatewayTxnId(),
-				'gateway_refund_id' => mt_rand(),
-				'gross' => $donation_message->get( 'original_gross' ) + 1,
-				'gross_currency' => $donation_message->get( 'original_currency' ),
-			)
-		);
+  /**
+   * Test refunding a mismatched amount.
+   *
+   * Note that we were checking against an exception - but it turned out the
+   * exception could be thrown in this fn $this->queueConsumer->processMessage
+   * if the exchange rate does not exist - which is not what we are testing
+   * for.
+   */
+  public function testRefundMismatched() {
+    $this->setExchangeRates(1234567, ['USD' => 1, 'PLN' => 0.5]);
+    $donation_message = new TransactionMessage(
+      [
+        'gateway' => 'test_gateway',
+        'gateway_txn_id' => mt_rand(),
+      ]
+    );
+    $refund_message = new RefundMessage(
+      [
+        'gateway' => 'test_gateway',
+        'gateway_parent_id' => $donation_message->getGatewayTxnId(),
+        'gateway_refund_id' => mt_rand(),
+        'gross' => $donation_message->get('original_gross') + 1,
+        'gross_currency' => $donation_message->get('original_currency'),
+      ]
+    );
 
-		$message_body = $donation_message->getBody();
-		wmf_civicrm_contribution_message_import( $message_body );
-		$contributions = wmf_civicrm_get_contributions_from_gateway_id(
-			$donation_message->getGateway(),
-			$donation_message->getGatewayTxnId()
-		);
-		$this->assertEquals( 1, count( $contributions ) );
+    $message_body = $donation_message->getBody();
+    wmf_civicrm_contribution_message_import($message_body);
+    $contributions = wmf_civicrm_get_contributions_from_gateway_id(
+      $donation_message->getGateway(),
+      $donation_message->getGatewayTxnId()
+    );
+    $this->assertEquals(1, count($contributions));
 
-		$this->consumer->processMessage( $refund_message->getBody() );
-		$contributions = $this->callAPISuccess(
-			'Contribution',
-			'get',
-			array( 'contact_id' => $contributions[0]['contact_id'], 'sequential' => 1 )
-		);
-		$this->assertEquals( 2, count( $contributions['values'] ) );
-		$this->assertEquals(
-			'Chargeback',
-			CRM_Contribute_PseudoConstant::contributionStatus( $contributions['values'][0]['contribution_status_id'] )
-		);
-		$this->assertEquals( '-.5', $contributions['values'][1]['total_amount'] );
-	}
+    $this->consumer->processMessage($refund_message->getBody());
+    $contributions = $this->callAPISuccess(
+      'Contribution',
+      'get',
+      ['contact_id' => $contributions[0]['contact_id'], 'sequential' => 1]
+    );
+    $this->assertEquals(2, count($contributions['values']));
+    $this->assertEquals(
+      'Chargeback',
+      CRM_Contribute_PseudoConstant::contributionStatus($contributions['values'][0]['contribution_status_id'])
+    );
+    $this->assertEquals('-.5', $contributions['values'][1]['total_amount']);
+  }
 
 }
