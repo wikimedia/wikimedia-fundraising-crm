adc2b4e3f35f5f60712e471f0bb8aa53ca2b26a4
eileen <emcnaughton@wikimedia.org>
2019-03-13 15:42:27 +1300

    Redo merges on contacts who clashed during merge with inappropriate data outcomes
    We had an issue a while back where 2 dedupe jobs collided. Under certain circumstances the rollback would leave
    data on the contact who was deleted.
    
    This update restores the contact and then remerges it - taking the data over this time.
    
    Note to get the tests to work I had to do a civi fix so this is dependent on that
    
    I ran this on failing & despite the scathing output I think the results were pretty good
    
    Massive fail dude - contacts are20560841,14436684,19363834,10058954,11902286,677507,4166193,9211964,29930598,3640571,10633509,11002009,9668591,6680931,3634198,3702891,7560321,7144044,61,2221100,2311619,1527003,13013227,6548611,2186670,768859,2311609,11399477,623831,4242664,2231364,2231369,1582787,4970326,6304870,2309680,8072059,14644918,823826,5525861,15037455,2311630,2311599,2309457,7779587,13567007,2925067,2246932,2128624,12240324,4190267,30991259,1112492,1046062,18325443,721490,6880091
    
    I did a spot check & the ones I found were older or odd whereas I found a lot where is had worked well
    
    only 4 were within the last 2 years - so they might be worth manually checking
    
    Bug: T218061
    
    Change-Id: I6f4dfacd987991f54e69338dc1704d2f553585ff

---
+++ sites/default/civicrm/extensions/org.wikimedia.dedupetools/api/v3/Merge/Redo.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Merge redo spec
+ *
+ * @param array $spec description of fields supported by this API call
+ *
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
+function _civicrm_api3_merge_redo_spec(&$spec) {
+  $spec['contact_id']['api.required'] = 1;
+}
+
+/**
+ * Redo merge.
+ *
+ * This function undeletes a merged contact and remerges it. This could be used to
+ * fix a merge where a deadlock caused some data to be left behind.
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @throws API_Exception
+ */
+function civicrm_api3_merge_redo($params) {
+  $newContact = civicrm_api3('Contact', 'getmergedto', [
+    'contact_id' => $params['contact_id'],
+    'sequential' => 1,
+  ]);
+  if ($newContact['count'] == 0) {
+    throw new API_Exception(ts('Contact not re-mergeable'));
+  }
+  civicrm_api3('Contact', 'create', ['id' => $params['contact_id'], 'is_deleted' => 0]);
+  return civicrm_api3('Contact', 'merge', ['to_remove_id' => $params['contact_id'], 'to_keep_id' => $newContact['id']]);
+}
---
+++ sites/default/civicrm/extensions/org.wikimedia.dedupetools/tests/phpunit/CRM/DedupeTools/Merge/RedoTest.php
@@ -0,0 +1,84 @@
+<?php
+
+use CRM_Dedupetools_ExtensionUtil as E;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+use Civi\Test\Api3TestTrait;
+
+/**
+ * FIXME - Add test description.
+ *
+ * Tips:
+ *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
+ *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
+ *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
+ *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
+ *    If this test needs to manipulate schema or truncate tables, then either:
+ *       a. Do all that using setupHeadless() and Civi\Test.
+ *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ *
+ * @group headless
+ */
+class CRM_DedupeTools_Merge_RedoTest extends \PHPUnit_Framework_TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+
+  use Api3TestTrait;
+
+  protected $ids = [];
+
+  public function setUpHeadless() {
+    // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+    // See: https://docs.civicrm.org/dev/en/latest/testing/phpunit/#civitest
+    return \Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+  public function setUp() {
+    parent::setUp();
+    civicrm_initialize();
+  }
+
+  public function tearDown() {
+    foreach ($this->ids as $entity => $ids) {
+      foreach ($ids as $id) {
+        if ($entity === 'contact') {
+          foreach ($this->callAPISuccess('Contribution', 'get', ['contact_id' => $id])['values'] as $contribution) {
+            civicrm_api3('Contribution', 'delete', ['id' => $contribution['id']]);
+          }
+        }
+        civicrm_api3($entity, 'delete', ['id' => $id, 'skip_undelete' => TRUE]);
+      }
+    }
+    parent::tearDown();
+  }
+
+  /**
+   * Test redo-ing a merge.
+   *
+   * Steps
+   *  - merge 2 contacts
+   *  - add a contribution to the deleted by merge contact
+   *  - call redo merge
+   *  - check the contribution has been transferred
+   *  - check the deleted contact is still deleted.
+   * (note getsingle is an implict 'assert').
+   */
+  public function testRedoMerge() {
+    $contactParams = [
+      'contact_type' => 'Individual',
+      'first_name' => 'Wonder',
+      'last_name' => 'Woman',
+      'email' => 'wonderwoman@example.org',
+    ];
+    $contactToBeMergedID = $this->ids['contact'][] = $this->callAPISuccess('Contact', 'create', $contactParams)['id'];
+    $contactToKeepID = $this->ids['contact'][] = $this->callAPISuccess('Contact', 'create', $contactParams)['id'];
+    $this->callAPISuccess('Contact', 'merge', ['to_keep_id' => $contactToKeepID, 'to_remove_id' => $contactToBeMergedID]);
+    $this->callAPISuccess('Contribution', 'create', ['contact_id' => $contactToBeMergedID, 'total_amount' => 10, 'financial_type_id' => 'Donation']);
+
+    $this->callAPISuccess('Merge', 'redo', ['contact_id' => $contactToBeMergedID]);
+    $this->callAPISuccessGetSingle('Contribution', ['contact_id' => $contactToKeepID]);
+    $this->callAPISuccessGetSingle('Contact', ['is_deleted' => 1, 'id' => $contactToBeMergedID]);
+  }
+
+}
