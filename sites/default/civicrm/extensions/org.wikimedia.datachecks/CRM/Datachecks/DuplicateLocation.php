ee8e35232f381083425e2f3a341404b34b956c71
eileen <emcnaughton@wikimedia.org>
2019-03-21 18:48:53 +1300

    Update Datacheck extension to fix duplicate location types.
    the way to run this is
    
    drush cvapi Data.fix check=DuplicateLocation
    
    We have some thousands of rows to repair - as can be checked by running
    
    drush cvapi Data.check check=DuplicateLocation
    
    Once this has run we should look at
    1) fixing any with no location_type_id
    2) removing our core patch to cope with this data discrepancy
    3) figuring out a way to schedule data discrepancy checks to run & email us
    when some arise - this might include figuring out how to do the same with the core system checks
    & getting some control over them- currently they are hacked into oblivion due to our inability to control them
    (which may be mitigated now anyway by figuring out how to stop it trying to pull dashboard data)
    4) probably also run the Primary fix too - it's showing a few lines also.
    
    Bug: T152474
    Change-Id: Ic4101b3c3e4a7757a161ab033550279dc40f24ae

--- sites/default/civicrm/extensions/org.wikimedia.datachecks/CRM/Datachecks/DuplicateLocation.php
+++ sites/default/civicrm/extensions/org.wikimedia.datachecks/CRM/Datachecks/DuplicateLocation.php
@@ -12,13 +12,13 @@ class CRM_Datachecks_DuplicateLocation {
    *
    * @var string
    */
-  protected $temporaryTable = 'civicrm_temp_duplicate_location';
+  protected $temporaryTable;
   /**
    * List of entities to check.
    *
    * @var array
    */
-  protected $entities = array('email', 'phone', 'address', 'im');
+  protected $entities = ['email', 'phone', 'address', 'im'];
 
   /**
    * Do data integrity check on primary locations.
@@ -40,13 +40,13 @@ class CRM_Datachecks_DuplicateLocation {
           "SELECT contact_id
           FROM $this->temporaryTable e
           LEFT JOIN civicrm_contact c ON e.contact_id = c.id
-          WHERE c.is_deleted = 0 LIMIT 5
+          WHERE c.is_deleted = 0 ORDER BY id DESC LIMIT 5
         ");
 
         while ($exampleContactsDAO->fetch()) {
           $examples[] = $exampleContactsDAO->contact_id;
         }
-        // It would be interesting to use the api v3 style arrayobject return here.
+        // It would be interesting to use the api v4 style arrayobject return here.
         $result[$entity] = array(
           'message' => ts('%1 contact/s have multiple %2 with the same location type', array($count, $entity)),
           'example' => array('contact' => $examples),
@@ -60,9 +60,6 @@ class CRM_Datachecks_DuplicateLocation {
   /**
    * Create a temporary table of all results.
    *
-   * Filtering this against deleted contacts is more efficient on dbs
-   * with lots of deleted contacts than a single query.
-   *
    * This query is much slower if you try to include location_type_id in
    * the group by.
    *
@@ -71,17 +68,17 @@ class CRM_Datachecks_DuplicateLocation {
    * @return string
    */
   protected function createTemporaryTable($entity) {
-
-    CRM_Core_DAO::executeQuery("
-      CREATE TEMPORARY TABLE {$this->temporaryTable}(
+    $phoneClause = ($entity === 'phone') ? ', phone_type_id' : '';
+    // This query basically says - is the count of rows for this contact greater than the count of
+    // different location types? For phones it compares to the count of location type / phone combos.
+    $this->temporaryTable = CRM_Utils_SQL_TempTable::build()->createWithQuery("
       SELECT contact_id,
-      IF(count(location_type_id) > COUNT(DISTINCT location_type_id), 1, 0) as is_duplicate
+      IF(count(*) > COUNT(DISTINCT location_type_id $phoneClause), 1, 0) as is_duplicate
       FROM civicrm_{$entity}
       WHERE contact_id IS NOT NULL
       GROUP BY contact_id
-      HAVING is_duplicate  = 1
-      )"
-    );
+      HAVING is_duplicate  = 1"
+    )->getName();
     CRM_Core_DAO::executeQuery("
       ALTER TABLE {$this->temporaryTable}
       ADD INDEX contact_id(contact_id)"
@@ -99,38 +96,151 @@ class CRM_Datachecks_DuplicateLocation {
   }
 
   /**
-   * Fix the lack of primaries.
+   * Fix the duplicates.
    *
-   * Use a temporary table as a straight update could fail if logging it on
-   * due to logic loop.
+   * If we have an exact match we will delete the duplicates. If not we will change the
+   * location type of the second one to 'Other'
    *
    * @param array $options
    */
   public function fix($options) {
-    // @todo unfinished.
-    return;
-    /**
     foreach ($this->entities as $entity) {
-      CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS civicrm_location_updates");
       $this->createTemporaryTable($entity);
-
-      if (!empty($options['delete_exact_duplicates']) && $entity == 'email') {
-        // @todo figure out how to deal with other entities.
-        CRM_Core_DAO::executeQuery(
-          "DELETE l, e2 FROM civicrm_location_updates l
-           LEFT JOIN civicrm_{$entity} e
-           ON e.id = l.id
-           INNER JOIN civicrm_email e2 ON e2.email = e.email
-           AND e.contact_id = e2.contact_id
-           // If
-           AND (e.on_hold = 1 OR e2.is_oh_hold = 0)
-           AND (e.is_bulkmail = 1 OR e2.is_bulkmail = 0)
-           AND e2.id >e.id
-           AND e.is_primary = 1
-        "
-        );
+      $result = CRM_Core_DAO::executeQuery('Select * FROM ' . $this->temporaryTable);
+      while ($result->fetch()) {
+        $data = civicrm_api3($entity, 'get', [
+          'contact_id' => $result->contact_id,
+          ['options' => ['sort' => 'is_primary']]
+        ])['values'];
+        $byLocation = [];
+        foreach ($data as $index => $instance) {
+          $byLocation[CRM_Utils_Array::value('location_type_id', $instance)][] = $instance;
+        }
+        foreach ($byLocation as $locationTypeID => $entitiesArray) {
+          if ($entity === 'phone') {
+            $this->filterOutPhonesWhereTypeIsDifferentButLocationISame($entitiesArray);
+          }
+          if (count($entitiesArray) === 1) {
+            continue;
+          }
+          // Here we have 2 contacts with the same location type. Scenarios are
+          // 1 they match - we delete one
+          // 2 they don't match - we change the type on one
+          foreach ($entitiesArray as $index => $thisEntity) {
+            // Compare the entity with the next one in the array to see it is the same, if it is
+            // then delete it, otherwise update it.
+            if (empty($entitiesArray[$index + 1])) {
+              continue;
+            }
+            else {
+              $nextEntity = $entitiesArray[$index + 1];
+              if ($this->isDataMatch($thisEntity, $nextEntity)) {
+                civicrm_api3($entity, 'delete', ['id' => $thisEntity['id']]);
+              }
+              else {
+                $newLocationType = $this->getBestLocationType($byLocation);
+                // Yay - we found a 'free type'
+                $byLocation[$newLocationType] = $thisEntity;
+                civicrm_api3($entity, 'create', [
+                  'id' => $thisEntity['id'],
+                  'location_type_id' => $newLocationType
+                ]);
+              }
+            }
+          }
+        }
       }
     }
-    **/
+  }
+
+  /**
+   * @param $byLocation
+   *
+   * @return int
+   */
+  protected function getBestLocationType($byLocation) {
+    static $locationTypes = NULL;
+    if (!$locationTypes) {
+      $locationTypes = $this->getLocationTypes();
+    }
+
+    foreach ($locationTypes as $locationType) {
+      if (!isset($byLocation[$locationType])) {
+        return $locationType;
+      }
+    }
+  }
+
+  protected function isDataMatch($entity1, $entity2) {
+    return ($this->getFilteredEntity($entity1) === $this->getFilteredEntity($entity2));
+  }
+
+  /**
+   * Get the entity with only the compariable fields.
+   *
+   * @param array $entity
+   *
+   * @return array
+   */
+  protected function getFilteredEntity($entity) {
+    return array_intersect_key(array_filter($entity), array_fill_keys([
+      'street_address',
+      'name',
+      'supplemental_address_1',
+      'supplemental_address_2',
+      'supplemental_address_3',
+      'city',
+      'postal_code',
+      'postal_code_suffix',
+      'geo_code_1',
+      'geo_code_2',
+      'state_province',
+      'country_id',
+      'county_id',
+      'phone',
+      'phone_ext',
+      'phone_numeric',
+      'mobile_provider_id',
+      'phone_type_id',
+      'email',
+      'signature_text',
+      'signature_html',
+      'on_hold',
+      'is_bulkmail',
+      'provider_id',
+    ], 1));
+  }
+
+  /**
+   * Get the available location types with a light re-order to make our preferences for reassignment (in order)
+   *  - Other
+   *  - Main
+   *  - Home
+   *  - Mailing
+   *  - Billing
+   */
+  protected function getLocationTypes() {
+    $locationTypes = civicrm_api3('Address', 'getoptions', ['field' => 'location_type_id'])['values'];
+    $preferredOrder = array_intersect_key(['Other' => 0, 'Main' => 1, 'Home' => 2, 'Mailing' => 3, 'Billing' => 4], array_flip($locationTypes));
+    return array_merge($preferredOrder, array_flip($locationTypes));
+  }
+
+  /**
+   * Filter out phones that are not true duplicates as they have different types.
+   *
+   * @param $entitiesArray
+   */
+  protected function filterOutPhonesWhereTypeIsDifferentButLocationISame(&$entitiesArray) {
+
+    $phonesByTypeArray = [];
+    foreach ($entitiesArray as $index => $entityArray) {
+      $phonesByTypeArray[$entityArray['phone_type_id']][$index] = TRUE;
+    }
+    foreach ($phonesByTypeArray as $phonesByType) {
+      if (count($phonesByType) === 1) {
+        // This one is actually unique when you take type into account so let it go... let it go..
+        unset($entitiesArray[key($phonesByType)]);
+      }
+    }
   }
 }
