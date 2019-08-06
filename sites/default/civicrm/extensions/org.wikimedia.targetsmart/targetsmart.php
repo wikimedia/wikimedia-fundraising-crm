1385fb6b19f90a5fd3a62add3309a9a8b2773f7f
eileenmcnaughton <emcnaughton@wikimedia.org>
2019-08-07 11:27:55 +1200

    Add targetsmart extension
    This is intended to be used for the batch script but at the moment it is just a shell with
    an upgrader function that creates the field mappings.
    
    My intent is to do some tests through the UI (first on staging & then on live) with the mapping &
    see how it looks & get MG to do some looking too & then start of the batch side.
    
    Bonus - they will get a handful imported to nosy at in the meantime.
    
    Bug: T228712
    
    Change-Id: Ibeeeee8e8c40f0e365ee8786406629ed2a61db6b

---
+++ sites/default/civicrm/extensions/org.wikimedia.targetsmart/targetsmart.php
@@ -0,0 +1,170 @@
+<?php
+
+require_once 'targetsmart.civix.php';
+use CRM_Targetsmart_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/ 
+ */
+function targetsmart_civicrm_config(&$config) {
+  _targetsmart_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function targetsmart_civicrm_xmlMenu(&$files) {
+  _targetsmart_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function targetsmart_civicrm_install() {
+  _targetsmart_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function targetsmart_civicrm_postInstall() {
+  _targetsmart_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function targetsmart_civicrm_uninstall() {
+  _targetsmart_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function targetsmart_civicrm_enable() {
+  _targetsmart_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function targetsmart_civicrm_disable() {
+  _targetsmart_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function targetsmart_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _targetsmart_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function targetsmart_civicrm_managed(&$entities) {
+  _targetsmart_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function targetsmart_civicrm_caseTypes(&$caseTypes) {
+  _targetsmart_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function targetsmart_civicrm_angularModules(&$angularModules) {
+  _targetsmart_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function targetsmart_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _targetsmart_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function targetsmart_civicrm_entityTypes(&$entityTypes) {
+  _targetsmart_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_thems().
+ */
+function targetsmart_civicrm_themes(&$themes) {
+  _targetsmart_civix_civicrm_themes($themes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ *
+function targetsmart_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ *
+function targetsmart_civicrm_navigationMenu(&$menu) {
+  _targetsmart_civix_insert_navigation_menu($menu, 'Mailings', array(
+    'label' => E::ts('New subliminal message'),
+    'name' => 'mailing_subliminal_message',
+    'url' => 'civicrm/mailing/subliminal',
+    'permission' => 'access CiviMail',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _targetsmart_civix_navigationMenu($menu);
+} // */
