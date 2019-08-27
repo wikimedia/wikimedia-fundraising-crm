1a5c33405fb87b23fd43e4159554169c5c332f9d
eileenmcnaughton <emcnaughton@wikimedia.org>
2019-08-28 11:52:57 +1200

    Update afform
    Makes sense to grab latest before fixing up deduper to cope with upgrade.
    
    Deduper needs fixing as I upstreamed the things I developed for it but not all hit
    core exactly the same
    
    Bug: T228826
    
    Change-Id: I0a4a1545fc4ee357aef1f00f3711a3554eb791d6

--- sites/default/civicrm/extensions/afform/core/ang/afformCore.js
+++ sites/default/civicrm/extensions/afform/core/ang/afformCore.js
@@ -1,22 +1,23 @@
 (function(angular, $, _) {
   // Declare a list of dependencies.
-  angular.module('afformCore', [
-    'crmUi', 'crmUtil', 'ngRoute', 'api4'
-  ]);
+  angular.module('afformCore', CRM.angRequires('afformCore'));
 
   // Use `afformCoreDirective(string name)` to generate an AngularJS directive.
-  angular.module('afformCore').service('afformCoreDirective', function($routeParams, crmApi4, crmStatus, crmUiAlert){
-    return function(camelName, meta, d){
+  angular.module('afformCore').service('afformCoreDirective', function($routeParams, crmApi4, crmStatus, crmUiAlert) {
+    return function(camelName, meta, d) {
       d.restrict = 'AE';
       d.scope = {};
       d.scope.options = '=' + camelName;
-      d.link = function($scope, $el, $attr) {
-        $scope.ts = CRM.ts(camelName);
-        $scope.routeParams = $routeParams;
-        $scope.meta = meta;
-        $scope.crmApi4 = crmApi4;
-        $scope.crmStatus = crmStatus;
-        $scope.crmUiAlert = crmUiAlert;
+      d.link = {
+        pre: function($scope, $el, $attr) {
+          $scope.ts = CRM.ts(camelName);
+          $scope.routeParams = $routeParams;
+          $scope.meta = meta;
+          $scope.crmApi4 = crmApi4;
+          $scope.crmStatus = crmStatus;
+          $scope.crmUiAlert = crmUiAlert;
+          $scope.crmUrl = CRM.url;
+        }
       };
       return d;
     };
