def68386f55e8d6891f028eca327214dce2af91d
eileenmcnaughton <emcnaughton@wikimedia.org>
2019-09-13 09:29:25 +1200

    Pass params to allow scheduling
    Bug: T231362
    Change-Id: I7a75284e3a517415cc43f170e50c1b63f9d68bae

--- sites/default/civicrm/extensions/org.wikimedia.targetsmart/api/v3/TargetSmart/ImportJob.php
+++ sites/default/civicrm/extensions/org.wikimedia.targetsmart/api/v3/TargetSmart/ImportJob.php
@@ -16,6 +16,19 @@ function _civicrm_api3_target_smart_import_job_spec(&$spec) {
     'api.default' => 1000,
   ];
   $spec['identifier'] = ['title' => ts('Identifier for job - 1 to 8'), 'type' => CRM_Utils_Type::T_INT];
+  $spec['mapping_name'] = [
+    'title' => E::ts('Import name'),
+    'api.default' => '2019_targetsmart_bulkimport',
+  ];
+  $spec['add_to_group_name'] = [
+    'title' => E::ts('Add contacts to group (name)'),
+    'api.default' => '2019_targetsmart_bulkimport',
+  ];
+  $spec['null_rows_at_end_count'] = [
+    'title' => E::ts('Number of "nulls" columns to add at the end to blank out data'),
+    'type' => CRM_Utils_Type::T_INT,
+    'api.default' => 0,
+  ];
 }
 
 /**
@@ -33,6 +46,9 @@ function civicrm_api3_target_smart_import_job($params) {
     'csv' => $params['csv'],
     'batch_size' => $params['batch_size'],
     'offset' => $offset,
+    'mapping_name' => $params['mapping_name'],
+    'add_to_group_name' => $params['add_to_group_name'],
+    'null_rows_at_end_count' => $params['null_rows_at_end_count'],
   ]);
   Civi::settings()->set('targetsmart_progress' . $params['identifier'], $offset + $params['batch_size']);
   return $result;
