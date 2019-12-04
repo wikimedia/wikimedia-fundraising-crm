7b154f8f2bb021b0f9e8731a0838a8fa85eb5561
Elliott Eggleston <ejegg@ejegg.com>
2019-12-03 20:52:56 -0500

    IDE reformat: wmf_campaign module
    Change-Id: Ia0438500d56f6e75ad1c3e4737deae9d8c603a3e

--- sites/all/modules/wmf_campaigns/tests/WmfCampaignTest.php
+++ sites/all/modules/wmf_campaigns/tests/WmfCampaignTest.php
@@ -6,90 +6,94 @@ use wmf_communication\TestMailer;
  * @group WmfCampaigns
  */
 class WmfCampaignTest extends BaseWmfDrupalPhpUnitTestCase {
-    public $campaign_custom_field_name;
-    public $campaign_key;
-    public $notification_email;
-    public $contact_id;
 
-    function setUp() {
-        parent::setUp();
-        civicrm_initialize();
+  public $campaign_custom_field_name;
 
-        TestMailer::setup();
+  public $campaign_key;
 
-        $this->campaign_custom_field_name = wmf_civicrm_get_custom_field_name( 'Appeal' );
+  public $notification_email;
 
-        $this->campaign_key = 'fooCamp' . mt_rand();
-        $this->notification_email = 'notifee@localhost.net';
+  public $contact_id;
 
-       $contactID = $this->createTestContact( [
-          'contact_type' => 'Individual',
-          'first_name' => 'Testes',
-        ]);
-        $this->contact_id = $contactID;
+  function setUp() {
+    parent::setUp();
+    civicrm_initialize();
 
-        db_merge( 'wmf_campaigns_campaign' )
-            ->key( array( 'campaign_key' => $this->campaign_key ) )
-            ->fields( array(
-                'campaign_key' => $this->campaign_key,
-                'notification_email' => $this->notification_email,
-            ) )
-            ->execute();
+    TestMailer::setup();
 
-        civicrm_api3( 'OptionValue', 'create', array(
-            'option_group_id' => WMF_CAMPAIGNS_OPTION_GROUP_NAME,
-            'label' => $this->campaign_key,
-            'value' => $this->campaign_key,
-        ) );
-    }
+    $this->campaign_custom_field_name = wmf_civicrm_get_custom_field_name('Appeal');
 
-    function testMatchingDonation() {
-        $result = civicrm_api3( 'Contribution', 'create', array(
-            'contact_id' => $this->contact_id,
-            'contribution_type' => 'Cash',
-            'currency' => 'USD',
-            'payment_instrument' => 'Credit Card',
-            'total_amount' => '1.23',
-            'trxn_id' => 'TEST_GATEWAY ' . mt_rand(),
-            $this->campaign_custom_field_name => $this->campaign_key,
-        ) );
+    $this->campaign_key = 'fooCamp' . mt_rand();
+    $this->notification_email = 'notifee@localhost.net';
 
-        $this->assertEquals( 1, TestMailer::countMailings(),
-            'Exactly one email was sent.' );
+    $contactID = $this->createTestContact([
+      'contact_type' => 'Individual',
+      'first_name' => 'Testes',
+    ]);
+    $this->contact_id = $contactID;
 
-        $mailing = TestMailer::getMailing( 0 );
-        $this->assertNotEquals( false, strpos( $mailing['html'], $this->campaign_key ),
-            'Campaign name found in notification email.' );
-    }
+    db_merge('wmf_campaigns_campaign')
+      ->key(['campaign_key' => $this->campaign_key])
+      ->fields([
+        'campaign_key' => $this->campaign_key,
+        'notification_email' => $this->notification_email,
+      ])
+      ->execute();
 
-    /**
-     * @expectedException CiviCRM_API3_Exception
-     * @expectedExceptionMessageRegExp /fooCamp.*NOT/
-     */
-    function testNonMatchingDonation() {
-        $result = civicrm_api3( 'Contribution', 'create', array(
-            'contact_id' => $this->contact_id,
-            'contribution_type' => 'Cash',
-            'currency' => 'USD',
-            'payment_instrument' => 'Credit Card',
-            'total_amount' => '1.23',
-            'trxn_id' => 'TEST_GATEWAY ' . mt_rand(),
-            $this->campaign_custom_field_name => $this->campaign_key . "NOT",
-        ) );
+    civicrm_api3('OptionValue', 'create', [
+      'option_group_id' => WMF_CAMPAIGNS_OPTION_GROUP_NAME,
+      'label' => $this->campaign_key,
+      'value' => $this->campaign_key,
+    ]);
+  }
 
-        $this->fail( 'Should have exceptioned out already.' );
-    }
+  function testMatchingDonation() {
+    $result = civicrm_api3('Contribution', 'create', [
+      'contact_id' => $this->contact_id,
+      'contribution_type' => 'Cash',
+      'currency' => 'USD',
+      'payment_instrument' => 'Credit Card',
+      'total_amount' => '1.23',
+      'trxn_id' => 'TEST_GATEWAY ' . mt_rand(),
+      $this->campaign_custom_field_name => $this->campaign_key,
+    ]);
 
-    function testNoCampaignDonation() {
-        $result = civicrm_api3( 'Contribution', 'create', array(
-            'contact_id' => $this->contact_id,
-            'contribution_type' => 'Cash',
-            'currency' => 'USD',
-            'payment_instrument' => 'Credit Card',
-            'total_amount' => '1.23',
-            'trxn_id' => 'TEST_GATEWAY ' . mt_rand(),
-        ) );
+    $this->assertEquals(1, TestMailer::countMailings(),
+      'Exactly one email was sent.');
 
-        $this->assertEquals( 0, TestMailer::countMailings() );
-    }
+    $mailing = TestMailer::getMailing(0);
+    $this->assertNotEquals(FALSE, strpos($mailing['html'], $this->campaign_key),
+      'Campaign name found in notification email.');
+  }
+
+  /**
+   * @expectedException CiviCRM_API3_Exception
+   * @expectedExceptionMessageRegExp /fooCamp.*NOT/
+   */
+  function testNonMatchingDonation() {
+    $result = civicrm_api3('Contribution', 'create', [
+      'contact_id' => $this->contact_id,
+      'contribution_type' => 'Cash',
+      'currency' => 'USD',
+      'payment_instrument' => 'Credit Card',
+      'total_amount' => '1.23',
+      'trxn_id' => 'TEST_GATEWAY ' . mt_rand(),
+      $this->campaign_custom_field_name => $this->campaign_key . "NOT",
+    ]);
+
+    $this->fail('Should have exceptioned out already.');
+  }
+
+  function testNoCampaignDonation() {
+    $result = civicrm_api3('Contribution', 'create', [
+      'contact_id' => $this->contact_id,
+      'contribution_type' => 'Cash',
+      'currency' => 'USD',
+      'payment_instrument' => 'Credit Card',
+      'total_amount' => '1.23',
+      'trxn_id' => 'TEST_GATEWAY ' . mt_rand(),
+    ]);
+
+    $this->assertEquals(0, TestMailer::countMailings());
+  }
 }
