dada820a879e721fea2d516d7524489e36cde030
eileenmcnaughton <emcnaughton@wikimedia.org>
2020-01-15 16:43:46 +1300

    Reformat arrays in test classes
    IDE reformat
    
    Change-Id: I451d7e1348c75ebb1d1a3634a4ce43d9980ad462

--- sites/default/civicrm/extensions/org.wikimedia.omnimail/tests/phpunit/OmnigroupmemberLoadTest.php
+++ sites/default/civicrm/extensions/org.wikimedia.omnimail/tests/phpunit/OmnigroupmemberLoadTest.php
@@ -7,6 +7,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
+
 require_once __DIR__ . '/OmnimailBaseTestClass.php';
 
 /**
@@ -32,10 +33,18 @@ class OmnigroupmemberLoadTest extends OmnimailBaseTestClass {
    */
   public function testOmnigroupmemberLoad() {
     $client = $this->setupSuccessfulDownloadClient();
-    $group = civicrm_api3('Group', 'create', array('name' => 'Omnimailers', 'title' => 'Omni'));
+    $group = civicrm_api3('Group', 'create', ['name' => 'Omnimailers', 'title' => 'Omni']);
 
-    civicrm_api3('Omnigroupmember', 'load', array('mail_provider' => 'Silverpop', 'username' => 'Shrek', 'password' => 'Fiona', 'options' => array('limit' => 3), 'client' => $client, 'group_identifier' => 123, 'group_id' => $group['id']));
-    $groupMembers = civicrm_api3('GroupContact', 'get', array('group_id' => $group['id']));
+    civicrm_api3('Omnigroupmember', 'load', [
+      'mail_provider' => 'Silverpop',
+      'username' => 'Shrek',
+      'password' => 'Fiona',
+      'options' => ['limit' => 3],
+      'client' => $client,
+      'group_identifier' => 123,
+      'group_id' => $group['id'],
+    ]);
+    $groupMembers = civicrm_api3('GroupContact', 'get', ['group_id' => $group['id']]);
     $this->assertEquals(3, $groupMembers['count']);
     $contacts = $this->getGroupMemberDetails($groupMembers);
     $this->assertEquals('fr_FR', $contacts['values'][0]['preferred_language']);
@@ -53,10 +62,18 @@ class OmnigroupmemberLoadTest extends OmnimailBaseTestClass {
    */
   public function testOmnigroupmemberLoadOffset() {
     $client = $this->setupSuccessfulDownloadClient();
-    $group = civicrm_api3('Group', 'create', array('name' => 'Omnimailers', 'title' => 'Omni'));
+    $group = civicrm_api3('Group', 'create', ['name' => 'Omnimailers', 'title' => 'Omni']);
 
-    civicrm_api3('Omnigroupmember', 'load', array('mail_provider' => 'Silverpop', 'username' => 'Shrek', 'password' => 'Fiona', 'options' => array('offset' => 1), 'client' => $client, 'group_identifier' => 123, 'group_id' => $group['id']));
-    $groupMembers = civicrm_api3('GroupContact', 'get', array('group_id' => $group['id']));
+    civicrm_api3('Omnigroupmember', 'load', [
+      'mail_provider' => 'Silverpop',
+      'username' => 'Shrek',
+      'password' => 'Fiona',
+      'options' => ['offset' => 1],
+      'client' => $client,
+      'group_identifier' => 123,
+      'group_id' => $group['id'],
+    ]);
+    $groupMembers = civicrm_api3('GroupContact', 'get', ['group_id' => $group['id']]);
     $this->assertEquals(2, $groupMembers['count']);
     $contacts = $this->getGroupMemberDetails($groupMembers);
     $this->assertEquals('sarah@example.com', $contacts['values'][0]['email']);
@@ -69,32 +86,48 @@ class OmnigroupmemberLoadTest extends OmnimailBaseTestClass {
    */
   public function testOmnigroupmemberLoadUseOffsetSetting() {
     $client = $this->setupSuccessfulDownloadClient();
-    $group = civicrm_api3('Group', 'create', array('name' => 'Omnimailers', 'title' => 'Omni'));
+    $group = civicrm_api3('Group', 'create', ['name' => 'Omnimailers', 'title' => 'Omni']);
 
-    civicrm_api3('Omnigroupmember', 'load', array('mail_provider' => 'Silverpop', 'username' => 'Shrek', 'password' => 'Fiona', 'options' => array('limit' => 1), 'client' => $client, 'group_identifier' => 123, 'group_id' => $group['id']));
-    $groupMembers = civicrm_api3('GroupContact', 'get', array('group_id' => $group['id']));
+    civicrm_api3('Omnigroupmember', 'load', [
+      'mail_provider' => 'Silverpop',
+      'username' => 'Shrek',
+      'password' => 'Fiona',
+      'options' => ['limit' => 1],
+      'client' => $client,
+      'group_identifier' => 123,
+      'group_id' => $group['id'],
+    ]);
+    $groupMembers = civicrm_api3('GroupContact', 'get', ['group_id' => $group['id']]);
     $this->assertEquals(1, $groupMembers['count']);
     $contacts = $this->getGroupMemberDetails($groupMembers);
     $this->assertEquals('eric@example.com', $contacts['values'][0]['email']);
 
     // Re-run. Offset is now 1 in settings & we are passing in limit =1. Sarah should be created.
     $client = $this->setupSuccessfulDownloadClient(FALSE);
-    civicrm_api3('Omnigroupmember', 'load', array('mail_provider' => 'Silverpop', 'username' => 'Shrek', 'password' => 'Fiona', 'options' => array('limit' => 1), 'client' => $client, 'group_identifier' => 123, 'group_id' => $group['id']));
-    $groupMembers = civicrm_api3('GroupContact', 'get', array('group_id' => $group['id']));
+    civicrm_api3('Omnigroupmember', 'load', [
+      'mail_provider' => 'Silverpop',
+      'username' => 'Shrek',
+      'password' => 'Fiona',
+      'options' => ['limit' => 1],
+      'client' => $client,
+      'group_identifier' => 123,
+      'group_id' => $group['id'],
+    ]);
+    $groupMembers = civicrm_api3('GroupContact', 'get', ['group_id' => $group['id']]);
     $this->assertEquals(2, $groupMembers['count']);
     $contacts = $this->getGroupMemberDetails($groupMembers);
     $this->assertEquals('sarah@example.com', $contacts['values'][1]['email']);
     $this->cleanupGroup($group);
 
-    $this->assertEquals(array(
+    $this->assertEquals([
       'last_timestamp' => '2017-02-23 23:00:00',
       'offset' => 2,
-      'retrieval_parameters' => array(
+      'retrieval_parameters' => [
         'jobId' => '101719657',
         'filePath' => '/download/20170509_noCID - All - Jul 5 2017 06-27-45 AM.csv',
-      ),
+      ],
       'progress_end_timestamp' => '2017-03-02 23:00:00',
-    ), $this->getUtcDateFormattedJobSettings());
+    ], $this->getUtcDateFormattedJobSettings());
 
   }
 
@@ -102,34 +135,41 @@ class OmnigroupmemberLoadTest extends OmnimailBaseTestClass {
    * Test when download does not complete in time.
    */
   public function testOmnigroupmemberLoadIncomplete() {
-    $this->createSetting(array(
+    $this->createSetting([
       'job' => 'omnimail_omnigroupmembers_load',
       'mailing_provider' => 'Silverpop',
       'last_timestamp' => '1487890800',
-    ));
-    $responses = array(
+    ]);
+    $responses = [
       file_get_contents(__DIR__ . '/Responses/ExportListResponse.txt'),
-    );
+    ];
     for ($i = 0; $i < 15; $i++) {
       $responses[] = file_get_contents(__DIR__ . '/Responses/JobStatusWaitingResponse.txt');
     }
-    civicrm_api3('setting', 'create', array('omnimail_job_retry_interval' => 0.01));
-    $group = civicrm_api3('Group', 'create', array('name' => 'Omnimailers2', 'title' => 'Omni2'));
+    civicrm_api3('setting', 'create', ['omnimail_job_retry_interval' => 0.01]);
+    $group = civicrm_api3('Group', 'create', ['name' => 'Omnimailers2', 'title' => 'Omni2']);
 
-    civicrm_api3('Omnigroupmember', 'load', array('mail_provider' => 'Silverpop', 'username' => 'Donald', 'password' => 'Duck', 'client' => $this->getMockRequest($responses), 'group_identifier' => 123, 'group_id' => $group['id']));
+    civicrm_api3('Omnigroupmember', 'load', [
+      'mail_provider' => 'Silverpop',
+      'username' => 'Donald',
+      'password' => 'Duck',
+      'client' => $this->getMockRequest($responses),
+      'group_identifier' => 123,
+      'group_id' => $group['id'],
+    ]);
 
-    $groupMembers = civicrm_api3('GroupContact', 'get', array('group_id' => $group['id']));
+    $groupMembers = civicrm_api3('GroupContact', 'get', ['group_id' => $group['id']]);
     $this->assertEquals(0, $groupMembers['count']);
 
-    $this->assertEquals(array(
+    $this->assertEquals([
       'last_timestamp' => '2017-02-23 23:00:00',
-      'retrieval_parameters' => array(
+      'retrieval_parameters' => [
         'jobId' => '101719657',
         'filePath' => '/download/20170509_noCID - All - Jul 5 2017 06-27-45 AM.csv',
-      ),
+      ],
       'progress_end_timestamp' => '2017-03-02 23:00:00',
       'offset' => 0,
-    ), $this->getUtcDateFormattedJobSettings());
+    ], $this->getUtcDateFormattedJobSettings());
     $this->cleanupGroup($group);
   }
 
@@ -137,22 +177,22 @@ class OmnigroupmemberLoadTest extends OmnimailBaseTestClass {
    * Test when download does not complete in time.
    */
   public function testOmnigroupmemberLoadIncompleteUseSuffix() {
-    $this->createSetting(array(
+    $this->createSetting([
       'job' => 'omnimail_omnigroupmembers_load',
       'mailing_provider' => 'Silverpop',
       'job_identifier' => '_woot',
       'last_timestamp' => '1487890800',
-    ));
-    $responses = array(
+    ]);
+    $responses = [
       file_get_contents(__DIR__ . '/Responses/ExportListResponse.txt'),
-    );
+    ];
     for ($i = 0; $i < 15; $i++) {
       $responses[] = file_get_contents(__DIR__ . '/Responses/JobStatusWaitingResponse.txt');
     }
-    civicrm_api3('setting', 'create', array('omnimail_job_retry_interval' => 0.01));
-    $group = civicrm_api3('Group', 'create', array('name' => 'Omnimailers2', 'title' => 'Omni2'));
+    civicrm_api3('setting', 'create', ['omnimail_job_retry_interval' => 0.01]);
+    $group = civicrm_api3('Group', 'create', ['name' => 'Omnimailers2', 'title' => 'Omni2']);
 
-    civicrm_api3('Omnigroupmember', 'load', array(
+    civicrm_api3('Omnigroupmember', 'load', [
       'mail_provider' => 'Silverpop',
       'username' => 'Donald',
       'password' => 'Duck',
@@ -160,20 +200,20 @@ class OmnigroupmemberLoadTest extends OmnimailBaseTestClass {
       'group_identifier' => 123,
       'group_id' => $group['id'],
       'job_identifier' => '_woot',
-      ));
+    ]);
 
-    $groupMembers = civicrm_api3('GroupContact', 'get', array('group_id' => $group['id']));
+    $groupMembers = civicrm_api3('GroupContact', 'get', ['group_id' => $group['id']]);
     $this->assertEquals(0, $groupMembers['count']);
 
-    $this->assertEquals(array(
+    $this->assertEquals([
       'last_timestamp' => '2017-02-23 23:00:00',
-      'retrieval_parameters' => array(
+      'retrieval_parameters' => [
         'jobId' => '101719657',
         'filePath' => '/download/20170509_noCID - All - Jul 5 2017 06-27-45 AM.csv',
-      ),
+      ],
       'progress_end_timestamp' => '2017-03-02 23:00:00',
       'offset' => 0,
-    ), $this->getUtcDateFormattedJobSettings(array('mail_provider' => 'Silverpop', 'job_identifier' => '_woot')));
+    ], $this->getUtcDateFormattedJobSettings(['mail_provider' => 'Silverpop', 'job_identifier' => '_woot']));
     $this->cleanupGroup($group);
   }
 
@@ -182,52 +222,53 @@ class OmnigroupmemberLoadTest extends OmnimailBaseTestClass {
    */
   public function testCompleteIncomplete() {
     $client = $this->setupSuccessfulDownloadClient(FALSE);
-    $group = civicrm_api3('Group', 'create', array('name' => 'Omnimailers3', 'title' => 'Omni3'));
-    $this->createSetting(array(
+    $group = civicrm_api3('Group', 'create', ['name' => 'Omnimailers3', 'title' => 'Omni3']);
+    $this->createSetting([
       'job' => 'omnimail_omnigroupmembers_load',
       'mailing_provider' => 'Silverpop',
       'last_timestamp' => '1487890800',
-      'retrieval_parameters' => array(
+      'retrieval_parameters' => [
         'jobId' => '101719657',
         'filePath' => '/download/20170509_noCID - All - Jul 5 2017 06-27-45 AM.csv',
-      ),
+      ],
       'progress_end_timestamp' => '1488150000',
-    ));
+    ]);
 
-    civicrm_api3('Omnigroupmember', 'load', array(
+    civicrm_api3('Omnigroupmember', 'load', [
       'mail_provider' => 'Silverpop',
       'username' => 'Shrek',
       'password' => 'Fiona',
-      'options' => array('limit' => 3),
+      'options' => ['limit' => 3],
       'client' => $client,
       'group_identifier' => 123,
       'group_id' => $group['id'],
-     ));
+    ]);
 
-    $groupMembers = civicrm_api3('GroupContact', 'get', array('group_id' => $group['id']));
+    $groupMembers = civicrm_api3('GroupContact', 'get', ['group_id' => $group['id']]);
     $this->assertEquals(3, $groupMembers['count']);
 
-    $this->assertEquals(array(
+    $this->assertEquals([
       'last_timestamp' => '2017-03-02 23:00:00',
-    ), $this->getUtcDateFormattedJobSettings(array('mail_provider' => 'Silverpop')));
+    ], $this->getUtcDateFormattedJobSettings(['mail_provider' => 'Silverpop']));
     $this->cleanupGroup($group);
   }
 
   /**
    * Set up the mock client to emulate a successful download.
+   *
    * @param bool $isUpdateSetting
    *
    * @return \GuzzleHttp\Client
    */
   protected function setupSuccessfulDownloadClient($isUpdateSetting = TRUE) {
-    $responses = array(
+    $responses = [
       file_get_contents(__DIR__ . '/Responses/ExportListResponse.txt'),
       file_get_contents(__DIR__ . '/Responses/JobStatusCompleteResponse.txt'),
-    );
+    ];
     copy(__DIR__ . '/Responses/20170509_noCID - All - Jul 5 2017 06-27-45 AM.csv', sys_get_temp_dir() . '/20170509_noCID - All - Jul 5 2017 06-27-45 AM.csv');
     fopen(sys_get_temp_dir() . '/20170509_noCID - All - Jul 5 2017 06-27-45 AM.csv.complete', 'c');
     if ($isUpdateSetting) {
-      $this->createSetting(array('job' => 'omnimail_omnigroupmembers_load', 'mailing_provider' => 'Silverpop', 'last_timestamp' => '1487890800'));
+      $this->createSetting(['job' => 'omnimail_omnigroupmembers_load', 'mailing_provider' => 'Silverpop', 'last_timestamp' => '1487890800']);
     }
 
     $client = $this->getMockRequest($responses);
@@ -241,7 +282,7 @@ class OmnigroupmemberLoadTest extends OmnimailBaseTestClass {
    *
    * @return array
    */
-  public function getJobSettings($params = array('mail_provider' => 'Silverpop')) {
+  public function getJobSettings($params = ['mail_provider' => 'Silverpop']) {
     $omnimail = new CRM_Omnimail_Omnigroupmembers($params);
     $result = $omnimail->getJobSettings();
     unset($result['id'], $result['mailing_provider'], $result['job'], $result['job_identifier']);
@@ -252,35 +293,36 @@ class OmnigroupmemberLoadTest extends OmnimailBaseTestClass {
    * @param $group
    */
   protected function cleanupGroup($group) {
-    civicrm_api3('GroupContact', 'get', array(
+    civicrm_api3('GroupContact', 'get', [
       'group_id' => $group['id'],
-      'api.contact.delete' => array('skip_undelete' => 1),
-    ));
-    civicrm_api3('Group', 'delete', array('id' => $group['id']));
+      'api.contact.delete' => ['skip_undelete' => 1],
+    ]);
+    civicrm_api3('Group', 'delete', ['id' => $group['id']]);
 
   }
 
   /**
    * @param $groupMembers
+   *
    * @return array
    */
   protected function getGroupMemberDetails($groupMembers) {
-    $contactIDs = array('IN' => array());
+    $contactIDs = ['IN' => []];
     foreach ($groupMembers['values'] as $groupMember) {
       $contactIDs['IN'][] = $groupMember['contact_id'];
     }
-    $contacts = civicrm_api3('Contact', 'get', array(
+    $contacts = civicrm_api3('Contact', 'get', [
       'contact_id' => $contactIDs,
       'sequential' => 1,
-      'return' => array(
+      'return' => [
         'contact_source',
         'email',
         'country',
         'created_date',
         'preferred_language',
-        'is_opt_out'
-      )
-    ));
+        'is_opt_out',
+      ],
+    ]);
     return $contacts;
   }
 
--- sites/default/civicrm/extensions/org.wikimedia.omnimail/tests/phpunit/OmnimailBaseTestClass.php
+++ sites/default/civicrm/extensions/org.wikimedia.omnimail/tests/phpunit/OmnimailBaseTestClass.php
@@ -48,7 +48,7 @@ class OmnimailBaseTestClass extends \PHPUnit\Framework\TestCase implements Headl
    *
    * @var array
    */
-  protected $contactIDs = array();
+  protected $contactIDs = [];
 
   public function setUp() {
     parent::setUp();
@@ -70,11 +70,12 @@ class OmnimailBaseTestClass extends \PHPUnit\Framework\TestCase implements Headl
    *
    * @param $body
    * @param bool $authenticateFirst
+   *
    * @return \GuzzleHttp\Client
    */
-  public function getMockRequest($body = array(), $authenticateFirst = TRUE) {
+  public function getMockRequest($body = [], $authenticateFirst = TRUE) {
 
-    $responses = array();
+    $responses = [];
     if ($authenticateFirst) {
       $responses[] = new Response(200, [], file_get_contents(__DIR__ . '/Responses/AuthenticateResponse.txt'));
     }
@@ -83,7 +84,7 @@ class OmnimailBaseTestClass extends \PHPUnit\Framework\TestCase implements Headl
     }
     $mock = new MockHandler($responses);
     $handler = HandlerStack::create($mock);
-    return new Client(array('handler' => $handler));
+    return new Client(['handler' => $handler]);
   }
 
 
@@ -95,14 +96,14 @@ class OmnimailBaseTestClass extends \PHPUnit\Framework\TestCase implements Headl
    * @return \GuzzleHttp\Client
    */
   protected function setupSuccessfulDownloadClient($job = 'omnimail_omnigroupmembers_load') {
-    $responses = array(
+    $responses = [
       file_get_contents(__DIR__ . '/Responses/RawRecipientDataExportResponse.txt'),
       file_get_contents(__DIR__ . '/Responses/JobStatusCompleteResponse.txt'),
-    );
+    ];
     //Raw Recipient Data Export Jul 02 2017 21-46-49 PM 758.zip
     copy(__DIR__ . '/Responses/Raw Recipient Data Export Jul 03 2017 00-47-42 AM 1295.csv', sys_get_temp_dir() . '/Raw Recipient Data Export Jul 03 2017 00-47-42 AM 1295.csv');
     fopen(sys_get_temp_dir() . '/Raw Recipient Data Export Jul 03 2017 00-47-42 AM 1295.csv.complete', 'c');
-    $this->createSetting(array('job' => $job, 'mailing_provider' => 'Silverpop', 'last_timestamp' => '1487890800'));
+    $this->createSetting(['job' => $job, 'mailing_provider' => 'Silverpop', 'last_timestamp' => '1487890800']);
     $client = $this->getMockRequest($responses);
     return $client;
   }
@@ -113,7 +114,7 @@ class OmnimailBaseTestClass extends \PHPUnit\Framework\TestCase implements Headl
    * @param array $values
    */
   protected function createSetting($values) {
-    foreach (array('last_timestamp', 'progress_end_timestamp') as $dateField) {
+    foreach (['last_timestamp', 'progress_end_timestamp'] as $dateField) {
       if (!empty($values[$dateField])) {
         $values[$dateField] = gmdate('YmdHis', $values[$dateField]);
       }
@@ -133,52 +134,52 @@ class OmnimailBaseTestClass extends \PHPUnit\Framework\TestCase implements Headl
    *
    * @return array
    */
-  public function getUtcDateFormattedJobSettings($params = array('mail_provider' => 'Silverpop')) {
-     $settings = $this->getJobSettings($params);
-     $dateFields = array('last_timestamp', 'progress_end_timestamp');
-     foreach ($dateFields as $dateField) {
-       if (!empty($settings[$dateField])) {
-         $settings[$dateField] = date('Y-m-d H:i:s', $settings[$dateField]);
-       }
-     }
-     return $settings;
+  public function getUtcDateFormattedJobSettings($params = ['mail_provider' => 'Silverpop']) {
+    $settings = $this->getJobSettings($params);
+    $dateFields = ['last_timestamp', 'progress_end_timestamp'];
+    foreach ($dateFields as $dateField) {
+      if (!empty($settings[$dateField])) {
+        $settings[$dateField] = date('Y-m-d H:i:s', $settings[$dateField]);
+      }
+    }
+    return $settings;
   }
 
   public function createMailingProviderData() {
-    $this->callAPISuccess('Campaign', 'create', array('name' => 'xyz', 'title' => 'Cool Campaign'));
-    $this->callAPISuccess('Mailing', 'create', array('campaign_id' => 'xyz', 'hash' => 'xyz', 'name' => 'Mail Unit Test'));
+    $this->callAPISuccess('Campaign', 'create', ['name' => 'xyz', 'title' => 'Cool Campaign']);
+    $this->callAPISuccess('Mailing', 'create', ['campaign_id' => 'xyz', 'hash' => 'xyz', 'name' => 'Mail Unit Test']);
 
-    $this->callAPISuccess('MailingProviderData', 'create',  array(
+    $this->callAPISuccess('MailingProviderData', 'create', [
       'contact_id' => $this->contactIDs['charlie_clone'],
       'email' => 'charlie@example.com',
       'event_type' => 'Opt Out',
       'mailing_identifier' => 'xyz',
       'recipient_action_datetime' => '2017-02-02',
       'contact_identifier' => 'a',
-    ));
-    $this->callAPISuccess('MailingProviderData', 'create',  array(
+    ]);
+    $this->callAPISuccess('MailingProviderData', 'create', [
       'contact_id' => $this->contactIDs['marie'],
       'event_type' => 'Open',
       'email' => 'bob@example.com',
       'mailing_identifier' => 'xyz',
       'recipient_action_datetime' => '2017-03-03',
       'contact_identifier' => 'b',
-    ));
-    $this->callAPISuccess('MailingProviderData', 'create',  array(
+    ]);
+    $this->callAPISuccess('MailingProviderData', 'create', [
       'contact_id' => $this->contactIDs['isaac'],
       'event_type' => 'Suppressed',
       'mailing_identifier' => 'xyuuuz',
       'recipient_action_datetime' => '2017-04-04',
       'contact_identifier' => 'c',
-    ));
-    $this->callAPISuccess('MailingProviderData', 'create',  array(
+    ]);
+    $this->callAPISuccess('MailingProviderData', 'create', [
       'contact_id' => $this->contactIDs['isaac'],
       'email' => 'charlie@example.com',
       'event_type' => 'Hard Bounce',
       'mailing_identifier' => 'xyuuuz',
       'recipient_action_datetime' => '2017-05-04',
       'contact_identifier' => 'c',
-    ));
+    ]);
   }
 
   /**
@@ -192,34 +193,34 @@ class OmnimailBaseTestClass extends \PHPUnit\Framework\TestCase implements Headl
   }
 
   protected function makeScientists() {
-    $contact = $this->callAPISuccess('Contact', 'create', array(
+    $contact = $this->callAPISuccess('Contact', 'create', [
       'first_name' => 'Charles',
       'last_name' => 'Darwin',
-      'contact_type' => 'Individual'
-    ));
+      'contact_type' => 'Individual',
+    ]);
     $this->contactIDs['charlie'] = $contact['id'];
-    $contact = $this->callAPISuccess('Contact', 'create', array(
+    $contact = $this->callAPISuccess('Contact', 'create', [
       'first_name' => 'Charlie',
       'last_name' => 'Darwin',
       'contact_type' => 'Individual',
-      'api.email.create' => array(
+      'api.email.create' => [
         'is_bulkmail' => 1,
         'email' => 'charlie@example.com',
-      )
-    ));
+      ],
+    ]);
     $this->contactIDs['charlie_clone'] = $contact['id'];
 
-    $contact = $this->callAPISuccess('Contact', 'create', array(
+    $contact = $this->callAPISuccess('Contact', 'create', [
       'first_name' => 'Marie',
       'last_name' => 'Currie',
-      'contact_type' => 'Individual'
-    ));
+      'contact_type' => 'Individual',
+    ]);
     $this->contactIDs['marie'] = $contact['id'];
-    $contact = $this->callAPISuccess('Contact', 'create', array(
+    $contact = $this->callAPISuccess('Contact', 'create', [
       'first_name' => 'Isaac',
       'last_name' => 'Newton',
-      'contact_type' => 'Individual'
-    ));
+      'contact_type' => 'Individual',
+    ]);
     $this->contactIDs['isaac'] = $contact['id'];
   }
 
@@ -258,7 +259,7 @@ class OmnimailBaseTestClass extends \PHPUnit\Framework\TestCase implements Headl
     $files = [
       '/Responses/AuthenticateRestResponse.txt',
       '/Responses/Privacy/EraseInProgressResponse.txt',
-      '/Responses/Privacy/EraseSuccessResponse.txt'
+      '/Responses/Privacy/EraseSuccessResponse.txt',
     ];
 
     $this->createMockHandlerForFiles($files);
