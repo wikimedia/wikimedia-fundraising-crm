87851727333cb0f8263c236fb956109215b01d8c
Elliott Eggleston <ejegg@ejegg.com>
2020-06-24 17:22:57 -0500

    Add matching gift employer export
    Exports a CSV with employer contact ID and name for each of the org
    contacts with a name_from_matching_gifts_db, as well as rows for each
    of the names listed in the subsidiaries field.
    
    Excludes records marked as 'Suppress from employer field'
    
    drush cvapi MatchingGiftPolicies.Export path=/tmp/employers.csv
    
    Bug: T250328
    Change-Id: Id72359a40abdad69c37cf22259a075faa32a52cc

---
+++ sites/default/civicrm/extensions/matching-gifts/api/v3/MatchingGiftPolicies/Export.php
@@ -0,0 +1,67 @@
+<?php
+
+use League\Csv\Writer;
+
+/**
+ * @param array $params
+ */
+function _civicrm_api3_matching_gift_policies_export_spec(&$params) {
+  $params['path'] = [
+    'title' => 'Output path',
+    'api.required' => TRUE
+  ];
+}
+
+/**
+ * @param array $params
+ * @return array API result descriptor
+ */
+function civicrm_api3_matching_gift_policies_export($params) {
+  $mgNameFieldId = CRM_Core_BAO_CustomField::getCustomFieldID(
+    'name_from_matching_gift_db', 'matching_gift_policies', TRUE
+  );
+  $subsidiariesFieldId = CRM_Core_BAO_CustomField::getCustomFieldID(
+    'subsidiaries', 'matching_gift_policies', TRUE
+  );
+  $suppressionFieldId = CRM_Core_BAO_CustomField::getCustomFieldID(
+    'suppress_from_employer_field', 'matching_gift_policies', TRUE
+  );
+  $orgContacts = civicrm_api3(
+    'Contact', 'get', [
+      'contact_type' => 'Organization',
+      'return' => [$mgNameFieldId, $subsidiariesFieldId],
+      'sequential' => 1,
+      $mgNameFieldId => ['IS NOT NULL' => 1],
+      $suppressionFieldId => 0,
+      'options' => [
+        'limit' => 0,
+      ],
+    ]
+  );
+  $outputPath = $params['path'];
+  if (
+    (file_exists($outputPath) && !is_writable($outputPath)) ||
+    !is_writable(dirname($outputPath))
+  ) {
+    return civicrm_api3_create_error("Output path $outputPath is not writeable.");
+  }
+
+  $writer = Writer::createFromPath($outputPath, 'w');
+  foreach ($orgContacts['values'] as $contact) {
+    $parentCompanyName = $contact[$mgNameFieldId];
+    $writer->insertOne([$contact['contact_id'], $parentCompanyName]);
+    $subsidiaries = json_decode($contact[$subsidiariesFieldId]);
+    foreach ($subsidiaries as $subsidiary) {
+      // Skip if the subsidiary name is basically equal to the parent co name.
+      if (
+        $subsidiary === $parentCompanyName ||
+        $subsidiary === 'The ' . $parentCompanyName ||
+        'The ' . $subsidiary === $parentCompanyName
+      ) {
+        continue;
+      }
+      $writer->insertOne([$contact['contact_id'], $subsidiary]);
+    }
+  }
+  return civicrm_api3_create_success();
+}
