71f6b7900d22f104eb08053a1467daf42c2ea0e4
eileen <emcnaughton@wikimedia.org>
2020-07-29 15:03:34 +1200

    Move handling for country only to the deduper.
    This is slowly making the deduper code simpler, supporting us figuring out the email merge weirdness
    
    Bug: T256316
    Change-Id: I136a295eed7efc13ab91ed7c97b9fad25377496b

--- sites/default/civicrm/extensions/deduper/CRM/Deduper/BAO/Resolver/EquivalentAddressResolver.php
+++ sites/default/civicrm/extensions/deduper/CRM/Deduper/BAO/Resolver/EquivalentAddressResolver.php
@@ -11,16 +11,51 @@ class CRM_Deduper_BAO_Resolver_EquivalentAddressResolver extends CRM_Deduper_BAO
 
   /**
    * Resolve conflicts if possible.
+   *
+   * @throws \CiviCRM_API3_Exception
    */
   public function resolveConflicts() {
-    foreach (array_keys($this->getAllAddressConflicts()) as $blockNumber) {
+    foreach ($this->getAllAddressConflicts() as $blockNumber => $conflicts) {
       $mainBlock = $this->getAddressBlock(TRUE, $blockNumber);
       $otherBlock = $this->getAddressBlock(FALSE, $blockNumber);
       if ($this->hasSamePostalCodeButOneSuffixIsEmpty($mainBlock, $otherBlock)) {
         $postalCodeSuffix = $mainBlock['postal_code_suffix'] ?? $otherBlock['postal_code_suffix'];
         $this->setResolvedAddressValue('postal_code_suffix', 'address', $blockNumber, $postalCodeSuffix);
       }
+      // Check if one address is only a country, and is a subset of the other.
+      // This could be the case if only 'display' is in conflict and both have countries.
+      // We could extend this to check state+country but as we expand it we might hit some edge cases.
+      if (!empty($mainBlock['country_id']) && !empty($otherBlock['country_id']) && array_keys($conflicts) === ['display'] ) {
+        if ($this->isDisplayCountryOnly((int) $mainBlock['country_id'], $mainBlock['display'])) {
+          // ditch this address & use the other.
+          $this->setResolvedAddressValue('display', 'address', $blockNumber, $otherBlock['display']);
+        }
+        elseif ($this->isDisplayCountryOnly((int) $otherBlock['country_id'], $otherBlock['display'])) {
+          // ditch this address & use the other.
+          $this->setResolvedAddressValue('display', 'address', $blockNumber, $mainBlock['display']);
+        }
+      }
     }
+  }
+
+  /**
+   * Is the display field just the country?
+   *
+   * If display is 'Mexico\n' and the country id matches Mexico this will return true.
+   *
+   * @param int $countryID
+   * @param string $display
+   *
+   * @return bool
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function isDisplayCountryOnly(int $countryID, $display): bool {
+    static $countries = [];
+    if (empty($countries)) {
+      $countries = civicrm_api3('Address', 'getoptions', ['field' => 'country_id'])['values'];
+    }
+    $country = $countries[$countryID] ?? NULL;
+    return $country . "\n" === $display;
   }
 
   /**
