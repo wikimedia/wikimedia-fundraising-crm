e50aaa55ce243f74dffb308bfdc402a897ce115e
eileen <emcnaughton@wikimedia.org>
2020-08-19 14:40:04 +1200

    IDE reformat
    Change-Id: I54f5434ffb99c60f145d2c552e5e54bfc49e1600

--- sites/all/modules/queue2civicrm/tests/phpunit/BannerHistoryTest.php
+++ sites/all/modules/queue2civicrm/tests/phpunit/BannerHistoryTest.php
@@ -1,4 +1,5 @@
 <?php
+
 use queue2civicrm\banner_history\BannerHistoryQueueConsumer;
 
 /**
@@ -6,50 +7,50 @@ use queue2civicrm\banner_history\BannerHistoryQueueConsumer;
  */
 class BannerHistoryTest extends BaseWmfDrupalPhpUnitTestCase {
 
-	/**
-	 * @var BannerHistoryQueueConsumer
-	 */
-	protected $consumer;
+  /**
+   * @var BannerHistoryQueueConsumer
+   */
+  protected $consumer;
 
-	public function setUp() {
-		parent::setUp();
-		$this->consumer = new BannerHistoryQueueConsumer(
-			'test'
-		);
-	}
+  public function setUp() {
+    parent::setUp();
+    $this->consumer = new BannerHistoryQueueConsumer(
+      'test'
+    );
+  }
 
-	public function testValidMessage() {
-		$msg = array(
-			'banner_history_id' => substr(
-				md5( mt_rand() . time() ), 0, 16
-			),
-			'contribution_tracking_id' => strval( mt_rand() ),
-		);
-		$this->consumer->processMessage( $msg );
-		// check for thing in db
-	}
+  public function testValidMessage() {
+    $msg = [
+      'banner_history_id' => substr(
+        md5(mt_rand() . time()), 0, 16
+      ),
+      'contribution_tracking_id' => strval(mt_rand()),
+    ];
+    $this->consumer->processMessage($msg);
+    // check for thing in db
+  }
 
-	/**
-	 * @expectedException WmfException
-	 */
-	public function testBadContributionId() {
-		$msg = array(
-			'banner_history_id' => substr(
-				md5( mt_rand() . time() ), 0, 16
-			),
-			'contribution_tracking_id' => '1=1; DROP TABLE students;--',
-		);
-		$this->consumer->processMessage( $msg );
-	}
+  /**
+   * @expectedException WmfException
+   */
+  public function testBadContributionId() {
+    $msg = [
+      'banner_history_id' => substr(
+        md5(mt_rand() . time()), 0, 16
+      ),
+      'contribution_tracking_id' => '1=1; DROP TABLE students;--',
+    ];
+    $this->consumer->processMessage($msg);
+  }
 
-	/**
-	 * @expectedException WmfException
-	 */
-	public function testBadHistoryId() {
-		$msg = array(
-			'banner_history_id' => '\';GRANT ALL ON drupal.* TO \'leet\'@\'haxx0r\'',
-			'contribution_tracking_id' => strval( mt_rand() ),
-		);
-		$this->consumer->processMessage( $msg );
-	}
+  /**
+   * @expectedException WmfException
+   */
+  public function testBadHistoryId() {
+    $msg = [
+      'banner_history_id' => '\';GRANT ALL ON drupal.* TO \'leet\'@\'haxx0r\'',
+      'contribution_tracking_id' => strval(mt_rand()),
+    ];
+    $this->consumer->processMessage($msg);
+  }
 }
