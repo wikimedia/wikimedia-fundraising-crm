68131e8719f0c589c2836b52fda35d6a6a7f83e0
eileen <emcnaughton@wikimedia.org>
2020-11-02 16:12:20 +1300

    Move PhpMailer to Omnimail
    Bug: T264663
    Change-Id: I2425ab71159faa2e0c460f173a6f05c01e4aac06

--- sites/all/modules/wmf_communication/Mailer.php
+++ sites/all/modules/wmf_communication/Mailer.php
@@ -2,7 +2,6 @@
 
 use Civi\Omnimail\MailFactory;
 use Html2Text\Html2Text;
-use PHPMailer;
 use Civi\Omnimail\IMailer;
 use WmfException;
 
@@ -119,54 +118,5 @@ abstract class MailerBase {
 	protected function splitAddresses( $to ) {
 		return preg_split( '/\\s*[,\\n]\\s*/', $to, -1, PREG_SPLIT_NO_EMPTY );
 	}
-}
-
-/**
- * Use the PHPMailer engine
- */
-class MailerPHPMailer extends MailerBase implements IMailer {
-    function send( $email, $headers = array() ) {
-        $mailer = new PHPMailer( true );
-
-        $mailer->set( 'CharSet', 'utf-8' );
-        $mailer->Encoding = 'quoted-printable';
-
-        $mailer->AddReplyTo( $email['from_address'], $email['from_name'] );
-        $mailer->SetFrom( $email['from_address'], $email['from_name'] );
-        if ( !empty( $email['reply_to'] ) ) {
-            $mailer->set( 'Sender', $email['reply_to'] );
-        }
-
-        // Note that this is incredibly funky.  This is the only mailer to support
-        // a "to" parameter, and it behaves differently than to_address/to_name.
-        // You can pass a list of bare email addresses through "to" and they'll all
-        // become to addresses, but without names, so this should only be used by
-        // maintenancey things directed at staff.
-        if ( isset( $email['to'] ) ) {
-            if ( is_string( $email['to'] ) ) {
-                $email['to'] = $this->splitAddresses( $email['to'] );
-            }
-            foreach ( $email['to'] as $to ) {
-                $mailer->AddAddress( $to );
-            }
-        } else {
-            $mailer->AddAddress( $email['to_address'], $email['to_name'] );
-        }
-
-		foreach ($headers as $header => $value) {
-			$mailer->AddCustomHeader( "$header: $value" );
-		}
-
-        $mailer->Subject = $email['subject'];
-        # n.b. - must set AltBody after MsgHTML(), or the text will be overwritten.
-        $locale = empty( $email['locale'] ) ? null : $email['locale'];
-        $mailer->MsgHTML( $this->wrapHtmlSnippet( $email['html'], $locale ) );
-        $this->normalizeContent( $email );
-        $mailer->AltBody = $email['plaintext'];
-
-        $success = $mailer->Send();
-
-        return $success;
-    }
 }
 
