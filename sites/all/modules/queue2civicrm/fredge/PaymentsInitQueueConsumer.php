9c51bc689d8e3acfd9b397fd30438991c57d4bf1
Elliott Eggleston <ejegg@ejegg.com>
2020-11-12 15:53:24 -0500

    Reformat payments-init consumer
    Change-Id: Id3072a971d9b0d0f871f5a77dfb1c5889359d4f5

--- sites/all/modules/queue2civicrm/fredge/PaymentsInitQueueConsumer.php
+++ sites/all/modules/queue2civicrm/fredge/PaymentsInitQueueConsumer.php
@@ -7,72 +7,74 @@ use WmfException;
 
 class PaymentsInitQueueConsumer extends WmfQueueConsumer {
 
-	/**
-	 * Validate and store messages from the payments-init queue
-	 *
-	 * @param array $message
-	 * @throws WmfException
-	 */
-	function processMessage( $message ) {
-		$logId = "{$message['gateway']}-{$message['order_id']}";
-		watchdog(
-			'fredge',
-			"Beginning processing of payments-init message for $logId: " .
-				json_encode( $message ),
-			array(),
-			WATCHDOG_INFO
-		);
+  /**
+   * Validate and store messages from the payments-init queue
+   *
+   * @param array $message
+   *
+   * @throws WmfException
+   */
+  function processMessage($message) {
+    $logId = "{$message['gateway']}-{$message['order_id']}";
+    watchdog(
+      'fredge',
+      "Beginning processing of payments-init message for $logId: " .
+      json_encode($message),
+      [],
+      WATCHDOG_INFO
+    );
 
-		$id = 0;
-		$inserting = true;
+    $id = 0;
+    $inserting = TRUE;
 
-		// Delete corresponding pending rows if this contribution failed.
-		// The DonationQueueConsumer will delete pending rows for successful
-		// contributions, and we don't want to be too hasty.
-		// Leave details for payments still open for manual review.
+    // Delete corresponding pending rows if this contribution failed.
+    // The DonationQueueConsumer will delete pending rows for successful
+    // contributions, and we don't want to be too hasty.
+    // Leave details for payments still open for manual review.
     // We make an exception for Adyen and Astropay because those
     // processors allow donors to reuse the merchant reference by
     // reloading the hosted page. Note that this means we can't
     // implement orphan rectifiers for those gateways.
-    $processorAllowsRepeat = in_array(['gateway'],['astropay', 'adyen']);
-		if (
-		  PaymentsInitialDatabase::isMessageFailed($message) &&
+    $processorAllowsRepeat = in_array(['gateway'], ['astropay', 'adyen']);
+    if (
+      PaymentsInitialDatabase::isMessageFailed($message) &&
       !$processorAllowsRepeat
     ) {
-			watchdog(
-				'fredge',
-				"Deleting pending row for failed payment {$logId}",
-				array(),
-				WATCHDOG_INFO
-			);
-			PendingDatabase::get()->deleteMessage( $message );
-		}
+      watchdog(
+        'fredge',
+        "Deleting pending row for failed payment {$logId}",
+        [],
+        WATCHDOG_INFO
+      );
+      PendingDatabase::get()->deleteMessage($message);
+    }
 
-		$dbs = wmf_civicrm_get_dbs();
-		$dbs->push( 'fredge' );
-		$query = 'SELECT id FROM payments_initial
+    $dbs = wmf_civicrm_get_dbs();
+    $dbs->push('fredge');
+    $query = 'SELECT id FROM payments_initial
                   WHERE contribution_tracking_id = :ct_id
                   AND order_id = :order_id LIMIT 1';
-		$result = db_query( $query, array(
-			':ct_id' => $message['contribution_tracking_id'],
-			':order_id' => $message['order_id']
-		) );
-		if ( $result->rowCount() === 1 ) {
-			$id = $result->fetch()->id;
-			$inserting = false;
-		}
+    $result = db_query($query, [
+      ':ct_id' => $message['contribution_tracking_id'],
+      ':order_id' => $message['order_id'],
+    ]);
+    if ($result->rowCount() === 1) {
+      $id = $result->fetch()->id;
+      $inserting = FALSE;
+    }
 
-		$data = fredge_prep_data( $message, 'payments_initial', $logId, $inserting );
+    $data = fredge_prep_data($message, 'payments_initial', $logId, $inserting);
 
-		if ( $inserting ) {
-			db_insert( 'payments_initial' )
-				->fields( $data )
-				->execute();
-		} else {
-			db_update( 'payments_initial' )
-				->fields( $data )
-				->condition( 'id', $id )
-				->execute();
-		}
-	}
+    if ($inserting) {
+      db_insert('payments_initial')
+        ->fields($data)
+        ->execute();
+    }
+    else {
+      db_update('payments_initial')
+        ->fields($data)
+        ->condition('id', $id)
+        ->execute();
+    }
+  }
 }
