b8f88559e707adfe309c94f000a7a432c1440dfc
eileen <emcnaughton@wikimedia.org>
2020-12-31 12:57:50 +1300

    Move remaining tag-creation to extension
    I put a bit of a routine in the file to ensure the option value is present as that seemed
    like the only reliable way
    
    Bug: 270680
    Change-Id: I1d45d6b9786b1e90fa7bd526479d380c2c8f592e

--- sites/default/civicrm/extensions/wmf-civicrm/Managed/Tags.mgd.php
+++ sites/default/civicrm/extensions/wmf-civicrm/Managed/Tags.mgd.php
@@ -1,7 +1,40 @@
 <?php
 
-// Note that reserved tags cannot be edited by (most) users.
+use Civi\Api4\OptionValue;
+use Civi\Api4\Tag;
 
+// First ensure that the option values exist so that we can add tags
+// against contributions and recurring contributions.
+// The timing is a bit fiddly so doing this as a separate mgd file
+// does not necessarily ensure it's created in time for these tags, hence here.
+$usedForOptions = Tag::getFields(FALSE)
+  ->setLoadOptions(TRUE)
+  ->addSelect('options')
+  ->addWhere('name', '=', 'used_for')
+  ->execute()->first()['options'];
+
+$requiredUsedForOptions = [
+  'civicrm_contribution' => [
+    'name' => 'Contributions',
+    'label' => 'Contributions',
+    'value' => 'civicrm_contribution',
+    'option_group_id:name' => 'tag_used_for',
+  ],
+  'civicrm_contribution_recur' => [
+    'name' => 'Recurring contributions',
+    'label' => 'Recurring contributions',
+    'value' => 'civicrm_contribution_recur',
+    'option_group_id:name' => 'tag_used_for',
+  ],
+];
+
+$missing = array_diff_key($requiredUsedForOptions, $usedForOptions);
+if (!empty($missing)) {
+  foreach ($missing as $optionValue) {
+    OptionValue::create(FALSE)->setValues($optionValue)->execute();
+  }
+}
+// Note that reserved tags cannot be edited by (most) users.
   return [
     'RecurringRestarted' => [
       // Tag used by recurring global collect module
@@ -15,6 +48,23 @@
         'is_selectable' => FALSE,
         'is_reserved' => TRUE,
         'used_for' => 'civicrm_contribution',
+      ],
+    ],
+    'RecurringRestartedUncharged' => [
+      // Indicates that the subscription has been cured of some
+      // malady, and that the next contribution record created
+      // from it should get the RecurringRestarted tag so the donor is thanked
+      // correctly. The recurring processor should then remove
+      // this tag from the civicrm_contribution_recur table.
+      'name' => 'RecurringRestartedUncharged',
+      'entity' => 'Tag',
+      'cleanup' => 'never',
+      'params' => [
+        'name' => 'RecurringRestartedUncharged',
+        'description' => 'A subscription that has been restarted but not yet charged.',
+        'is_selectable' => FALSE,
+        'is_reserved' => TRUE,
+        'used_for' => 'civicrm_contribution_recur',
       ],
     ],
     'UnrecordedCharge' => [
