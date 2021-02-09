2b9373cbb38efcfbfa48807c582dc451a1d228e0
eileen <emcnaughton@wikimedia.org>
2021-02-09 17:49:30 +1300

    Add notes about ci-create-dbs
    Change-Id: I565b8e12f34f8b42584d58fb618e0a3f03a6b36f

--- bin/ci-create-dbs.sh
+++ bin/ci-create-dbs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -eu
-
+# this file creates the databases for the jenkins CI job.
+# on our dev installs they are created by buildkit
+# the reason for doing it differently on jenkins is historical
+# - in the past different ci jobs had not clash in their db names
+# @todo remove this file / approach so we can unfork buildkit
+# and use the same method locally and here.
 BASEDIR=$(dirname "$0")
 # shellcheck source=ci-settings.sh
 . $BASEDIR/ci-settings.sh
