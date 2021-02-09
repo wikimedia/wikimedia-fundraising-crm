0a404126d3da3ce2fa692d49f6c41075c78b8b2e
eileen <emcnaughton@wikimedia.org>
2021-02-09 16:29:40 +1300

    Set default charset for created dbs to utf8 on CI
    This is in line with live DBs - it might not be the last word on it but
    it gets us to a more consistent place
    
    Bug: T273704
    
    Change-Id: I93981b10016e73aabcb97be74f8cabb6d920f5cb

--- bin/ci-create-dbs.sh
+++ bin/ci-create-dbs.sh
@@ -9,7 +9,7 @@ echo "Creating databases with the prefix '${CIVICRM_SCHEMA_PREFIX}'"
 for i in 1 2 3; do
 	mysql -u root <<EOS
 	drop database if exists ${CIVICRM_SCHEMA_PREFIX}${i};
-	create database ${CIVICRM_SCHEMA_PREFIX}${i};
+	create database ${CIVICRM_SCHEMA_PREFIX}${i} CHARACTER SET utf8 COLLATE utf8_general_ci;
 	grant all on ${CIVICRM_SCHEMA_PREFIX}${i}.* to '${CIVICRM_MYSQL_USERNAME}'@'${CIVICRM_MYSQL_CLIENT}' identified by '${CIVICRM_MYSQL_PASSWORD}';
 EOS
 done
