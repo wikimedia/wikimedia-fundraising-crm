fc49d935d0e9edfa8a56572b47a5d1027b34a238
eileen <emcnaughton@wikimedia.org>
2021-02-09 16:36:54 +1300

    Set mysql global row format to dynamic
    This is necessary to get the DBS to create in CI on civicrm 5.35
    
    Bug: T273704
    Change-Id: Ia0e7975dc07fc44def70504586fb7df26b9d6153

--- bin/ci-create-dbs.sh
+++ bin/ci-create-dbs.sh
@@ -8,6 +8,8 @@ echo "Creating databases with the prefix '${CIVICRM_SCHEMA_PREFIX}'"
 
 for i in 1 2 3; do
 	mysql -u root <<EOS
+	SET GLOBAL innodb_file_format='Barracuda';
+	SET GLOBAL innodb_default_row_format='dynamic';
 	drop database if exists ${CIVICRM_SCHEMA_PREFIX}${i};
 	create database ${CIVICRM_SCHEMA_PREFIX}${i} CHARACTER SET utf8 COLLATE utf8_general_ci;
 	grant all on ${CIVICRM_SCHEMA_PREFIX}${i}.* to '${CIVICRM_MYSQL_USERNAME}'@'${CIVICRM_MYSQL_CLIENT}' identified by '${CIVICRM_MYSQL_PASSWORD}';
