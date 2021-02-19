a73237a0558afe93918666d4f1001eee87490210
eileen <emcnaughton@wikimedia.org>
2021-02-19 17:25:06 +1300

    Reformat, remove silly function
    The getInfo function relates to something that went by the wayside years ago
    
    Bug: T274943
    Change-Id: If952a4c5a314f9d72281b3d71db0951516925da4

--- sites/all/modules/wmf_civicrm/tests/phpunit/SoftCreditTest.php
+++ sites/all/modules/wmf_civicrm/tests/phpunit/SoftCreditTest.php
@@ -5,58 +5,52 @@
  * @group WmfCivicrm
  */
 class SoftCreditTest extends BaseWmfDrupalPhpUnitTestCase {
-    public static function getInfo() {
-        return array(
-            'name' => 'Soft Credit',
-            'group' => 'Pipeline',
-            'description' => 'Ensure we record soft credits.',
-        );
-    }
 
-    public function testSoftCredit() {
-        $fixtures = CiviFixtures::create();
+  public function testSoftCredit() {
+    $fixtures = CiviFixtures::create();
 
-        $msg = array(
-            'currency' => 'USD',
-            'date' => time(),
-            'email' => 'nobody@wikimedia.org',
-            'gateway' => 'test_gateway',
-            'gateway_txn_id' => mt_rand(),
-            'gross' => '1.23',
-            'payment_method' => 'cc',
+    $msg = [
+      'currency' => 'USD',
+      'date' => time(),
+      'email' => 'nobody@wikimedia.org',
+      'gateway' => 'test_gateway',
+      'gateway_txn_id' => mt_rand(),
+      'gross' => '1.23',
+      'payment_method' => 'cc',
 
-            'soft_credit_to' => $fixtures->org_contact_name,
-        );
+      'soft_credit_to' => $fixtures->org_contact_name,
+    ];
 
-        $contribution = wmf_civicrm_contribution_message_import( $msg );
+    $contribution = wmf_civicrm_contribution_message_import($msg);
 
-        $retrievedContribution = $this->callAPISuccessGetSingle('Contribution', array(
-          'id' => $contribution['id'],
-          'return' => array(
-            'soft_credit_to' => 1,
-          ),
-        ));
+    $retrievedContribution = $this->callAPISuccessGetSingle('Contribution', [
+      'id' => $contribution['id'],
+      'return' => [
+        'soft_credit_to' => 1,
+      ],
+    ]);
 
-        $this->assertEquals( $fixtures->org_contact_id, $retrievedContribution['soft_credit_to']);
-    }
+    $this->assertEquals($fixtures->org_contact_id, $retrievedContribution['soft_credit_to']);
+  }
 
-    /**
-     * @expectedException WmfException
-     * @expectedExceptionMessage Bad soft credit target
-     */
-    public function testBadSoftCreditTarget() {
-        $msg = array(
-            'currency' => 'USD',
-            'date' => time(),
-            'email' => 'nobody@wikimedia.org',
-            'gateway' => 'test_gateway',
-            'gateway_txn_id' => mt_rand(),
-            'gross' => '1.23',
-            'payment_method' => 'cc',
+  /**
+   * @expectedException WmfException
+   * @expectedExceptionMessage Bad soft credit target
+   */
+  public function testBadSoftCreditTarget() {
+    $msg = [
+      'currency' => 'USD',
+      'date' => time(),
+      'email' => 'nobody@wikimedia.org',
+      'gateway' => 'test_gateway',
+      'gateway_txn_id' => mt_rand(),
+      'gross' => '1.23',
+      'payment_method' => 'cc',
 
-            'soft_credit_to' => 'Not a thing',
-        );
+      'soft_credit_to' => 'Not a thing',
+    ];
 
-        $contribution = wmf_civicrm_contribution_message_import( $msg );
-    }
+    $contribution = wmf_civicrm_contribution_message_import($msg);
+  }
+
 }
