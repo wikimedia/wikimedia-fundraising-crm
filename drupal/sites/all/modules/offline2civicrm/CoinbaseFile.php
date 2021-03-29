e1f2b234f7beeec66e8253dbbd25dd344af39432
eileen <emcnaughton@wikimedia.org>
2021-03-29 16:27:14 +1300

    Formatting only
    Change-Id: I832a52b6a725ecc03edfcd97dd622ca3ccc1b6a7

--- drupal/sites/all/modules/offline2civicrm/CoinbaseFile.php
+++ drupal/sites/all/modules/offline2civicrm/CoinbaseFile.php
@@ -6,94 +6,99 @@
  * See https://coinbase.com/reports
  */
 class CoinbaseFile extends ChecksFile {
-    protected $refundLastTransaction = false;
 
-    protected function getRequiredColumns() {
-        return array(
-            'BTC Price',
-            'Currency',
-            'Customer Email',
-            'Native Price',
-            'Phone Number',
-            'Recurring Payment ID',
-            'Refund Transaction ID',
-            'Shipping Address 1',
-            'Shipping Address 2',
-            'Shipping City',
-            'Shipping Country',
-            'Shipping Name',
-            'Shipping Postal Code',
-            'Shipping State',
-            'Status',
-            'Timestamp',
-            'Tracking Code',
-        );
+  protected $refundLastTransaction = FALSE;
+
+  protected function getRequiredColumns() {
+    return [
+      'BTC Price',
+      'Currency',
+      'Customer Email',
+      'Native Price',
+      'Phone Number',
+      'Recurring Payment ID',
+      'Refund Transaction ID',
+      'Shipping Address 1',
+      'Shipping Address 2',
+      'Shipping City',
+      'Shipping Country',
+      'Shipping Name',
+      'Shipping Postal Code',
+      'Shipping State',
+      'Status',
+      'Timestamp',
+      'Tracking Code',
+    ];
+  }
+
+  protected function getRequiredData() {
+    return parent::getRequiredData() + [
+        'gateway_txn_id',
+      ];
+  }
+
+  protected function mungeMessage(&$msg) {
+    $msg['gateway'] = 'coinbase';
+    $msg['contribution_type'] = 'cash';
+    $msg['payment_instrument'] = 'Bitcoin';
+    // It seems that payment_method is used for creating the contribution_tracking source
+    // whereas payment_instrument is used for CiviCRM contributions payment_instrument_id field.
+    $msg['payment_method'] = 'bitcoin';
+
+    [
+      $msg['first_name'],
+      $msg['last_name'],
+    ] = wmf_civicrm_janky_split_name($msg['full_name']);
+
+    if (!empty($msg['gateway_refund_id'])) {
+      $this->refundLastTransaction = TRUE;
+      unset($msg['gateway_refund_id']);
     }
 
-    protected function getRequiredData() {
-        return parent::getRequiredData() + array(
-            'gateway_txn_id',
-        );
+    if (!empty($msg['subscr_id'])) {
+      $msg['recurring'] = TRUE;
     }
+  }
 
-    protected function mungeMessage( &$msg ) {
-        $msg['gateway'] = 'coinbase';
-        $msg['contribution_type'] = 'cash';
-        $msg['payment_instrument'] = 'Bitcoin';
-        // It seems that payment_method is used for creating the contribution_tracking source
-        // whereas payment_instrument is used for CiviCRM contributions payment_instrument_id field.
-        $msg['payment_method'] = 'bitcoin';
+  protected function mungeContribution($contribution) {
+    if ($this->refundLastTransaction) {
+      wmf_civicrm_mark_refund(
+        $contribution['id'],
+        'refund',
+        TRUE
+      );
+      watchdog('offline2civicrm', 'Refunding contribution @id', [
+        '@id' => $contribution['id'],
+      ], WATCHDOG_INFO);
 
-        list($msg['first_name'], $msg['last_name']) = wmf_civicrm_janky_split_name( $msg['full_name'] );
-
-        if ( !empty( $msg['gateway_refund_id'] ) ) {
-            $this->refundLastTransaction = true;
-            unset( $msg['gateway_refund_id'] );
-        }
-
-        if ( !empty( $msg['subscr_id'] ) ) {
-            $msg['recurring'] = true;
-        }
+      $this->refundLastTransaction = FALSE;
     }
+  }
 
-    protected function mungeContribution( $contribution ) {
-        if ( $this->refundLastTransaction ) {
-            wmf_civicrm_mark_refund(
-                $contribution['id'],
-                'refund',
-                true
-            );
-            watchdog( 'offline2civicrm', 'Refunding contribution @id', array(
-                '@id' => $contribution['id'],
-            ), WATCHDOG_INFO );
+  protected function getFieldMapping() {
+    return [
+      'Banner' => 'utm_source',
+      'Campaign' => 'utm_campaign',
+      //'BTC Price' => 'original_gross',
+      'Currency' => 'currency',
+      'Customer Email' => 'email',
+      'Medium' => 'utm_medium',
+      // FIXME: this will destroy recurring subscription import, for now.
+      'Native Price' => 'gross',
+      'Phone Number' => 'phone', // TODO: not stored
+      'Recurring Payment ID' => 'subscr_id',
+      'Refund Transaction ID' => 'gateway_refund_id',
+      'Shipping Address 1' => 'street_address',
+      'Shipping Address 2' => 'supplemental_address_1',
+      'Shipping City' => 'city',
+      'Shipping Country' => 'country',
+      'Shipping Name' => 'full_name',
+      'Shipping Postal Code' => 'postal_code',
+      'Shipping State' => 'state_province',
+      'Status' => 'gateway_status_raw',
+      'Timestamp' => 'date',
+      'Tracking Code' => 'gateway_txn_id',
+    ];
+  }
 
-            $this->refundLastTransaction = false;
-        }
-    }
-
-    protected function getFieldMapping() {
-        return array(
-            'Banner' => 'utm_source',
-            'Campaign' => 'utm_campaign',
-            //'BTC Price' => 'original_gross',
-            'Currency' => 'currency',
-            'Customer Email' => 'email',
-            'Medium' => 'utm_medium',
-            // FIXME: this will destroy recurring subscription import, for now.
-            'Native Price' => 'gross',
-            'Phone Number' => 'phone', // TODO: not stored
-            'Recurring Payment ID' => 'subscr_id',
-            'Refund Transaction ID' => 'gateway_refund_id',
-            'Shipping Address 1' => 'street_address',
-            'Shipping Address 2' => 'supplemental_address_1',
-            'Shipping City' => 'city',
-            'Shipping Country' => 'country',
-            'Shipping Name' => 'full_name',
-            'Shipping Postal Code' => 'postal_code',
-            'Shipping State' => 'state_province',
-            'Status' => 'gateway_status_raw',
-            'Timestamp' => 'date',
-            'Tracking Code' => 'gateway_txn_id',
-        );
-    }
 }
