feedb59a03e9dd33e9592df68a1ac4756954c7fc
Eileen McNaughton <emcnaughton@wikimedia.org>
2021-05-18 12:15:58 +1200

    Initial api to purge old thank yous
    Command:
     drush @wmff cvapi WMFDataManagement.ArchiveThankYou version=4 limit=50 CheckPermissions=0 BatchSize=700 EndTimeStamp=2016-01-01
    
    Defaults will be used if nothing is passed in - limit 10000, batchSize 1000, endTimeStamp = 1 year ago
    
    That will result in 10 queries of 1000 rows. On staging I've tested increasing limit substantially
    & it seems quick & non-locky with batchSize still set on the 1000 default
    
    Bug: T280587
    
    Change-Id: I2b5251c36c3ae2686745da6056f45a5b0fc4c83d

---
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/tests/Civi/Wmf/ArchiveThankYouTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Civi\Wmf;
+
+use Civi\Api4\Activity;
+use Civi\Api4\Contact;
+use Civi\Api4\WMFDataManagement;
+use Civi\Test;
+use Civi\Test\Api3TestTrait;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test our thank you cleanup.
+ *
+ * Tips:
+ *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
+ *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
+ *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
+ *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
+ *    If this test needs to manipulate schema or truncate tables, then either:
+ *       a. Do all that using setupHeadless() and Civi\Test.
+ *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ *
+ * @group headless
+ */
+class ArchiveThankYouTest extends TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+
+  use Api3TestTrait;
+
+  /**
+   * @return \Civi\Test\CiviEnvBuilder
+   * @throws \CRM_Extension_Exception_ParseException
+   */
+  public function setUpHeadless(): Test\CiviEnvBuilder {
+    // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+    // See: https://docs.civicrm.org/dev/en/latest/testing/phpunit/#civitest
+    return Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+  /***
+   * Test purging old thank you details
+   *
+   * @throws \API_Exception
+   */
+  public function testArchiveThankYou(): void {
+    $contactID = Contact::create(FALSE)
+      ->setValues(['first_name' => 'Bob', 'contact_type' => 'Individual'])
+      ->execute()->first()['id'];
+    $activityID = Activity::create(FALSE)->setValues([
+      'activity_type_id:name' => 'Email',
+      'activity_date_time' => '2 years ago',
+      'source_contact_id' => $contactID,
+      'subject' => 'Your € 11.00 gift = free knowledge for billions',
+      'details' => 'delete this please',
+    ])->execute()->first()['id'];
+    WMFDataManagement::archiveThankYou(FALSE)
+      ->setLimit(1)
+      ->execute();
+    $activity = Activity::get(FALSE)
+      ->addWhere('id', '=', $activityID)
+      ->addSelect('details', 'subject')
+      ->execute()->first();
+    $this->assertEquals('', $activity['details']);
+    $this->assertEquals('Your € 11.00 gift = free knowledge for billions', $activity['subject']);
+  }
+
+}
