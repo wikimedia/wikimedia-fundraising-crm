c0b47bb9c9de338fbbd54ca64db630997ec302bb
Eileen McNaughton <emcnaughton@wikimedia.org>
2021-10-13 16:48:48 +1300

    Superficial changes merged upstream
    https://lab.civicrm.org/extensions/deduper/-/commit/5ecd8f1c2f9a14617c91896cc6837616cba35397
    (I can't quite spot the other one but it is also superficial)
    
    Change-Id: I338e84ebfa1eeeb60c87b93501cee7284cd60a20

--- drupal/sites/default/civicrm/extensions/deduper/ang/deduper.ang.php
+++ drupal/sites/default/civicrm/extensions/deduper/ang/deduper.ang.php
@@ -1,33 +1,24 @@
 <?php
-// This file declares an Angular module which can be autoloaded
-// in CiviCRM. See also:
-// http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+// Declare Angular module
 
 return [
   'bundles' => ['bootstrap3'],
-  'js' =>
-    [
-      0 => 'ang/deduper.js',
-      1 => 'ang/deduper/*.js',
-      2 => 'ang/deduper/*/*.js',
-    ],
-  'css' =>
-    [
-      0 => 'ang/deduper.css',
-    ],
-  'partials' =>
-    [
-      0 => 'ang/deduper',
-    ],
-  'requires' =>
-    [
-      0 => 'crmUi',
-      1 => 'crmUtil',
-      2 => 'ngRoute',
-      3 => 'contactBasic',
-      4 => 'conflictBasic',
-    ],
-  'settings' =>
-    [
-    ],
+  'js' => [
+    'ang/deduper.js',
+    'ang/deduper/*.js',
+    'ang/deduper/*/*.js',
+  ],
+  'css' => [
+    'ang/deduper.css',
+  ],
+  'partials' => [
+    'ang/deduper',
+  ],
+  'requires' => [
+    'crmUi',
+    'crmUtil',
+    'ngRoute',
+    'contactBasic',
+    'conflictBasic',
+  ],
 ];
