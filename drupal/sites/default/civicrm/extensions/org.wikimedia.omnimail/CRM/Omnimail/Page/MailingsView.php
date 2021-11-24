60184b17c6e254e1bcc53df9b93e28915a14a7d7
Eileen McNaughton <emcnaughton@wikimedia.org>
2021-11-25 11:21:36 +1300

    Add link to Acoustic from mailing events
    In trying to dig into bug https://phabricator.wikimedia.org/T293587
    and update the docs too I found it a pain that we had the contact identifier
    but couldn't use it to to find the acoustic record.
    
    This adds a link to look up the contact in Acoustic based on the contact_identifier in
    mailing_provider_data table to the MailingEvents tab
    
    This sql will add a viewable row to see at
    https://wmff.localhost:32353/civicrm/contact/mailings/view?reset=1&force=1&cid=204
    
    INSERT INTO civicrm.civicrm_mailing_provider_data (contact_identifier, mailing_identifier, email,
    event_type, recipient_action_datetime, contact_id, is_civicrm_updated)
    VALUES ('490521770780', ' ', 'a@b.com', 'SENT', '2021-11-24 21:39:16', '204', 0);
    
    The alignment is a bit odd - I was gonna ignore that.
    
    Bug: T293587
    
    Change-Id: I79a77a914161991e71a7f73b138e653547e3cb6d

--- drupal/sites/default/civicrm/extensions/org.wikimedia.omnimail/CRM/Omnimail/Page/MailingsView.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.omnimail/CRM/Omnimail/Page/MailingsView.php
@@ -11,6 +11,7 @@ class CRM_Omnimail_Page_MailingsView extends CRM_Core_Page {
       'return' => [
         'event_type',
         'mailing_identifier',
+        'contact_identifier',
         'email',
         'recipient_action_datetime',
         'mailing_identifier.name',
@@ -18,8 +19,7 @@ class CRM_Omnimail_Page_MailingsView extends CRM_Core_Page {
       ],
       'sequential' => 1,
       'options' => ['limit' => 500, 'sort' => 'recipient_action_datetime DESC'],
-    ]);
-    $mailings = $mailings['values'];
+    ])['values'];
 
     foreach ($mailings as $index => $mailing) {
       $mailings[$index]['mailing_identifier'] = [
@@ -27,6 +27,13 @@ class CRM_Omnimail_Page_MailingsView extends CRM_Core_Page {
             'civicrm/mailing/view', 'reset=1&id=' . $mailing['mailing_identifier.id']
           ) . '" class="action-item crm-hover-button" title=' . ts("View Mailing") . '> ' . $mailing['mailing_identifier.name'] . '</a>' : $mailing['mailing_identifier']),
         'name' => (isset($mailing['mailing_identifier.name']) ? $mailing['mailing_identifier.name'] : $mailing['mailing_identifier']),
+      ];
+      $mailings[$index]['contact_identifier'] = [
+        'display' => (isset($mailing['contact_identifier']) ?
+          '<a href="https://engage4.silverpop.com/searchRecipient.do?action=edit&listId=9644238&recipientId=' . $mailing['contact_identifier']
+          . '" class="action-item crm-hover-button no-popup" title="' . ts("View Contact in Acoustic")
+          . '">' . $mailing['contact_identifier'] . '</a>' : ''),
+        'name' => $mailing['contact_identifier'],
       ];
     }
     //CRM_Core_Resources::singleton()->ad
