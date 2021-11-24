7da4276e4d183bca13c101bf27f3fcb8b8787df4
Eileen McNaughton <emcnaughton@wikimedia.org>
2021-11-25 10:22:03 +1300

    Update array formatting
    Change-Id: Ibd234a48b62a56b3f699bcd372de8171e7247f5d

--- drupal/sites/default/civicrm/extensions/org.wikimedia.omnimail/CRM/Omnimail/Page/MailingsView.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.omnimail/CRM/Omnimail/Page/MailingsView.php
@@ -6,21 +6,28 @@ class CRM_Omnimail_Page_MailingsView extends CRM_Core_Page {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(ts('MailingsView'));
 
-    $mailings = civicrm_api3('MailingProviderData', 'get', array(
+    $mailings = civicrm_api3('MailingProviderData', 'get', [
       'contact_id' => CRM_Utils_Request::retrieve('cid', 'Integer'),
-      'return' => array('event_type', 'mailing_identifier', 'email', 'recipient_action_datetime', 'mailing_identifier.name', 'mailing_identifier.id'),
+      'return' => [
+        'event_type',
+        'mailing_identifier',
+        'email',
+        'recipient_action_datetime',
+        'mailing_identifier.name',
+        'mailing_identifier.id',
+      ],
       'sequential' => 1,
-      'options' => array('limit' => 500, 'sort' => 'recipient_action_datetime DESC')
-    ));
+      'options' => ['limit' => 500, 'sort' => 'recipient_action_datetime DESC'],
+    ]);
     $mailings = $mailings['values'];
 
     foreach ($mailings as $index => $mailing) {
-      $mailings[$index]['mailing_identifier'] = array(
+      $mailings[$index]['mailing_identifier'] = [
         'display' => (isset($mailing['mailing_identifier.id']) ? '<a href="' . CRM_Utils_System::url(
-          'civicrm/mailing/view', 'reset=1&id=' . $mailing['mailing_identifier.id']
-          ) . '" class="action-item crm-hover-button" title=' . ts("View Mailing") . '> ' . $mailing['mailing_identifier.name'] . '</a>': $mailing['mailing_identifier']),
+            'civicrm/mailing/view', 'reset=1&id=' . $mailing['mailing_identifier.id']
+          ) . '" class="action-item crm-hover-button" title=' . ts("View Mailing") . '> ' . $mailing['mailing_identifier.name'] . '</a>' : $mailing['mailing_identifier']),
         'name' => (isset($mailing['mailing_identifier.name']) ? $mailing['mailing_identifier.name'] : $mailing['mailing_identifier']),
-      );
+      ];
     }
     //CRM_Core_Resources::singleton()->ad
     $this->assign('mailings', json_encode($mailings));
