5c268509c3caa584ecf943ef2113fabc0983365a
wfan <wfan@wikimedia.org>
2022-02-08 18:59:46 -0800

    Make country editable in email preferences
    Test Steps:
    1: UI, make update from https://localhost:9002/index.php/Special:EmailPreferences/emailPreferences?contact_hash=<civicrm_contact:hash>&contact_id=<civicrm_contact:id>
    2: Go to docker civi, run queue consume: `drush cvapi Preferencesqueue.consume time_limit=1000 max_batch_size=1`
    3: Go to db, and check if civicrm_address.country_id updated for this user
    4: Run PHPUNIT test: Go to docker civi, run `./vendor/bin/phpunit --filter testEmailPreferenceCenterUpdateApi`
    
    Bug: T280674
    Change-Id: I3f09ea67419c964fff554577e47bb25b4fef4a2b

--- drupal/sites/default/civicrm/extensions/wmf-civicrm/api/v3/Preferences/Create.php
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/api/v3/Preferences/Create.php
@@ -1,6 +1,8 @@
 <?php
 
 use Civi\Api4\Contact;
+use Civi\Api4\Address;
+use Civi\Api4\Country;
 
 /**
  * PreferencesQueue.consume API specification (optional)
@@ -23,18 +25,24 @@ function _civicrm_api3_preferences_create_spec(&$spec) {
     'api.required' => TRUE,
     'type' => CRM_Utils_Type::T_INT,
   ];
-   $spec['language'] = [
-     'name' => 'language',
-     'title' => 'Language',
-     'api.required' => TRUE,
-     'type' => CRM_Utils_Type::T_STRING,
-   ];
-    $spec['send_email'] = [
-      'name' => 'send_email',
-      'title' => 'Send Email',
-      'api.required' => TRUE,
-      'type' => CRM_Utils_Type::T_BOOLEAN,
-    ];
+  $spec['language'] = [
+    'name' => 'language',
+    'title' => 'Language',
+    'api.required' => TRUE,
+    'type' => CRM_Utils_Type::T_STRING,
+  ];
+  $spec['country'] = [
+    'name' => 'country',
+    'title' => 'Country',
+    'api.required' => TRUE,
+    'type' => CRM_Utils_Type::T_STRING,
+  ];
+  $spec['send_email'] = [
+    'name' => 'send_email',
+    'title' => 'Send Email',
+    'api.required' => TRUE,
+    'type' => CRM_Utils_Type::T_STRING,
+  ];
 }
 
 /**
@@ -49,10 +57,10 @@ function _civicrm_api3_preferences_create_spec(&$spec) {
  * @see civicrm_api3_create_success
  */
 function civicrm_api3_preferences_create(array $params): array {
-
   if (
     !preg_match('/^[0-9a-f]*$/', $params['contact_hash']) ||
-    !preg_match('/^[0-9a-zA-Z_-]*$/', $params['language'])
+    !preg_match('/^[0-9a-zA-Z_-]*$/', $params['language']) ||
+    !preg_match('/^[a-zA-Z]*$/', $params['country'])
   ) {
     throw new API_Exception('Invalid data in e-mail preferences message.', 'invalid_message');
   }
@@ -64,5 +72,48 @@ function civicrm_api3_preferences_create(array $params): array {
     ->addWhere('hash', '=', (string) $params['contact_hash'])
     ->addWhere('id', '=', (int) $params['contact_id'])
     ->execute()->first();
-  return civicrm_api3_create_success((array([$params['contact_id'] => (array) $result])), $params, 'Preferencesqueue', 'consume');
+
+  $address = Address::get(FALSE)
+    ->addSelect('country_id.iso_code')
+    ->addWhere('contact_id', '=', (int) $params['contact_id'])
+    ->addWhere('location_type_id:name', '=', 'EmailPreference')
+    ->execute();
+
+  $countryId = Country::get(FALSE)
+    ->addWhere('iso_code', '=', (string) $params['country'])
+    ->execute()
+    ->first()['id'];
+
+  Civi::log('wmf')
+    ->info("Email Preference Center update - civicrm_contact id: {$params['contact_id']}'s preferred_language to {$params['language']}, country to {$params['country']}($countryId) and civicrm_value_1_communication_4.opt_in to {$params['send_email']}.");
+  // country_id could be null, because we get the country options from CountryNames::getNames( $uiLang ),
+  // and here we update country_id in civicrm_address which has FK with civicrm_country.
+  // Need to confirm what should we do with the null country_id?
+  if(!$countryId){
+    Civi::log('wmf')
+      ->error("Email Preference Center update - civicrm_contact id: {$params['contact_id']}'s is trying to update preferred_language to {$params['language']}, country to {$params['country']}, and civicrm_value_1_communication_4.opt_in to {$params['send_email']}, but no country_id is been found from civicrm_country table.");
+  }
+
+  if (count($address) === 1) {
+    Address::update(FALSE)->setValues([
+      'country_id.iso_code' => (string) $params['country'],
+      'location_type_id:name' => 'EmailPreference',
+      'is_primary' => 1,
+    ])
+      ->addWhere('location_type_id:name', '=', 'EmailPreference')
+      ->addWhere('contact_id', '=', (int) $params['contact_id'])
+      ->execute();
+  }
+  else {
+    // Our UI currently use is_primary = 1's country, so do we update the EmailPreference is_primary to 1 and others to 0
+    // Or we do not update the is_primary just check if emailPreference exist for UI?
+    Address::create(FALSE)->setValues([
+      'location_type_id:name' => 'EmailPreference',
+      'country_id.iso_code' => (string) $params['country'],
+      'contact_id' => (int) $params['contact_id'],
+      'is_primary' => 1,
+    ])->execute();
+  }
+
+  return civicrm_api3_create_success(([[$params['contact_id'] => (array) $result]]), $params, 'Preferencesqueue', 'consume');
 }
--- drupal/sites/default/civicrm/extensions/wmf-civicrm/tests/phpunit/api/v3/Preferences/CreateTest.php
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/tests/phpunit/api/v3/Preferences/CreateTest.php
@@ -11,9 +11,11 @@ use Civi\Api4\Contact;
 /**
  * Civiproxy.Preferences API Test Case
  * This is a generic test class implemented with PHPUnit.
+ *
  * @group headless
  */
 class api_v3_Preferences_CreateTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+
   use Api3TestTrait;
 
   /**
@@ -49,33 +51,69 @@ class api_v3_Preferences_CreateTest extends \PHPUnit\Framework\TestCase implemen
    *
    * @throws \CRM_Core_Exception
    */
-  public function testApi(): void {
+  public function testEmailPreferenceCenterUpdateApi(): void {
     $contactID = Contact::create(FALSE)->setValues([
       'first_name' => 'Bob',
       'last_name' => 'Roberto',
-      'hash' => 'ab2',
-      'country' => 'US',
+      'hash' => '12341234',
       'Communication.opt_in' => 0,
       'contact_type' => 'Individual',
-      'preferred_language' => 'en_US',
-    ]) ->addChain('address', Address::create(FALSE)
+      'preferred_language' => 'en',
+    ])->addChain('address', Address::create(FALSE)
       ->addValue('contact_id', '$id')
-      ->addValue('country_id:label', 'United States')
+      ->addValue('country_id:label', 'Canada')
       ->addValue('location_type_id:name', 'Home')
+      ->addValue('is_primary', 1)
     )
       ->execute()->first()['id'];
 
     $this->callAPISuccess('Preferences', 'create', [
       'contact_id' => $contactID,
-      'contact_hash' => 'ab2',
-      'language' => 'es_ES',
-      'send_email' => TRUE,
+      'contact_hash' => '12341234',
+      'language' => 'es',
+      "country" => 'US',
+      'send_email' => 'true',
     ]);
-    $contact = Contact::get(FALSE)->addWhere('id', '=', $contactID)
-      ->setSelect(['preferred_language',  'Communication.opt_in'])
+
+    $contact = Contact::get(FALSE)->addWhere('id', '=', (int) $contactID)
+      ->setSelect(['preferred_language', 'Communication.opt_in'])
       ->execute()->first();
-    $this->assertEquals(TRUE, $contact['Communication.opt_in']);
-    $this->assertEquals('es_ES', $contact['preferred_language']);
+
+    $address = Address::get(FALSE)
+      ->addWhere('contact_id', '=', (int) $contactID)
+      ->addWhere('location_type_id:name', '=', 'EmailPreference')
+      ->addSelect('country_id.iso_code')
+      ->execute()
+      ->first();
+    echo "\nTest1: Email Preference Center update civicrm_contact id: $contactID's preferred_language to es, country to US(1228) and civicrm_value_1_communication_4.opt_in to 1.";
+
+    $this->assertEquals(1, $contact['Communication.opt_in']);
+    $this->assertEquals('es', $contact['preferred_language']);
+    $this->assertEquals('US', $address['country_id.iso_code']);
+
+    $this->callAPISuccess('Preferences', 'create', [
+      'contact_id' => $contactID,
+      'contact_hash' => '12341234',
+      'language' => 'pt-br',
+      "country" => 'AF',
+      'send_email' => 'false',
+    ]);
+
+    echo "\nTest2: Email Preference Center update civicrm_contact id: $contactID's preferred_language to pt-br, country to AF(1001) and civicrm_value_1_communication_4.opt_in to 0.";
+    $contact2 = Contact::get(FALSE)->addWhere('id', '=', (int) $contactID)
+      ->setSelect(['preferred_language', 'Communication.opt_in'])
+      ->execute()->first();
+
+    $address2 = Address::get(FALSE)
+      ->addWhere('contact_id', '=', (int) $contactID)
+      ->addWhere('location_type_id:name', '=', 'EmailPreference')
+      ->addSelect('country_id.iso_code')
+      ->execute()
+      ->first();
+
+    $this->assertEquals(0, $contact2['Communication.opt_in']);
+    $this->assertEquals('pt-br', $contact2['preferred_language']);
+    $this->assertEquals('AF', $address2['country_id.iso_code']);
   }
 
 }
