e728bc9430747ca37fb83071fd0b9e36945fc571
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-08-03 12:19:00 +1200

    Update to latest civiToken
    Bug: T313000
    Change-Id: I652fb159bf2dcb7e63989a3405eb0ed0cbd6ca20

---
+++ drupal/sites/default/civicrm/extensions/nz.co.fuzion.civitoken/Civi/Token/CiviTokens.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Civi\Token;
+
+use Civi\Token\Event\TokenRegisterEvent;
+use Civi\Token\Event\TokenValueEvent;
+use Civi\Token\TokenProcessor;
+use Civi\Token\TokenRow;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class to declare out CiviTokens to the TokenProcessor.
+ *
+ * We implement the EventSubscriberInterface because it seems to help us not
+ * have to make the functions static.
+ */
+class CiviTokens implements EventSubscriberInterface{
+
+  /**
+   * Get the implemented functions.
+   * @return array
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      'civi.token.list' => 'registerTokens',
+      'civi.token.eval' => 'evaluateTokens',
+    ];
+  }
+
+  /**
+   * Register the declared tokens.
+   *
+   * @param \Civi\Token\Event\TokenRegisterEvent $registerEvent
+   *   The registration event. Add new tokens using register().
+   *
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function registerTokens(TokenRegisterEvent $registerEvent): void {
+    if (!$this->checkActive($registerEvent->getTokenProcessor())) {
+      return;
+    }
+    foreach ($this->getTokenMetadata() as $entity => $field) {
+      foreach ($field as $fieldKey => $label) {
+        [, $tokenName] = explode('.', $fieldKey);
+        $registerEvent->register([
+          'entity' => $entity,
+          'field' => $tokenName,
+          'label' => $label,
+        ]);
+      }
+    }
+  }
+
+  /**
+   * Get metadata about the declared tokens.
+   *
+   * @return array
+   *
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function getTokenMetadata(): array {
+    $civiTokens = \Civi::cache('metadata')->get('civitoken_enabled_tokens');
+    if (!is_array($civiTokens)) {
+      $civiTokens = [];
+      civitoken_civicrm_tokens_all($civiTokens);
+      $setting = civicrm_api3('Setting', 'get', [
+        'return' => 'civitoken_enabled_tokens',
+        'sequential' => 1,
+      ])['values'][0];
+
+      if (empty($setting) || empty($setting['civitoken_enabled_tokens'])) {
+        // Treat un-configured as 'all enabled'.
+        \Civi::cache()->set('civitoken_enabled_tokens', $civiTokens);
+        return $civiTokens;
+      }
+
+      foreach ($civiTokens as $category => $tokenSubset) {
+        foreach ($tokenSubset as $key => $token) {
+          if (!in_array($key, $setting['civitoken_enabled_tokens'], TRUE)) {
+            unset($civiTokens[$category][$key]);
+          }
+        }
+        if (empty($civiTokens[$category])) {
+          unset($civiTokens[$category]);
+        }
+      }
+      \Civi::cache()->set('civitoken_enabled_tokens', $civiTokens);
+    }
+    return $civiTokens;
+  }
+
+  /**
+   * Determine whether this token-handler should be used with
+   * the given processor.
+   *
+   * To short-circuit token-processing in irrelevant contexts,
+   * override this.
+   *
+   * @param \Civi\Token\TokenProcessor $processor
+   * @return bool
+   */
+  public function checkActive(TokenProcessor $processor): bool {
+    return in_array($this->getEntityIDField(), $processor->context['schema'], TRUE)
+      // The mailing is not passing contactId in the context['schema'] - quick & dirty.
+      || in_array('mailingId', $processor->context['schema'], TRUE);
+  }
+
+  /**
+   * Get the field that would be in the schema context if this class is in use.
+   */
+  protected function getEntityIDField(): string {
+    return 'contactId';
+  }
+
+  /**
+   * Populate the token data.
+   *
+   * @param \Civi\Token\Event\TokenValueEvent $e
+   *   The event, which includes a list of rows and tokens.
+   */
+  public function evaluateTokens(TokenValueEvent $e) {
+    if (!$this->checkActive($e->getTokenProcessor())) {
+      return;
+    }
+
+    $activeTokens = $e->getTokenProcessor()->getMessageTokens();
+    $civitokens = array_intersect_key($this->getTokenMetadata(), $activeTokens);
+    if (empty($civitokens)) {
+      return;
+    }
+    $tokenFunctions = civitoken_initialize();
+    foreach ($e->getRows() as $row) {
+      if (empty($row->context['contactId'])) {
+        continue;
+      }
+
+      foreach ($civitokens as $type => $tokens) {
+        foreach ($activeTokens[$type] as $token) {
+          if (in_array($type, $tokenFunctions, TRUE) && isset($civitokens[$type][$type . '.' . $token])) {
+            $fn = $type . '_civitoken_get';
+            $result = [];
+            $fn($row->context['contactId'], $result);
+            foreach ($result as $key => $value) {
+              $tokenName = explode('.', $key)[1];
+              $row->format('text/html')->tokens($type, $tokenName, (string) $value);
+            }
+         }
+        }
+      }
+    }
+  }
+
+}
--- drupal/sites/default/civicrm/extensions/nz.co.fuzion.civitoken/tests/phpunit/CRM/CivitokenTest.php
+++ drupal/sites/default/civicrm/extensions/nz.co.fuzion.civitoken/tests/phpunit/CRM/CivitokenTest.php
@@ -1,9 +1,14 @@
 <?php
 
-use CRM_Civitoken_ExtensionUtil as E;
+use Civi\Test\Api3TestTrait;
+use Civi\Test\CiviEnvBuilder;
 use Civi\Test\HeadlessInterface;
 use Civi\Test\HookInterface;
 use Civi\Test\TransactionalInterface;
+use Civi\Token\TokenProcessor;
+use Civi\CiviTokens;
+use PHPUnit\Framework\TestCase;
+
 require_once 'BaseUnitTestClass.php';
 
 /**
@@ -20,8 +25,9 @@ require_once 'BaseUnitTestClass.php';
  *
  * @group headless
  */
-class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, HookInterface, TransactionalInterface {
+class CRM_CivitokenTest extends TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
 
+  use Api3TestTrait;
   public $ids;
 
   /**
@@ -30,7 +36,7 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
    * @return \Civi\Test\CiviEnvBuilder
    * @throws \CRM_Extension_Exception_ParseException
    */
-  public function setUpHeadless() {
+  public function setUpHeadless(): CiviEnvBuilder {
     // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
     // See: https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
     return \Civi\Test::headless()
@@ -38,34 +44,28 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
       ->apply();
   }
 
-  public function setUp() {
-    parent::setUp();
-  }
-
-  public function tearDown() {
-    parent::tearDown();
-  }
-
   /**
    * Test token hook function works.
+   *
+   * @throws \CiviCRM_API3_Exception
    */
-  public function testTokenHook() {
-    $tokens = array();
-    civitoken_civicrm_tokens($tokens);
-    $this->assertTrue(!empty($tokens));
+  public function testTokenHook(): void {
+    $processor = new \Civi\Token\CiviTokens();
+    $tokens = $processor->getTokenMetadata();
+
+    $this->assertNotEmpty($tokens);
     $this->assertEquals('Address Block', $tokens['address']['address.address_block']);
   }
 
   /**
    * Test token hook function is limited if a setting is used.
    *
-   * @throws \CRM_Core_Exception
    * @throws \CiviCRM_API3_Exception
    */
-  public function testTokenHookAlteredBySetting() {
-    $tokens = [];
-    $this->callAPISuccess('Setting', 'create', array('civitoken_enabled_tokens' => array('address.address_block')));
-    civitoken_civicrm_tokens($tokens);
+  public function testTokenHookAlteredBySetting(): void {
+    $this->callAPISuccess('Setting', 'create', ['civitoken_enabled_tokens' => ['address.address_block']]);
+    $processor = new \Civi\Token\CiviTokens();
+    $tokens = $processor->getTokenMetadata();
     $this->assertEquals(['address' => ['address.address_block' => 'Address Block']], $tokens);
   }
 
@@ -74,38 +74,37 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
    *
    * @throws \CiviCRM_API3_Exception
    */
-  public function testRelationShipTokens() {
-    $tokens = [];
-    civitoken_civicrm_tokens($tokens);
-    $this->assertNotEmpty($tokens);
+  public function testRelationShipTokens(): void {
+    $tokens = $this->getTokenProcessorTokens();
 
     $relationships = relationships_get_relationship_list();
-    foreach($relationships as $id => $label) {
-      $this->assertEquals($label . ' : Name of first contact found', $tokens['relationships']['relationships.display_name_' . $id]);
-      $this->assertEquals($label . ' : First Name of first contact found', $tokens['relationships']['relationships.first_name_' . $id]);
-      $this->assertEquals($label . ' : Last Name of first contact found', $tokens['relationships']['relationships.last_name_' . $id]);
-      $this->assertEquals($label . ' : Phone of first contact found', $tokens['relationships']['relationships.phone_' . $id]);
-      $this->assertEquals($label . ' : Email of first contact found', $tokens['relationships']['relationships.email_' . $id]);
-      $this->assertEquals($label . ' : ID of first contact found', $tokens['relationships']['relationships.id_' . $id]);
+    foreach ($relationships as $id => $label) {
+      $this->assertEquals($label . ' : Name of first contact found', $tokens['{relationships.display_name_' . $id . '}']);
+      $this->assertEquals($label . ' : First Name of first contact found', $tokens['{relationships.first_name_' . $id . '}']);
+      $this->assertEquals($label . ' : Last Name of first contact found', $tokens['{relationships.last_name_' . $id. '}']);
+      $this->assertEquals($label . ' : Phone of first contact found', $tokens['{relationships.phone_' . $id. '}']);
+      $this->assertEquals($label . ' : Email of first contact found', $tokens['{relationships.email_' . $id. '}']);
+      $this->assertEquals($label . ' : ID of first contact found', $tokens['{relationships.id_' . $id. '}']);
     }
   }
 
   /**
-   * Test token hook function is limited if a setting is used in this case for relationship.
+   * Test token hook function is limited if a setting is used in this case for
+   * relationship.
    *
    * @throws \CiviCRM_API3_Exception
-   * @throws \CRM_Core_Exception
    */
-  public function testRelationShipTokensAlteredBySettings() {
+  public function testRelationShipTokensAlteredBySettings(): void {
     $tokens = [];
     $tokens_to_enable = [];
     $relationships = relationships_get_relationship_list();
-    foreach($relationships as $id => $label) {
-      $tokens_to_enable[] = 'relationships.first_name_'.$id;
+    foreach ($relationships as $id => $label) {
+      $tokens_to_enable[] = 'relationships.first_name_' . $id;
     }
     $this->callAPISuccess('Setting', 'create', ['civitoken_enabled_tokens' => $tokens_to_enable]);
-    civitoken_civicrm_tokens($tokens);
-    foreach($relationships as $id => $label) {
+    $processor = new \Civi\Token\CiviTokens();
+    $tokens = $processor->getTokenMetadata();
+    foreach ($relationships as $id => $label) {
       $this->assertEquals($label . ' : First Name of first contact found', $tokens['relationships']['relationships.first_name_' . $id]);
     }
   }
@@ -113,12 +112,12 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
   /**
    * Test contribution tokens for a contact with no contributions.
    *
-   * This tests that regression from https://github.com/eileenmcnaughton/nz.co.fuzion.civitoken/pull/18
-   * whereby an exception was thrown for contact without contributions stays fixed.
+   * This tests that regression from
+   * https://github.com/eileenmcnaughton/nz.co.fuzion.civitoken/pull/18 whereby
+   * an exception was thrown for contact without contributions stays fixed.
    *
-   * @throws \CRM_Core_Exception
    */
-  public function testContributionTokenNull() {
+  public function testContributionTokenNull(): void {
     $contributionTokens = [
       'latestcontribs.softcredit_name',
       'latestcontribs.softcredit_type',
@@ -128,51 +127,114 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
     $this->ids['contact'][0] = $this->individualCreate();
     $this->ids['contact'][1] = $this->individualCreate();
     $this->ids['contribution'][0] = $this->callAPISuccess('Contribution', 'create', [
-      'api.ContributionSoft.create' => ['amount' => 5, 'contact_id' => $this->ids['contact'][1], 'soft_credit_type_id' => 'in_memory_of'],
+      'api.ContributionSoft.create' => [
+        'amount' => 5,
+        'contact_id' => $this->ids['contact'][1],
+        'soft_credit_type_id' => 'in_memory_of',
+      ],
       'total_amount' => 10,
       'contact_id' => $this->ids['contact'][0],
       'financial_type_id' => 'Donation',
     ]);
-    $values = [];
-    civitoken_civicrm_tokenValues($values, [$this->ids['contact'][0]], NULL, ['latestcontribs' => ['softcredit_name', 'softcredit_type']]);
-    $this->assertEquals('In Memory of', $values[$this->ids['contact'][0]]['latestcontribs.softcredit_type']);
+    $rendered = $this->processTokens(['latestcontribs.softcredit_name', 'latestcontribs.softcredit_type']);
+    $this->assertEquals('bob In Memory of',  $rendered);
+  }
+
+  /**
+   * Test that no fatal error occurs when all tokens are requested.
+   */
+  public function testNoFatal(): void {
+    $this->ids['contact'][0] = $this->individualCreate();
+    $tokens = $this->getTokenProcessorTokens();
+    $string = '';
+    foreach ($tokens as $token => $label) {
+      $string.= $label . ': ' . $token . "\n";
+    }
+
+    // This is a test  it doesn't matter if it's not supported - everything that IS supported is hard to use.
+    $rendered = $this->render(['text' => $string]);
+    $this->assertStringContainsString('Communication Style: Formal', $rendered['text']);
+  }
+
+  /**
+   * Render some template(s), evaluating token expressions and Smarty expressions.
+   *
+   * Copied from Core as it is internal in core so should not call....
+   *
+   * This helper simplifies usage of hybrid notation. As a simplification, it may not be optimal for processing
+   * large batches (e.g. CiviMail or scheduled-reminders), but it's a little more convenient for 1-by-1 use-cases.
+   *
+   * @param array $messages
+   *   Message templates. Any mix of the following templates ('text', 'html', 'subject', 'msg_text', 'msg_html', 'msg_subject').
+   *   Ex: ['subject' => 'Hello {contact.display_name}', 'text' => 'What up?'].
+   *   Note: The content-type may be inferred by default. A key like 'html' or 'msg_html' indicates HTML formatting; any other key indicates text formatting.
+   * @param array $tokenContext
+   *   Ex: ['contactId' => 123, 'activityId' => 456]
+   * @param array|null $smartyAssigns
+   *   List of data to export via Smarty.
+   *   Data is only exported temporarily (long enough to execute this render() method).
+   * @return array
+   *   Rendered messages. These match the various inputted $messages.
+   *   Ex: ['msg_subject' => 'Hello Bob Roberts', 'msg_text' => 'What up?']
+   * @internal
+   */
+  protected function render(array $messages, $format = 'text/html'): array {
+    $result = [];
+    $tokenContextDefaults = [
+      'controller' => __CLASS__,
+      'smarty' => TRUE,
+      'schema' => ['contactId'],
+    ];
+    $tokenProcessor = new TokenProcessor(\Civi::dispatcher(), $tokenContextDefaults);
+    $tokenProcessor->addRow(['contactId' => $this->ids['contact'][0]]);
+
+    // Load templates
+    foreach ($messages as $messageId => $messageTpl) {
+      $tokenProcessor->addMessage($messageId, $messageTpl, $format);
+    }
+
+    // Evaluate/render templates
+    $tokenProcessor->evaluate();
+    foreach ($messages as $messageId => $ign) {
+      foreach ($tokenProcessor->getRows() as $row) {
+        $result[$messageId] = $row->render($messageId);
+      }
+    }
+    return $result;
   }
 
   /**
    * Test address token renders state.
-   *
-   * @throws \CRM_Core_Exception
    */
-  public function testAddressToken() {
+  public function testAddressToken(): void {
     $this->ids['contact'][0] = $this->individualCreate();
-    $this->callAPISuccess('Address', 'create', ['contact_id' => $this->ids['contact'][0], 'city' => 'Baltimore', 'state_province_id' => 'Maine', 'country_id' => 'US']);
+    $this->callAPISuccess('Address', 'create', [
+      'contact_id' => $this->ids['contact'][0],
+      'city' => 'Baltimore',
+      'state_province_id' => 'Maine',
+      'country_id' => 'US',
+    ]);
     $tokens = ['address.address_block'];
-    $values = $this->processTokens($tokens);
-    $this->assertEquals([
-      'address.address_conditional_country' => 'UNITED STATES',
-      'address.address_block_text' => 'bob
-Baltimore, ME
-UNITED STATES
-',
-      'address.address_block' => 'bob<br />
+    $rendered = $this->processTokens($tokens);
+    $this->assertEquals('bob<br />
 Baltimore, ME<br />
 UNITED STATES<br />
-',
-    ], $values[$this->ids['contact'][0]]
-    );
-    $this->callAPISuccess('Setting', 'create', ['mailing_format' => '{contact.addressee}
+', $rendered);
+    $this->callAPISuccess('Setting', 'create', [
+      'mailing_format' => '{contact.addressee}
 {contact.street_address}
 {contact.supplemental_address_1}
 {contact.city}
 {contact.state_province_name}
 {contact.postal_code}
-{contact.country}']);
-    $values = $this->processTokens($tokens);
+{contact.country}',
+    ]);
+    $rendered = $this->processTokens($tokens, 'text/plain');
     $this->assertEquals('bob
 Baltimore
 Maine
 UNITED STATES
-', $values[$this->ids['contact'][0]]['address.address_block_text']);
+', $rendered);
   }
 
   /**
@@ -180,10 +242,12 @@ UNITED STATES
    *
    * @return int
    *
-   * @throws \CRM_Core_Exception
    */
   protected function individualCreate(): int {
-    return (int) $this->callAPISuccess('Contact', 'create', ['contact_type' => 'Individual', 'first_name' => 'bob'])['id'];
+    return (int) $this->callAPISuccess('Contact', 'create', [
+      'contact_type' => 'Individual',
+      'first_name' => 'bob',
+    ])['id'];
   }
 
   /**
@@ -191,20 +255,41 @@ UNITED STATES
    *
    * @param array $tokens
    *
-   * @return array
-   * @throws \CRM_Core_Exception
+   * @return string
    */
-  protected function processTokens(array $tokens): array {
+  protected function processTokens(array $tokens, $format = 'text/html'): string {
     $this->callAPISuccess('Setting', 'create', ['civitoken_enabled_tokens' => $tokens]);
-    $values = [];
-    $parsedTokens = [];
-    foreach ($tokens as $token) {
-      $split = explode('.', $token);
-      $parsedTokens[$split[0]] = $split[1];
+    Civi::cache()->delete('civitoken_enabled_tokens');
+    $text = '{' . implode('} {' , $tokens) . '}';
+    return $this->render(['text' => $text], $format)['text'];
+  }
+
+  /**
+   * Get the tokens declared via the token processor.
+   *
+   * @return array
+   */
+  protected function getTokenProcessorTokens(): array {
+    $tokenProcessor = new TokenProcessor(\Civi::dispatcher(), [
+      'schema' => ['contactId'],
+    ]);
+    return $tokenProcessor->listTokens();
+  }
+
+  /**
+   * @param string $message
+   *
+   * @return string
+   */
+  protected function getRendered(string $message): string {
+    $tokenProcessor = new TokenProcessor(\Civi::dispatcher(), []);
+    $tokenProcessor->addRow(['contactId' => [$this->ids['contact'][0]]]);
+    $tokenProcessor->addMessage('text', $message, 'text/html');
+    $tokenProcessor->evaluate();
+    // Display mail-merge data.
+    foreach ($tokenProcessor->getRows() as $row) {
+      return $row->render('text');
     }
-    \Civi::cache()->delete('civitoken_enabled_tokens');
-    civitoken_civicrm_tokenValues($values, [$this->ids['contact'][0]], NULL, $parsedTokens);
-    return $values;
   }
 
 }
