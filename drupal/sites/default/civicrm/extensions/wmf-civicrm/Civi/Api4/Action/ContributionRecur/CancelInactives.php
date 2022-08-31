f58e25d087fb793e0b9374817e40b1823139268f
Elliott Eggleston <ejegg@ejegg.com>
2022-08-30 21:26:25 -0400

    Add ContributionRecur::cancelInactives API4 Action
    Sets status to cancelled for any recurring contributions whose
    next_sched_contribution_date is more than N days ago (default 60).
    
    Run with
    
    drush @wmff cvapi ContributionRecur.cancelInactives version=4 days=100
    
    -or-
    
    https://wmff.localhost:32353/civicrm/api4#/explorer/ContributionRecur/cancelInactives
    
    SQL to reset donations in your dev database if you want to try again:
    
    update civicrm_contribution_recur
    set contribution_status_id=5,
    cancel_reason=null,
    cancel_date=null,
    next_sched_contribution_date = date_add(start_date, INTERVAL 5 DAY);
    
    Bug: T315102
    Change-Id: Iecea7dd3dd8c1043f76d3d6e3fb1ff02e3ff1803

---
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/Civi/Api4/Action/ContributionRecur/CancelInactives.php
@@ -0,0 +1,44 @@
+<?php
+namespace Civi\Api4\Action\ContributionRecur;
+
+use Civi\Api4\ContributionRecur;
+use Civi\Api4\Generic\AbstractAction;
+use Civi\Api4\Generic\Result;
+
+/**
+ * Changes status to cancelled for any recurring contributions
+ * that haven't gotten a contribution for at least two months
+ * @method setDays(int $days) set the number of days
+ */
+class CancelInactives extends AbstractAction {
+
+  /**
+   * @var int number of days past due to consider inactive
+   */
+  protected $days = 60;
+
+  public function _run( Result $result ) {
+    $limitDate = date('Y-m-d', strtotime("-$this->days days"));
+    $inactives = ContributionRecur::get(FALSE)
+      ->addWhere('contribution_status_id', 'NOT IN', [1,3,4])
+      ->addWhere('next_sched_contribution_date', '<', $limitDate)
+      ->addWhere('cancel_date', 'IS NULL')
+      ->execute();
+
+    foreach($inactives as $inactive) {
+      ContributionRecur::update(FALSE)
+        ->addWhere('id', '=', $inactive['id'])
+        ->setValues([
+          'contribution_status_id.name' => 'Cancelled',
+          'cancel_date' => date('now'),
+          'cancel_reason' => 'Automatically cancelled for inactivity'
+        ])->execute();
+      $result[] = [
+        'id' => $inactive['id'],
+        'start_date' => $inactive['start_date'],
+        'next_sched_contribution_date' => $inactive['next_sched_contribution_date'],
+        'contact_id' => $inactive['contact_id']
+      ];
+    }
+  }
+}
