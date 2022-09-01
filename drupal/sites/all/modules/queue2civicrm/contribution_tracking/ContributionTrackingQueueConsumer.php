0017904886399b0f145fbac6b876b6a4ab777899
Elliott Eggleston <ejegg@ejegg.com>
2022-09-01 17:42:05 -0400

    Move contribution_source population into code
    DEPLOY NOTE: need to stop the contribution tracking QC job and
    drop triggers on the contribution_tracking table before pushing this
    out.
    
    Now that all the changes to this table go through the c_t queue
    consumer, we can parse the contribution_source columns out in code
    and not in this mysterious trigger.
    
    Bug: T316949
    Change-Id: I670770b069580f93fd67dbc42854364df5035f61

--- drupal/sites/all/modules/queue2civicrm/contribution_tracking/ContributionTrackingQueueConsumer.php
+++ drupal/sites/all/modules/queue2civicrm/contribution_tracking/ContributionTrackingQueueConsumer.php
@@ -8,7 +8,8 @@ use wmf_common\WmfQueueConsumer;
 class ContributionTrackingQueueConsumer extends WmfQueueConsumer {
 
   /**
-   * Normalise the queue message and insert into the contribution tracking tbl
+   * Normalise the queue message and insert into the contribution_tracking
+   * and contribution_source tables
    *
    * @param array $message contribution-tracking queue message
    *
@@ -46,9 +47,10 @@ class ContributionTrackingQueueConsumer extends WmfQueueConsumer {
         'ts',
       ]);
     }, ARRAY_FILTER_USE_KEY);
+    $csData = $this->getContributionSourceData($ctData);
 
     $ctData = $this->truncateFields($ctData);
-    $this->persistContributionTrackingData($ctData);
+    $this->persistContributionTrackingData($ctData, $csData);
 
     $ContributionTrackingStatsCollector = ContributionTrackingStatsCollector::getInstance();
     $ContributionTrackingStatsCollector->recordContributionTrackingRecord();
@@ -57,13 +59,14 @@ class ContributionTrackingQueueConsumer extends WmfQueueConsumer {
   /**
    * Insert or update a contribution tracking entry.
    *
-   * @param array $ctData data to be written to contribution tracking tbl
+   * @param array $ctData data to be written to contribution_tracking tbl
+   * @param array|null $csData data to be written to contribution_source tbl
    *
    * @throws \Exception
    */
-  protected function persistContributionTrackingData($ctData) {
+  protected function persistContributionTrackingData(array $ctData, ?array $csData) {
     $checkSql = "
-SELECT id, contribution_id FROM contribution_tracking
+SELECT id, contribution_id, utm_source FROM contribution_tracking
 WHERE id = :ct_id
 LIMIT 1";
 
@@ -96,12 +99,52 @@ LIMIT 1";
         ->fields($ctData)
         ->condition('id', $ctData['id'])
         ->execute();
+
+      // Only update contribution_source if message has a changed utm_source
+      if ($csData !== NULL && $existingRow['utm_source'] !== $ctData['utm_source']) {
+        db_update('contribution_source')
+          ->fields($csData)
+          ->condition('contribution_tracking_id', $ctData['id'])
+          ->execute();
+      }
     }
     else {
       db_insert('contribution_tracking')
         ->fields($ctData)
         ->execute();
+
+      if ($csData !== NULL) {
+        $csData['contribution_tracking_id'] = $ctData['id'];
+        db_insert('contribution_source')
+          ->fields($csData)
+          ->execute();
+      }
     }
+  }
+
+  /**
+   * @param array $msg Original (untruncated) contribution-tracking queue message
+   * @return array|null data for contribution_source table, or null if no good data found
+   */
+  protected function getContributionSourceData(array $msg): ?array {
+    if (empty($msg['utm_source'])) {
+      return null;
+    }
+    $source = $msg['utm_source'];
+    // Sometimes it's just dots. Skip it instead of writing an empty row
+    if (empty(str_replace('.', '', $source))) {
+      return null;
+    }
+    // Usually just 3 segments, 4 when payment_submethod is specified, e.g. for iDEAL
+    $exploded = explode('.', $source);
+    if (count($exploded) > 4 || count($exploded) < 3) {
+      return null;
+    }
+    return [
+      'banner' => mb_substr($exploded[0], 0, 128),
+      'landing_page' => mb_substr($exploded[1], 0, 128),
+      'payment_method' => mb_substr($exploded[2], 0, 128)
+    ];
   }
 
   protected function truncateFields($msg) {
