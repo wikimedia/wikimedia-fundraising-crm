507bff54f52604084833e8a28c757acd0f9952d2
Elliott Eggleston <ejegg@ejegg.com>
2022-09-06 14:16:53 -0400

    Update contribution_source backfill for iDEAL
    Make it work with rows that have three dots.
    
    To test,
    * populate your contribution_tracking table with drush @wmff ctqc
    * run the OLD version of this SQL script and note that it doesn't
      create rtbt rows when the source also includes .rtbt_ideal
    * run this version of this SQL script and see the new rtbt rows
    
    Bug: T316949
    Change-Id: Iaa5dffef9342f13aa04c5549be4ed3444ab8e3a5

--- drupal/sites/all/modules/contribution_tracking/contribution_source_backfill.sql
+++ drupal/sites/all/modules/contribution_tracking/contribution_source_backfill.sql
@@ -1,23 +1,19 @@
 -- Copied over from old ContributionTracking wiki extension
 -- https://phabricator.wikimedia.org/diffusion/ECNT/browse/master/patches/patch-contribution_source_table.sql;22cb54fc457b7b7d7aaaa38d86393b65ff5ae809
-
--- CAREFULLY backfill.  Run this statement manually
--- and verify that we aren’t locking the contribution
--- tracking table, after a few seconds.  Abort if so.
--- Run in limited batches until the coast is clear.
-insert ignore into contribution_source
+-- Updated to work with iDEAL rows that have 3 dots
+-- Backfill with the queues off.
+insert into contribution_source
 select
   ct.id as contribution_tracking_id,
   substring_index(ct.utm_source, '.', 1) as banner,
   substring_index(substring_index(ct.utm_source, '.', 2), '.', -1) as landing_page,
-  substring_index(ct.utm_source, '.', -1) as payment_method
+  substring_index(substring_index(ct.utm_source, '.', 3), '.', -1) as payment_method
 from contribution_tracking ct
        left join contribution_source cs
                  on ct.id = cs.contribution_tracking_id
 where
   cs.contribution_tracking_id is null
-  and (ct.utm_source is not null
-  and (length(ct.utm_source) - length(replace(ct.utm_source, '.', ''))) = 2
-  )
-limit 1000
-;
+  and ct.utm_source is not null
+  and length(ct.utm_source) - length(replace(ct.utm_source, '.', '')) BETWEEN 2 AND 3
+  and ct.utm_source <> '..'
+  and ct.utm_source <> '...';
