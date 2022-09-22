1be85ef0df65f01d9228cae4b491ca97e120a50b
cstone <cstone@wikimedia.org>
2022-09-22 00:56:46 +0000

    Create wmf-audit
    Bug: T314753
    Change-Id: I2bc64e0c4f4f9dfd91b80c62dd2dc29c4bcdcbbb

---
+++ drupal/sites/default/civicrm/extensions/wmf-audit/wmf_audit.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once 'wmf_audit.civix.php';
+// phpcs:disable
+use CRM_WmfAudit_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function wmf_audit_civicrm_config(&$config) {
+  _wmf_audit_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function wmf_audit_civicrm_install() {
+  _wmf_audit_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function wmf_audit_civicrm_postInstall() {
+  _wmf_audit_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function wmf_audit_civicrm_uninstall() {
+  _wmf_audit_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function wmf_audit_civicrm_enable() {
+  _wmf_audit_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function wmf_audit_civicrm_disable() {
+  _wmf_audit_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function wmf_audit_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _wmf_audit_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function wmf_audit_civicrm_entityTypes(&$entityTypes) {
+  _wmf_audit_civix_civicrm_entityTypes($entityTypes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function wmf_audit_civicrm_preProcess($formName, &$form) {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function wmf_audit_civicrm_navigationMenu(&$menu) {
+//  _wmf_audit_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _wmf_audit_civix_navigationMenu($menu);
+//}
