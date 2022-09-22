5a9c82c20e83ced24c94c88c1a1ad6bfe337e18f
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-09-22 17:18:30 +1200

    Run civix upgrade on rip extension
    Bug: T318304
    Change-Id: I5af144c8342dedc472dc80ceae905a49de422d14

--- drupal/sites/default/civicrm/extensions/org.wikimedia.rip/rip.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.rip/rip.php
@@ -12,17 +12,6 @@ function rip_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function rip_civicrm_xmlMenu(&$files) {
-  _rip_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -75,56 +64,6 @@ function rip_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function rip_civicrm_managed(&$entities) {
-  _rip_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function rip_civicrm_caseTypes(&$caseTypes) {
-  _rip_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function rip_civicrm_angularModules(&$angularModules) {
-_rip_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function rip_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _rip_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_pre().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_pre
@@ -144,9 +83,8 @@ function rip_civicrm_pre($op, $objectName, $id, &$params) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function rip_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
@@ -164,3 +102,21 @@ function rip_civicrm_navigationMenu(&$menu) {
   ));
   _rip_civix_navigationMenu($menu);
 } // */
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function rip_civicrm_postInstall() {
+  _rip_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function rip_civicrm_entityTypes(&$entityTypes) {
+  _rip_civix_civicrm_entityTypes($entityTypes);
+}
