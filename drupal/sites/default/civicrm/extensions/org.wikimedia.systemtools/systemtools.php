2d2afcb370ae406998ffa1cdb61ab4eb758c1fa7
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-09-28 12:51:51 +1300

    Civix upgrade on systemtools
    Bug: T318304
    Change-Id: I1c7a494031e90465e9c1e8330e41239b2ec43526

--- drupal/sites/default/civicrm/extensions/org.wikimedia.systemtools/systemtools.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.systemtools/systemtools.php
@@ -19,15 +19,6 @@ function systemtools_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function systemtools_civicrm_xmlMenu(&$files) {
-  _systemtools_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -82,54 +73,6 @@ function systemtools_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function systemtools_civicrm_managed(&$entities) {
-  _systemtools_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function systemtools_civicrm_caseTypes(&$caseTypes) {
-  _systemtools_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function systemtools_civicrm_angularModules(&$angularModules) {
-  _systemtools_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function systemtools_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _systemtools_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_entityTypes().
  *
  * Declare entity types provided by this module.
@@ -140,13 +83,6 @@ function systemtools_civicrm_entityTypes(&$entityTypes) {
   _systemtools_civix_civicrm_entityTypes($entityTypes);
 }
 
-/**
- * Implements hook_civicrm_thems().
- */
-function systemtools_civicrm_themes(&$themes) {
-  _systemtools_civix_civicrm_themes($themes);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -154,9 +90,8 @@ function systemtools_civicrm_themes(&$themes) {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
  *
-function systemtools_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
