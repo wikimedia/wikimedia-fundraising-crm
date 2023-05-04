3fe986d5c2c76596d0cb4b340114d46d0d75e355
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-05-04 12:05:05 +1200

    Run civix.upgrade on thank you tests
    For some reason some tests are not running - not sure this will
    help but it rules it out as the issue...
    
    Change-Id: Ibd8706bb06238a0f6d84792b62c78cf383e8a4d9

--- drupal/sites/default/civicrm/extensions/wmf-thankyou/wmf_thankyou.php
+++ drupal/sites/default/civicrm/extensions/wmf-thankyou/wmf_thankyou.php
@@ -22,59 +22,12 @@ function wmf_thankyou_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function wmf_thankyou_civicrm_postInstall() {
-  _wmf_thankyou_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function wmf_thankyou_civicrm_uninstall() {
-  _wmf_thankyou_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function wmf_thankyou_civicrm_enable() {
   _wmf_thankyou_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function wmf_thankyou_civicrm_disable() {
-  _wmf_thankyou_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function wmf_thankyou_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _wmf_thankyou_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function wmf_thankyou_civicrm_entityTypes(&$entityTypes) {
-  _wmf_thankyou_civix_civicrm_entityTypes($entityTypes);
 }
 
 function wmf_thankyou_civicrm_links($op, $objectName, $objectId, &$links, &$mask, &$values) {
