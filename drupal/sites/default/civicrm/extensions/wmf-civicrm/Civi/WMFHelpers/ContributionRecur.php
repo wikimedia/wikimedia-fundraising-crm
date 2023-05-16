11f2b8a8763af51b78b339714ef2671e7014287c
wfan <wfan@wikimedia.org>
2023-05-16 15:13:33 -0700

    Add contribution helper function to check if passed contact id and checksum valid for upgrade
    When checksum valid, find all related (share same primary email address) contacts'
    valid recurring recorders (filter out multi recurring, and only accept adyen and ingenico for now)
    
    Test:
    drush @wmff -vv cvapi ContributionRecur.getUpgradableRecur version=4 contact_id=<> checksum=<>
    For valid contact id's checksum, can get as
    SELECT id, CONCAT(MD5(CONCAT(hash, '_', id, '_', UNIX_TIMESTAMP(), '_', '720')),"_",UNIX_TIMESTAMP(),"_","720") FROM civicrm.civicrm_contact;
    
    Bug: T335788
    Change-Id: I3be75a62534044293fbee5eaa1726788c800b05e

--- drupal/sites/default/civicrm/extensions/wmf-civicrm/Civi/WMFHelpers/ContributionRecur.php
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/Civi/WMFHelpers/ContributionRecur.php
@@ -42,4 +42,54 @@ class ContributionRecur {
     }
     return self::$inactiveStatuses;
   }
+
+  /**
+   * If recur record is upgradable (non-upgradeable like PayPal, Dlocal India recurring )
+   * return contribution_recur.id, contact_id, currency, amount, donor_name and next_sched_contribution_date
+   *
+   * @param int $contact_id
+   * @param string $checksum
+   *
+   * @return array
+   * @throws \CRM_Core_Exception
+   * @throws \Civi\API\Exception\UnauthorizedException
+   */
+  public static function getUpgradeable(int $contact_id, string $checksum): array {
+    $result = [];
+    // check if valid checksum
+    if (\CRM_Contact_BAO_Contact_Utils::validChecksum($contact_id, $checksum)) {
+      \Civi::log('wmf')->info("Donor '$contact_id' has valid checksum");
+      $allContactIds = Contact::duplicateContactIds($contact_id);
+      \Civi::log('wmf')->info("Check if donor " . json_encode($allContactIds) . " upgradable");
+      $contribution_recurs = \Civi\Api4\ContributionRecur::get(FALSE)
+        ->addSelect('id', 'currency', 'amount', 'next_sched_contribution_date', 'contact.display_name')
+        ->addJoin('Contact AS contact', 'LEFT', ['contact.id', '=', $contact_id])
+        ->addJoin('PaymentProcessor AS payment_processor', 'LEFT', ['payment_processor.id', '=', 'payment_processor_id'])
+        ->addWhere('contact_id', 'IN', $allContactIds)
+        ->addWhere('cancel_date', 'IS NULL')
+        ->addWhere('next_sched_contribution_date', 'IS NOT NULL')
+        // use adyen and ingenico first, find dlocal except upi later
+        ->addWhere('payment_processor.name', 'IN', ['adyen', 'ingenico'])
+        ->execute();
+      // Also filter out multi recurring e.g. contact 1925710.
+      if (count($contribution_recurs) === 1) {
+        $result[] = [
+          'id' => $contribution_recurs[0]['id'],
+          'contact_id' => $contact_id,
+          'currency' => $contribution_recurs[0]['currency'],
+          'amount' => $contribution_recurs[0]['amount'],
+          'donor_name' => $contribution_recurs[0]['contact.display_name'],
+          'next_sched_contribution_date' => $contribution_recurs[0]['next_sched_contribution_date'],
+        ];
+      }
+      else {
+        \Civi::log('wmf')->info("Donor '$contact_id' has " . count($contribution_recurs) . " valid recurring");
+      }
+    }
+    else {
+      \Civi::log('wmf')->info("Donor '$contact_id' checksum not valid");
+    }
+    return $result;
+  }
+
 }
