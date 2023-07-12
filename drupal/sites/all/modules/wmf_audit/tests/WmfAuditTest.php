da0d9a41014ff7c6e96fbe638b16a6f7450dc8b7
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-07-12 17:02:20 +1200

    Follow up tweaks on wmf_audit patch
    This builds on https://gerrit.wikimedia.org/r/c/wikimedia/fundraising/crm/+/919940/8
    with the primary goal being to change the check on $result as it would
    be checking $result['id'] on NULL where the row is not found.
    
    But, When I changed that it was really obvious that the if should be combined
    with the if($result) below so I did that, with a minor re-ordering to early-return
    cos that seemed clearer. Of course it makes the patch hard to read with the white
    space changes...
    
    Lastly I added the lines to the test I used to check the no result scenario
    and made peace with my spell-checker
    
    Bug: T335711
    
    Change-Id: If56970f85b332185658082aeb09dc0666873eb7a

--- drupal/sites/all/modules/wmf_audit/tests/WmfAuditTest.php
+++ drupal/sites/all/modules/wmf_audit/tests/WmfAuditTest.php
@@ -8,11 +8,11 @@ use Civi\Api4\ContributionTracking;
  */
 class WmfAuditTest extends BaseAuditTestCase {
 
-  public function testGetContributionTrackingData() {
+  public function testGetContributionTrackingData(): void {
     $expectedContributionTrackingData = [
       'utm_source' => 'testBanner.default~default~default~default~control.cc',
-      'utm_medium' => 'testmedium',
-      'utm_campaign' => 'testcampaign',
+      'utm_medium' => 'test_medium',
+      'utm_campaign' => 'test_campaign',
       'language' => 'en',
       // It is called tracking_date in the database but is normalized to date for the rest of the audit code
       // The audit also expects a Unix timestamp
@@ -22,7 +22,11 @@ class WmfAuditTest extends BaseAuditTestCase {
 
     $all_data['contribution_tracking_id'] = $this->createContributionTracking();
     $contribution_tracking_data = wmf_audit_get_contribution_tracking_data($all_data);
-    $this->assertEquals($expectedContributionTrackingData,$contribution_tracking_data);
+    $this->assertEquals($expectedContributionTrackingData, $contribution_tracking_data);
+
+    // Now check that nothing weird happens when it doesn't exist.
+    ContributionTracking::delete(FALSE)->addWhere('id', '=', $all_data['contribution_tracking_id'])->execute();
+    $this->assertFalse(wmf_audit_get_contribution_tracking_data($all_data));
   }
 
   private function createContributionTracking() {
@@ -31,8 +35,8 @@ class WmfAuditTest extends BaseAuditTestCase {
     $data = [
       'id' => $id+1,
       'utm_source' => 'testBanner.default~default~default~default~control.cc',
-      'utm_medium' => 'testmedium',
-      'utm_campaign' => 'testcampaign',
+      'utm_medium' => 'test_medium',
+      'utm_campaign' => 'test_campaign',
       'language' => 'en',
       'tracking_date' => '2023-05-16 04:38:00'
     ];
