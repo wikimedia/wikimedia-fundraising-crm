d287f229a60890e8d2105d48e715a369db6ed808
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-09-06 09:53:13 +1200

    Reformat code
    Bug: T340064
    Change-Id: Ic9214a6273f18f0d82a82aa74b5316e351ce5662

--- drupal/sites/all/modules/wmf_communication/tests/phpunit/CiviMailTest.php
+++ drupal/sites/all/modules/wmf_communication/tests/phpunit/CiviMailTest.php
@@ -1,99 +1,102 @@
 <?php
+
 namespace wmf_communication;
 
 /**
  * Tests for CiviMail helper classes
+ *
  * @group CiviMail
  * @group WmfCommunication
  */
 class CiviMailTest extends CiviMailTestBase {
 
-	public function testAddMailing() {
-		$name = 'test_mailing';
-		$revision = mt_rand();
-		$storedMailing = $this->mailStore->addMailing(
-			$this->source,
-			$name,
-			$this->body,
-			$this->subject,
-			$revision
-		);
-		$this->assertInstanceOf(
-			\wmf_communication\CiviMailingRecord::class,
-			$storedMailing,
-			'addMailing should return an ICiviMailingRecord'
-		);
-		$this->assertTrue(
-			is_numeric( $storedMailing->getMailingID() ),
-			'CiviMailingRecord should have a numeric mailing ID'
-		);
-	}
+  public function testAddMailing() {
+    $name = 'test_mailing';
+    $revision = mt_rand();
+    $storedMailing = $this->mailStore->addMailing(
+      $this->source,
+      $name,
+      $this->body,
+      $this->subject,
+      $revision
+    );
+    $this->assertInstanceOf(
+      \wmf_communication\CiviMailingRecord::class,
+      $storedMailing,
+      'addMailing should return an ICiviMailingRecord'
+    );
+    $this->assertTrue(
+      is_numeric($storedMailing->getMailingID()),
+      'CiviMailingRecord should have a numeric mailing ID'
+    );
+  }
 
-	public function testGetMailing() {
-		$name = 'test_mailing';
-		$revision = mt_rand();
-		$storedMailing = $this->mailStore->addMailing(
-			$this->source,
-			$name,
-			$this->body,
-			$this->subject,
-			$revision
-		);
-		$retrievedMailing = $this->mailStore->getMailing(
-			$this->source,
-			$name,
-			$revision
-		);
-		$this->assertEquals(
-			$storedMailing->getMailingID(),
-			$retrievedMailing->getMailingID(),
-			'Retrieved mailing has wrong MailingID'
-		);
-		$this->assertEquals(
-			$storedMailing->getJobID(),
-			$retrievedMailing->getJobID(),
-			'Retrieved mailing has wrong JobID'
-		);
-	}
+  public function testGetMailing() {
+    $name = 'test_mailing';
+    $revision = mt_rand();
+    $storedMailing = $this->mailStore->addMailing(
+      $this->source,
+      $name,
+      $this->body,
+      $this->subject,
+      $revision
+    );
+    $retrievedMailing = $this->mailStore->getMailing(
+      $this->source,
+      $name,
+      $revision
+    );
+    $this->assertEquals(
+      $storedMailing->getMailingID(),
+      $retrievedMailing->getMailingID(),
+      'Retrieved mailing has wrong MailingID'
+    );
+    $this->assertEquals(
+      $storedMailing->getJobID(),
+      $retrievedMailing->getJobID(),
+      'Retrieved mailing has wrong JobID'
+    );
+  }
 
   public function testMissingMailing(): void {
     $this->expectException(\Civi\WMFMailTracking\CiviMailingMissingException::class);
-    $this->mailStore->getMailing( 'fakeSource', 'fakeName', mt_rand() );
+    $this->mailStore->getMailing('fakeSource', 'fakeName', mt_rand());
   }
 
-	public function testAddQueueRecord() {
-		$name = 'test_mailing';
-		$revision = mt_rand();
-		$storedMailing = $this->mailStore->addMailing(
-			$this->source,
-			$name,
-			$this->body,
-			$this->subject,
-			$revision
-		);
-		$queueRecord = $this->mailStore->addQueueRecord(
-			$storedMailing,
-			'generaltrius@hondo.mil',
-			$this->contactID
-		);
-		$this->assertInstanceOf(
-			'wmf_communication\ICiviMailQueueRecord',
-			$queueRecord,
-			'addQueueRecord should return an ICiviMailQueueRecord'
-		);
-		$this->assertTrue(
-			is_numeric( $queueRecord->getQueueID() ),
-			'CiviMailQueueRecord should have a numeric ID'
-		);
-		$this->assertEquals(
-			$this->contactID,
-			$queueRecord->getContactID(),
-			'CiviMailQueueRecord has wrong contact ID'
-		);
-		$this->assertEquals(
-			$this->emailID,
-			$queueRecord->getEmailID(),
-			'CiviMailQueueRecord has wrong email ID'
-		);
-	}
+  public function testAddQueueRecord() {
+    $name = 'test_mailing';
+    $revision = mt_rand();
+    $storedMailing = $this->mailStore->addMailing(
+      $this->source,
+      $name,
+      $this->body,
+      $this->subject,
+      $revision
+    );
+    $queueRecord = $this->mailStore->addQueueRecord(
+      $storedMailing,
+      'generaltrius@hondo.mil',
+      $this->contactID
+    );
+    $this->assertInstanceOf(
+      'wmf_communication\ICiviMailQueueRecord',
+      $queueRecord,
+      'addQueueRecord should return an ICiviMailQueueRecord'
+    );
+    $this->assertTrue(
+      is_numeric($queueRecord->getQueueID()),
+      'CiviMailQueueRecord should have a numeric ID'
+    );
+    $this->assertEquals(
+      $this->contactID,
+      $queueRecord->getContactID(),
+      'CiviMailQueueRecord has wrong contact ID'
+    );
+    $this->assertEquals(
+      $this->emailID,
+      $queueRecord->getEmailID(),
+      'CiviMailQueueRecord has wrong email ID'
+    );
+  }
+
 }
