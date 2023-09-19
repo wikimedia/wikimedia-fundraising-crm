a06ed5f029987789e2abf7d9349eff017e3f1bb8
Damilare Adedoyin <dadedoyin@wikimedia.org>
2023-09-19 16:27:20 +0100

    Add new payment processor for Fundraise-up
    To create the new processor, run docker compose exec civicrm drush @wmff cvapi system.flush
    
    Bug: T346622
    Change-Id: I0eb64061cd3a78917d1d9a72e1506ca2a493a12c

--- drupal/sites/default/civicrm/extensions/org.wikimedia.smashpig/Metadata/processors.mgd.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.smashpig/Metadata/processors.mgd.php
@@ -166,4 +166,23 @@ return [
       'payment_type' => 1,
     ],
   ],
+  8 => [
+    'name' => 'SmashPig - Fundraiseup',
+    'entity' => 'payment_processor_type',
+    'params' => [
+      'version' => 3,
+      'title' => 'Fundraiseup via SmashPig',
+      'name' => 'smashpig_fundraiseup',
+      'description' => 'SmashPig Fundraiseup Processor',
+      'user_name_label' => 'Unused',
+      'password_label' => 'Unused',
+      'signature_label' => 'Unused',
+      'subject_label' => 'Unused',
+      'class_name' => 'Payment_SmashPig',
+      'url_site_default' => 'https://dummyurl.com',
+      'url_api_default' => 'https://dummyurl.com',
+      'billing_mode' => 1,
+      'payment_type' => 1,
+    ],
+  ],
 ];
