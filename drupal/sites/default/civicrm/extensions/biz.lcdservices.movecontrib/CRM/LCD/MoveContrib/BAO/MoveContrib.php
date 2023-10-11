04077813354bb5443ef278f7a7706ff2230d1a66
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-10-11 14:29:24 +1300

    Add extension to permit moving contributions
    Note that this will not show up unless we add the
    relevant permission on prod. This brings us back to
    not having finalised permissions - but the phab was logged
    by donor services - I propose we give it to
    
    - fr-tech
    - donor relations
    - donor relations management
    - donation processing (new role from Nora. No members
    yet but we can add people from that team to it)
    allow Move Contribution
    Bug: T330539
    
    Change-Id: I0aebf305f2757182cb1fcb5f05945a228fc64c9d

---
+++ drupal/sites/default/civicrm/extensions/biz.lcdservices.movecontrib/CRM/LCD/MoveContrib/BAO/MoveContrib.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Class CRM_LCD_MoveContrib_Form_MoveContrib
+ */
+class CRM_LCD_MoveContrib_BAO_MoveContrib {
+
+  static function moveContribution($params) {
+    $params['id'] = $params['contribution_id'];
+    $contribution = CRM_Contribute_BAO_Contribution::create($params);
+
+    // Update the financial items' contact IDs as well.
+    $sql = "
+      UPDATE civicrm_financial_item cfi
+      JOIN civicrm_entity_financial_trxn cefti
+        ON cefti.entity_id = cfi.id
+      JOIN civicrm_financial_trxn cft
+        ON cefti.entity_table = 'civicrm_financial_item'
+        AND cft.id = cefti.financial_trxn_id
+      JOIN civicrm_entity_financial_trxn ceftc
+        ON ceftc.financial_trxn_id = cft.id
+      SET cfi.contact_id = %1
+      WHERE ceftc.entity_table = 'civicrm_contribution'
+        AND ceftc.entity_id = %2
+    ";
+    CRM_Core_DAO::executeQuery($sql, [
+      1 => [$params['change_contact_id'], 'Positive'],
+      2 => [$params['contribution_id'], 'Positive'],
+    ]);
+
+    // record activity for moving contribution
+    if ($contribution) {
+      $subject = "Contribution #{$params['contribution_id']} Moved";
+      $details = "Contribution #{$params['contribution_id']} was moved from contact #{$params['current_contact_id']} to contact #{$params['change_contact_id']}.";
+
+      $activityTypeID = \CRM_Core_PseudoConstant::getKey('CRM_Activity_BAO_Activity', 'activity_type_id', 'contribution_reassignment');
+
+      $activityParams = [
+        'source_contact_id' => $params['current_contact_id'],
+        'activity_type_id' => $activityTypeID,
+        'activity_date_time' => date('YmdHis'),
+        'subject' => $subject,
+        'details' => $details,
+        'status_id' => 2,
+      ];
+
+      $session = CRM_Core_Session::singleton();
+      $id = $session->get('userID');
+
+      if ($id) {
+        $activityParams['source_contact_id'] = $id;
+        $activityParams['target_contact_id'][] = $params['current_contact_id'];
+        $activityParams['target_contact_id'][] = $params['change_contact_id'];
+      }
+
+      try {
+        CRM_Activity_BAO_Activity::create($activityParams);
+      }
+      catch (CiviCRM_API3_Exception $e) {}
+
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+}
---
+++ drupal/sites/default/civicrm/extensions/biz.lcdservices.movecontrib/CRM/LCD/MoveContrib/Form/MoveContrib.php
@@ -0,0 +1,101 @@
+<?php
+
+require_once 'CRM/Core/Form.php';
+
+/**
+ * Form controller class
+ *
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
+ */
+class CRM_LCD_MoveContrib_Form_MoveContrib extends CRM_Core_Form {
+
+  /**
+   * check permissions
+   */
+  public function preProcess(): void {
+    //check for delete
+    if (!CRM_Core_Permission::checkActionPermission('CiviContribute', CRM_Core_Action::UPDATE)) {
+      CRM_Core_Error::statusBounce(ts('You do not have permission to access this page.'));
+    }
+    parent::preProcess();
+  }
+
+  /**
+   * @throws \CRM_Core_Exception
+   */
+  public function buildQuickForm(): void {
+    $contributionID = CRM_Utils_Request::retrieve('id', 'Positive', $this);
+
+    $contactID = civicrm_api3('contribution', 'getvalue', [
+      'id' => $contributionID,
+      'return' => 'contact_id',
+    ]);
+
+    //get current contact name.
+    $this->assign('currentContactName', CRM_Contact_BAO_Contact::displayName($contactID));
+
+    $this->addEntityRef('change_contact_id', ts('Select Contact'));
+    $this->add('hidden', 'contact_id', '', ['id' => 'contact_id']);
+    $this->add('hidden', 'contribution_id', $contributionID, ['id' => 'contribution_id']);
+    $this->add('hidden', 'current_contact_id', $contactID, ['id' => 'current_contact_id']);
+    $this->assign('contactId', $contactID);
+
+    // export form elements
+    $this->assign('elementNames', $this->getRenderableElementNames());
+
+    $this->addButtons([
+      [
+        'type' => 'submit',
+        'name' => ts('Submit'),
+        'isDefault' => TRUE,
+      ],
+    ]);
+
+    parent::buildQuickForm();
+  }
+
+  public function postProcess(): void {
+    $values = $this->exportValues();
+    //Civi::log()->debug('postProcess', array('values' => $values));
+
+    $params = [
+      'change_contact_id' => $values['change_contact_id'],
+      'contact_id' => $values['change_contact_id'],
+      'contribution_id' => $values['contribution_id'],
+      'current_contact_id' => $values['current_contact_id'],
+    ];
+
+    $result = CRM_LCD_MoveContrib_BAO_MoveContrib::moveContribution($params);
+
+    if ($result) {
+      CRM_Core_Session::setStatus(ts('Contribution moved successfully.'), ts('Moved'), 'success');
+    }
+    else {
+      CRM_Core_Session::setStatus(ts('Unable to move contribution.'), ts('Error'), 'error');
+    }
+
+    parent::postProcess();
+  }
+
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array (string)
+   */
+  public function getRenderableElementNames(): array {
+    // The _elements list includes some items which should not be
+    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
+    // items don't have labels.  We'll identify renderable by filtering on
+    // the 'label'.
+    $elementNames = [];
+    foreach ($this->_elements as $element) {
+      /** @var HTML_QuickForm_Element $element */
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
+  }
+
+}
