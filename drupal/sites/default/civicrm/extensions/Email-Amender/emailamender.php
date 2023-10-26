ca5d2fc20d7365958751be62e62f3650b6ba956e
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-10-26 13:54:21 +1300

    Update email amender extension
    This adds a new permission (which we need to manually allocate) to
    CiviCRM 'administer email corrections' which allows the email amender screen
    https://wmff.localhost:32353/civicrm/emailamendersettings to be configured
    
    Note that by default our civi user role 'administrator' has the permission
    'CiviCRM: all CiviCRM permissions and ACLs' which implicitly includes
    ALL other permissions. Even if you remove it from the role the admin user
    still has this (I think) by right of being user number 1 - so it is
    best to test with the demo user (demo/demo).
    
    Once 'CiviCRM: all CiviCRM permissions and ACLs' is removed there are
    2 permissions to tinker with to affect what demo user can see / do
    
    - Administer CiviCRM - this only impacts the top checkbox 'Automatic email corrections enabled'
    - administer_email_amender - this impacts all the other configuration options on the form.
    
    The various input fields should be hidden or disabled when the user does not have permission
    and should work when they do.
    
    Note that I also pulled in upstream cleanup & did a bit more cleanup - primarily moving
    the option value & navigation item the the 'managed' framework & various civix updates
    
    Bug: T343899
    
    Change-Id: I13928ce35b51c97f1a20a324f0b2534384384adc

--- drupal/sites/default/civicrm/extensions/Email-Amender/emailamender.php
+++ drupal/sites/default/civicrm/extensions/Email-Amender/emailamender.php
@@ -2,6 +2,8 @@
 
 require_once 'emailamender.civix.php';
 
+use CRM_Emailamender_ExtensionUtil as E;
+
 /**
  * Implements hook_civicrm_config().
  */
@@ -10,49 +12,10 @@ function emailamender_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_install().
- */
-function emailamender_civicrm_install() {
-  CRM_Core_BAO_OptionValue::ensureOptionValueExists([
-    'label'        => 'Corrected Email Address',
-    'name'         => 'corrected_email_address',
-    'weight'       => '1',
-    'description'  => 'Automatically corrected emails (by the Email Address Corrector extension).',
-    'option_group_id' => 'activity_type',
-    'filter' => 1,
-  ]);
-  return _emailamender_civix_civicrm_install();
-}
-
-/**
  * Implements hook_civicrm_uninstall().
  */
 function emailamender_civicrm_uninstall() {
-
   CRM_Core_DAO::executeQuery("DELETE FROM civicrm_setting WHERE name LIKE 'emailamender%'");
-
-  return _emailamender_civix_civicrm_uninstall();
-}
-
-/**
- * Implements hook_civicrm_enable().
- */
-function emailamender_civicrm_enable() {
-  return _emailamender_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- */
-function emailamender_civicrm_disable() {
-  return _emailamender_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- */
-function emailamender_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _emailamender_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -78,60 +41,31 @@ function emailamender_civicrm_emailProcessorContact($email, $contactID, &$result
 }
 
 /**
- * civicrm_civicrm_navigationMenu
- *
- * implementation of civicrm_civicrm_navigationMenu
- *
- */
-function emailamender_civicrm_navigationMenu(&$params) {
-  $sAdministerMenuId = CRM_Core_DAO::getFieldValue('CRM_Core_BAO_Navigation', 'Administer', 'id', 'name');
-  $sSystemSettingsMenuId = CRM_Core_DAO::getFieldValue('CRM_Core_BAO_Navigation', 'System Settings', 'id', 'name');
-
-  //  Get the maximum key of $params
-  $maxKey = max(array_keys($params));
-
-  $params[$sAdministerMenuId]['child'][$sSystemSettingsMenuId]['child'][$maxKey + 1] = array(
-    'attributes' => array(
-      'label'      => 'Email Address Corrector Settings',
-      'name'       => 'EmailAmenderSettings',
-      'url'        => 'civicrm/emailamendersettings',
-      'permission' => NULL,
-      'operator'   => NULL,
-      'separator'  => NULL,
-      'parentID'   => $sSystemSettingsMenuId,
-      'navID'      => $maxKey + 1,
-      'active'     => 1,
-    ),
-  );
-}
-
-/**
  * Implements hook_civicrm_searchTasks().
  */
 function emailamender_civicrm_searchTasks($objectType, &$tasks) {
   if ($objectType === 'contact') {
-    $tasks[] = array(
+    $tasks[] = [
       'title'  => ts('Email - correct email addresses'),
       'class'  => 'CRM_Emailamender_Form_Task_Correctemailaddresses',
       'result' => TRUE,
-    );
+    ];
   }
 }
 
 /**
- * Implements hook_civicrm_postInstall().
+ * Implements hook_civicrm_permission().
  *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ * @see CRM_Utils_Hook::permission()
+ * @param array $permissions
  */
-function emailamender_civicrm_postInstall() {
-  _emailamender_civix_civicrm_postInstall();
+function emailamender_civicrm_permission(&$permissions) {
+  $permissions['administer_email_amender'] = [
+    E::ts('Email Amender'),
+    E::ts('administer email corrections'),
+  ];
 }
 
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function emailamender_civicrm_entityTypes(&$entityTypes) {
-  _emailamender_civix_civicrm_entityTypes($entityTypes);
+function emailamender_civicrm_alterAPIPermissions($entity, $action, &$params, &$permissions) {
+  $permissions['email_amender']['default'] = 'administer_email_amender';
 }
