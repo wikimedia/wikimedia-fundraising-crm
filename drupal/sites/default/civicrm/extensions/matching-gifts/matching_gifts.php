fabeb7a5eeb167f8e094b2ed2941fe2428215b04
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-11-03 08:52:44 +1300

    Civix upgrade on matching gifts
    Change-Id: Ieeaeda6775df31418b112a64cbe4650b96e86988

--- drupal/sites/default/civicrm/extensions/matching-gifts/matching_gifts.php
+++ drupal/sites/default/civicrm/extensions/matching-gifts/matching_gifts.php
@@ -22,59 +22,12 @@ function matching_gifts_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function matching_gifts_civicrm_postInstall() {
-  _matching_gifts_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function matching_gifts_civicrm_uninstall() {
-  _matching_gifts_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function matching_gifts_civicrm_enable() {
   _matching_gifts_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function matching_gifts_civicrm_disable() {
-  _matching_gifts_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function matching_gifts_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _matching_gifts_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function matching_gifts_civicrm_entityTypes(&$entityTypes) {
-  _matching_gifts_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
