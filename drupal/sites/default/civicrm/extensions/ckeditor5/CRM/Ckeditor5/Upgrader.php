51277deb58ef146228ef36629fe8b7a76a205cb4
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-11-03 09:49:04 +1300

    Civix upgrade on ckeditor
    Change-Id: I43ef3fe85df031bd3ba2db91d232895dd73fabf0

--- drupal/sites/default/civicrm/extensions/ckeditor5/CRM/Ckeditor5/Upgrader.php
+++ drupal/sites/default/civicrm/extensions/ckeditor5/CRM/Ckeditor5/Upgrader.php
@@ -4,7 +4,7 @@ use CRM_Ckeditor5_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Ckeditor5_Upgrader extends CRM_Ckeditor5_Upgrader_Base {
+class CRM_Ckeditor5_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
