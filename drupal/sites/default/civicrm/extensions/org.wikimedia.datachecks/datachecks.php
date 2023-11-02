94e6d9eeb4c7f0220b7dafd5489ab65a603d33ed
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-11-03 09:46:48 +1300

    Run civix upgrade on datachecks
    Change-Id: Ie67e175d422b1c7eeba8851a934bf2861448d682

--- drupal/sites/default/civicrm/extensions/org.wikimedia.datachecks/datachecks.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.datachecks/datachecks.php
@@ -21,46 +21,12 @@ function datachecks_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function datachecks_civicrm_uninstall() {
-  _datachecks_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function datachecks_civicrm_enable() {
   _datachecks_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function datachecks_civicrm_disable() {
-  _datachecks_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function datachecks_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _datachecks_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -157,21 +123,3 @@ function datachecks_civicrm_navigationMenu(&$menu) {
   ));
   _datachecks_civix_navigationMenu($menu);
 } // */
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function datachecks_civicrm_postInstall() {
-  _datachecks_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function datachecks_civicrm_entityTypes(&$entityTypes) {
-  _datachecks_civix_civicrm_entityTypes($entityTypes);
-}
