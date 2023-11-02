0a86512db8766cbb893c95bbab57c59ddeec24e6
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-11-03 09:07:00 +1300

    Run civix on forgetme
    Change-Id: Icb4f3d9ed3aeb2b1db4ebe828b3c838328cb9058

--- drupal/sites/default/civicrm/extensions/org.wikimedia.forgetme/forgetme.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.forgetme/forgetme.php
@@ -22,48 +22,12 @@ function forgetme_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function forgetme_civicrm_postInstall() {
-  _forgetme_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function forgetme_civicrm_uninstall() {
-  _forgetme_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function forgetme_civicrm_enable() {
   _forgetme_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function forgetme_civicrm_disable() {
-  _forgetme_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function forgetme_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _forgetme_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -97,17 +61,6 @@ function forgetme_civicrm_alterLogTables(&$logTableSpec) {
       unset($logTableSpec[$staticDataTable]);
     }
   }
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function forgetme_civicrm_entityTypes(&$entityTypes) {
-  _forgetme_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
