426e41023556cccdf804c8ec3d79b3f8d5820b73
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-11-03 08:57:34 +1300

    Upgrade civix on unsubscribe
    Change-Id: I9d9badd14f5b9b8d622deeaf43e9ffcc66501cb0

--- drupal/sites/default/civicrm/extensions/org.wikimedia.unsubscribeemail/unsubscribeemail.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.unsubscribeemail/unsubscribeemail.php
@@ -18,45 +18,10 @@ function unsubscribeemail_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- */
-function unsubscribeemail_civicrm_postInstall() {
-  _unsubscribeemail_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- */
-function unsubscribeemail_civicrm_uninstall() {
-  _unsubscribeemail_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  */
 function unsubscribeemail_civicrm_enable() {
   _unsubscribeemail_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- */
-function unsubscribeemail_civicrm_disable() {
-  _unsubscribeemail_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function unsubscribeemail_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _unsubscribeemail_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -82,13 +47,4 @@ function unsubscribeemail_civicrm_navigationMenu(&$menu) {
     'operator' => 'OR',
   ]);
   _unsubscribeemail_civix_navigationMenu($menu);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function unsubscribeemail_civicrm_entityTypes(&$entityTypes) {
-  _unsubscribeemail_civix_civicrm_entityTypes($entityTypes);
 }
