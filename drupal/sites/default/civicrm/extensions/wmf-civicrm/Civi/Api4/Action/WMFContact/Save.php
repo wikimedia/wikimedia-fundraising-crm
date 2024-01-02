83cd0c1226b2d1fcc3b48af78d1416a941c79242
wfan <wfan@wikimedia.org>
2024-01-02 15:58:36 -0800

    remove the outdated exception use CRM_Core_Exception and getErrorData now
    Change-Id: I642efc389c057681eb6c3308726e9810bc7b51c3

--- drupal/sites/default/civicrm/extensions/wmf-civicrm/Civi/Api4/Action/WMFContact/Save.php
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/Civi/Api4/Action/WMFContact/Save.php
@@ -63,7 +63,6 @@ class Save extends AbstractAction {
    * @param \Civi\Api4\Generic\Result $result
    *
    * @throws \CRM_Core_Exception
-   * @throws \CiviCRM_API3_Exception
    * @throws \Civi\WMFException\WMFException
    */
   public function _run(Result $result): void {
@@ -225,19 +224,19 @@ class Save extends AbstractAction {
     }
     // Soon we will only catch CRM_Core_Exception as the other exceptions are now aliased to it
     // preparatory to being phased out
-    catch (\CiviCRM_API3_Exception|\CRM_Core_Exception $ex) {
+    catch (\CRM_Core_Exception $ex) {
       if (in_array($ex->getErrorCode(), ['constraint violation', 'deadlock', 'database lock timeout'])) {
         throw new WMFException(
           WMFException::DATABASE_CONTENTION,
           'Contact could not be added due to database contention',
-          $ex->getExtraParams()
+          $ex->getErrorData()
         );
       }
       throw new WMFException(
         WMFException::IMPORT_CONTACT,
         'Contact could not be added. Aborting import. Contact data was ' . print_r($contact, TRUE) . ' Original error: ' . $ex->getMessage()
-        . ' Details: ' . print_r($ex->getExtraParams(), TRUE),
-        $ex->getExtraParams()
+        . ' Details: ' . print_r($ex->getErrorData(), TRUE),
+        $ex->getErrorData()
       );
     }
     $contact_id = (int) $contact_result['id'];
@@ -255,10 +254,10 @@ class Save extends AbstractAction {
           'debug' => TRUE,
         ]);
       }
-      catch (\CiviCRM_API3_Exception $ex) {
+      catch (\CRM_Core_Exception $ex) {
         throw new WMFException(
           WMFException::IMPORT_CONTACT,
-          "Failed to add phone for contact ID {$contact_id}: {$ex->getMessage()} " . print_r($ex->getExtraParams(), TRUE)
+          "Failed to add phone for contact ID {$contact_id}: {$ex->getMessage()} " . print_r($ex->getErrorData(), TRUE)
         );
       }
     }
@@ -276,7 +275,7 @@ class Save extends AbstractAction {
             'group_id' => $supported_groups[$group],
           ]);
         }
-        catch (\CiviCRM_API3_Exception $ex) {
+        catch (\CRM_Core_Exception $ex) {
           $stacked_ex[] = "Failed to add group {$group} to contact ID {$contact_id}. Error: " . $ex->getMessage();
         }
       }
@@ -350,7 +349,7 @@ class Save extends AbstractAction {
             'tag_id' => $supported_tags[$tag],
           ]);
         }
-        catch (\CiviCRM_API3_Exception $ex) {
+        catch (\CRM_Core_Exception $ex) {
           $stacked_ex[] = "Failed to add tag {$tag} to contact ID {$contact_id}. Error: " . $ex->getMessage();
         }
       }
