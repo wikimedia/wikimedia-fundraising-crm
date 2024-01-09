a3548adfd0a3cb81ca4e3d5dd3535610c14f0850
eileen <emcnaughton@wikimedia.org>
2024-01-09 17:15:55 +1300

    Add api to get unsubscribe links
    This is the first in a chain of patches that consolidates this code
    
    This patch only adds the api + test.
    
    Bug: T354585
    Change-Id: Ibba26c52a060b36aceac4090fc6410b3e1b64321

---
+++ drupal/sites/default/civicrm/extensions/wmf-thankyou/tests/phpunit/Civi/Api4/WMFLinkTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Civi\Api4;
+
+use Civi\Omnimail\MailFactory;
+use Civi\Test\Api3TestTrait;
+use Civi\WorkflowMessage\EOYThankYou;
+use CRM_Core_DAO;
+use CRM_Core_PseudoConstant;
+use PHPUnit\Framework\TestCase;
+use Civi\Test\Mailer;
+
+class WMFLinkTest extends TestCase {
+
+  /**
+   * Test getting the unsubscribe link.
+   *
+   * Ensure that contribution_id is set to -1 if not provided
+   * - or more specifically that the calling function does not have to
+   * provide a dummy value and any dummy values will be provided by the api.
+   *
+   * @return void
+   * @throws \CRM_Core_Exception
+   */
+  public function testGetUnsubscribeLink(): void {
+    $link = WMFLink::getUnsubscribeURL(FALSE)
+      ->setEmail('me@example.com')
+      ->setLanguage('es_ES')
+      ->execute()->first()['unsubscribe_url'];
+    $this->assertStringContainsString('p=thankyou', $link);
+    $this->assertStringContainsString('c=-1', $link);
+    $this->assertStringContainsString('e=me%40example.com', $link);
+    $this->assertStringContainsString('uselang=es', $link);
+    $this->assertStringContainsString(\Civi::settings()->get('wmf_unsubscribe_url') , $link);
+  }
+
+}
