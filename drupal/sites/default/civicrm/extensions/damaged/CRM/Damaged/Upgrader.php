37e8738fc2dc25d3c7cc03c8b428f900084d3a73
Damilare Adedoyin <dadedoyin@wikimedia.org>
2024-02-07 21:36:52 +0000

    Update Damaged extension and filter out rows with retry_date from list of items.
    Change-Id: I20ccba00611b1dee65400c348108ee021fefc363

--- drupal/sites/default/civicrm/extensions/damaged/CRM/Damaged/Upgrader.php
+++ drupal/sites/default/civicrm/extensions/damaged/CRM/Damaged/Upgrader.php
@@ -4,7 +4,7 @@
 /**
  * Collection of upgrade steps.
  */
-class CRM_Damaged_Upgrader extends CRM_Damaged_Upgrader_Base {
+class CRM_Damaged_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
--- drupal/sites/default/civicrm/extensions/damaged/damaged.php
+++ drupal/sites/default/civicrm/extensions/damaged/damaged.php
@@ -24,59 +24,12 @@ function damaged_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function damaged_civicrm_postInstall() {
-  _damaged_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function damaged_civicrm_uninstall() {
-  _damaged_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function damaged_civicrm_enable() {
   _damaged_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function damaged_civicrm_disable() {
-  _damaged_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function damaged_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _damaged_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function damaged_civicrm_entityTypes(&$entityTypes) {
-  _damaged_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
