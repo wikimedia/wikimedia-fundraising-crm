ba960c68206607c9c102dae665c92ce34db84046
Elliott Eggleston <ejegg@ejegg.com>
2024-02-07 19:55:38 -0500

    Civix updates for tutorial
    Submitted upstream as
    
    https://lab.civicrm.org/extensions/tutorial/-/merge_requests/14
    
    Gets rid of PHP Notice
    CRM_Core_Smarty::$template_dir has no effect tutorial.civix.php:107
    
    Change-Id: I7896d58174e8c67c581ea774d8609c729edf37f4

--- drupal/sites/default/civicrm/extensions/org.civicrm.tutorial/tutorial.php
+++ drupal/sites/default/civicrm/extensions/org.civicrm.tutorial/tutorial.php
@@ -21,59 +21,12 @@ function tutorial_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function tutorial_civicrm_postInstall() {
-  _tutorial_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function tutorial_civicrm_uninstall() {
-  _tutorial_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function tutorial_civicrm_enable() {
   _tutorial_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function tutorial_civicrm_disable() {
-  _tutorial_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function tutorial_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _tutorial_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function tutorial_civicrm_entityTypes(&$entityTypes) {
-  _tutorial_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
