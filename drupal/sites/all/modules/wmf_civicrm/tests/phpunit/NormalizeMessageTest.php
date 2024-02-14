450bd8447a81139745cc8f3fea5fe976a6d4a097
eileen <emcnaughton@wikimedia.org>
2024-02-15 11:18:55 +1300

    Update normalizeMessage test to instantiate message directly
    Bug: T357471
    
    Change-Id: I91406cf5048e61f8aab41b4f5eedd100d43ef7e7

--- drupal/sites/all/modules/wmf_civicrm/tests/phpunit/NormalizeMessageTest.php
+++ drupal/sites/all/modules/wmf_civicrm/tests/phpunit/NormalizeMessageTest.php
@@ -1,160 +1,161 @@
 <?php
 
+use Civi\WMFException\WMFException;
+use Civi\WMFQueueMessage\DonationMessage;
+
 /**
  * @group Pipeline
  * @group WmfCivicrm
  */
 class NormalizeMessageTest extends BaseWmfDrupalPhpUnitTestCase {
-    public static function getInfo() {
-        return array(
-            'name' => 'WmfCivicrm message normalization',
-            'group' => 'Pipeline',
-            'description' => 'Checks for queue message normalization behavior',
-        );
-    }
 
-    public function testDoubleNormalization() {
-        // Start with a message already in normal form, to make comparison easy
-        $original_msg = array(
-            'anonymous' => 0,
-            'check_number' => '',
-            'city' => '',
-            'comment' => '',
-            'contact_id' => mt_rand(),
-            'contact_groups' => array(),
-            'contact_tags' => array(),
-            'contribution_recur_id' => mt_rand(),
-            'contribution_tags' => array(),
-            'contribution_tracking_id' => mt_rand(),
-            'contribution_tracking_update' => '1',
-            'financial_type_id' => '9',
-            'country' => 'IL',
-            'create_date' => time() + 11,
-            'currency' => 'USD',
-            'date' => time() + 1,
-            'effort_id' => '2',
-            'email' => 'test.es@localhost.net',
-            'fee' => 0.5,
-            'first_name' => 'test',
-            'gateway' => 'paypal',
-            'gateway_txn_id' => '1234AB1234-2',
-            'gross' => 5.8,
-            'last_name' => 'es',
-            'letter_code' => '',
-            'middle_name' => '',
-            'net' => 5.29,
-            'order_id' => mt_rand(),
-            'organization_name' => '',
-            'original_currency' => 'ILS',
-            'original_gross' => '20.00',
-            'payment_date' => time(),
-            'payment_instrument_id' => '25',
-            'payment_instrument' => 'Paypal',
-            'postal_code' => '',
-            'postmark_date' => null,
-            'recurring' => '1',
-            'soft_credit_to' => null,
-            'soft_credit_to_id' => null,
-            'source_enqueued_time' => time() + 2,
-            'source_host' => 'thulium',
-            'source_name' => 'PayPal IPN (legacy)',
-            'source_run_id' => mt_rand(),
-            'source_type' => 'listener',
-            'source_version' => 'legacy',
-            'start_date' => time() + 10,
-            'state_province' => '',
-            'street_address' => '',
-            'subscr_id' => 'TEST-S-1234567' . mt_rand(),
-            'supplemental_address_1' => '',
-            'supplemental_address_2' => '',
-            'thankyou_date' => '',
-            'txn_type' => 'subscr_payment',
-            'utm_campaign' => '',
-        );
+  public function testDoubleNormalization(): void {
+    // Start with a message already in normal form, to make comparison easy
+    $original_msg = [
+      'anonymous' => 0,
+      'check_number' => '',
+      'city' => '',
+      'comment' => '',
+      'contact_id' => mt_rand(),
+      'contact_groups' => [],
+      'contact_tags' => [],
+      'contribution_recur_id' => mt_rand(),
+      'contribution_tags' => [],
+      'contribution_tracking_id' => mt_rand(),
+      'contribution_tracking_update' => '1',
+      'financial_type_id' => '9',
+      'country' => 'IL',
+      'create_date' => time() + 11,
+      'currency' => 'USD',
+      'date' => time() + 1,
+      'effort_id' => '2',
+      'email' => 'test.es@localhost.net',
+      'fee' => 0.5,
+      'first_name' => 'test',
+      'gateway' => 'paypal',
+      'gateway_txn_id' => '1234AB1234-2',
+      'gross' => 5.8,
+      'last_name' => 'es',
+      'letter_code' => '',
+      'middle_name' => '',
+      'net' => 5.29,
+      'order_id' => mt_rand(),
+      'organization_name' => '',
+      'original_currency' => 'ILS',
+      'original_gross' => '20.00',
+      'payment_date' => time(),
+      'payment_instrument_id' => '25',
+      'payment_instrument' => 'Paypal',
+      'postal_code' => '',
+      'postmark_date' => NULL,
+      'recurring' => '1',
+      'soft_credit_to' => NULL,
+      'soft_credit_to_id' => NULL,
+      'source_enqueued_time' => time() + 2,
+      'source_host' => 'thulium',
+      'source_name' => 'PayPal IPN (legacy)',
+      'source_run_id' => mt_rand(),
+      'source_type' => 'listener',
+      'source_version' => 'legacy',
+      'start_date' => time() + 10,
+      'state_province' => '',
+      'street_address' => '',
+      'subscr_id' => 'TEST-S-1234567' . mt_rand(),
+      'supplemental_address_1' => '',
+      'supplemental_address_2' => '',
+      'thankyou_date' => '',
+      'txn_type' => 'subscr_payment',
+      'utm_campaign' => '',
+    ];
 
-        $msg = $original_msg;
-        $normal_msg_1 = wmf_civicrm_normalize_msg( $msg );
-        $this->assertEquals( $original_msg, $normal_msg_1 );
-        $normal_msg_2 = wmf_civicrm_normalize_msg( $normal_msg_1 );
-        $this->assertEquals( $original_msg, $normal_msg_2 );
-    }
+    $msg = $original_msg;
+    $message = new DonationMessage($msg);
+    $normal_msg_1 = $message->normalize();
+    $this->assertEquals($original_msg, $normal_msg_1);
+    $message = new DonationMessage($normal_msg_1);
+    $normal_msg_2 = $message->normalize();
+    $this->assertEquals($original_msg, $normal_msg_2);
+  }
 
-    public function testGetPaymentInstrumenReturnNullInNormalizeMsg() {
-        // Initialize message with no payment_instrument_id, payment_instrument, gateway, and payment_method set.
-        $original_msg = array(
-            'anonymous' => 0,
-            'check_number' => '',
-            'city' => '',
-            'comment' => '',
-            'contact_id' => mt_rand(),
-            'contact_groups' => array(),
-            'contact_tags' => array(),
-            'contribution_recur_id' => mt_rand(),
-            'contribution_tags' => array(),
-            'contribution_tracking_id' => mt_rand(),
-            'contribution_tracking_update' => '1',
-            'financial_type_id' => '9',
-            'country' => 'IL',
-            'create_date' => time() + 11,
-            'currency' => 'USD',
-            'date' => time() + 1,
-            'effort_id' => '2',
-            'email' => 'test.es@localhost.net',
-            'fee' => 0.5,
-            'first_name' => 'test',
-            'gateway' => 'UNKNOWN',
-            'gateway_txn_id' => '1234AB1234-2',
-            'gross' => 5.8,
-            'last_name' => 'es',
-            'letter_code' => '',
-            'middle_name' => '',
-            'net' => 5.29,
-            'order_id' => mt_rand(),
-            'organization_name' => '',
-            'original_currency' => 'ILS',
-            'original_gross' => '20.00',
-            'payment_date' => time(),
-            'postal_code' => '',
-            'postmark_date' => null,
-            'recurring' => '1',
-            'soft_credit_to' => null,
-            'soft_credit_to_id' => null,
-            'source_enqueued_time' => time() + 2,
-            'source_host' => 'thulium',
-            'source_name' => 'PayPal IPN (legacy)',
-            'source_run_id' => mt_rand(),
-            'source_type' => 'listener',
-            'source_version' => 'legacy',
-            'start_date' => time() + 10,
-            'state_province' => '',
-            'street_address' => '',
-            'subscr_id' => 'TEST-S-1234567' . mt_rand(),
-            'supplemental_address_1' => '',
-            'supplemental_address_2' => '',
-            'thankyou_date' => '',
-            'txn_type' => 'subscr_payment',
-            'utm_campaign' => '',
-        );
+  public function testGetPaymentInstrumenReturnNullInNormalizeMsg(): void {
+    // Initialize message with no payment_instrument_id, payment_instrument, gateway, and payment_method set.
+    $original_msg = [
+      'anonymous' => 0,
+      'check_number' => '',
+      'city' => '',
+      'comment' => '',
+      'contact_id' => mt_rand(),
+      'contact_groups' => [],
+      'contact_tags' => [],
+      'contribution_recur_id' => mt_rand(),
+      'contribution_tags' => [],
+      'contribution_tracking_id' => mt_rand(),
+      'contribution_tracking_update' => '1',
+      'financial_type_id' => '9',
+      'country' => 'IL',
+      'create_date' => time() + 11,
+      'currency' => 'USD',
+      'date' => time() + 1,
+      'effort_id' => '2',
+      'email' => 'test.es@localhost.net',
+      'fee' => 0.5,
+      'first_name' => 'test',
+      'gateway' => 'UNKNOWN',
+      'gateway_txn_id' => '1234AB1234-2',
+      'gross' => 5.8,
+      'last_name' => 'es',
+      'letter_code' => '',
+      'middle_name' => '',
+      'net' => 5.29,
+      'order_id' => mt_rand(),
+      'organization_name' => '',
+      'original_currency' => 'ILS',
+      'original_gross' => '20.00',
+      'payment_date' => time(),
+      'postal_code' => '',
+      'postmark_date' => NULL,
+      'recurring' => '1',
+      'soft_credit_to' => NULL,
+      'soft_credit_to_id' => NULL,
+      'source_enqueued_time' => time() + 2,
+      'source_host' => 'thulium',
+      'source_name' => 'PayPal IPN (legacy)',
+      'source_run_id' => mt_rand(),
+      'source_type' => 'listener',
+      'source_version' => 'legacy',
+      'start_date' => time() + 10,
+      'state_province' => '',
+      'street_address' => '',
+      'subscr_id' => 'TEST-S-1234567' . mt_rand(),
+      'supplemental_address_1' => '',
+      'supplemental_address_2' => '',
+      'thankyou_date' => '',
+      'txn_type' => 'subscr_payment',
+      'utm_campaign' => '',
+    ];
 
-        $msg = $original_msg;
-        $this->expectException(\Civi\WMFException\WMFException::class);
-        wmf_civicrm_normalize_msg( $msg );
-    }
+    $msg = $original_msg;
+    $this->expectException(WMFException::class);
+    $message = new DonationMessage($msg);
+    $message->normalize();
+  }
 
-    public function testEmptyNet() {
-        $msg = array(
-            'gateway' => 'adyen',
-            'payment_method' => 'cc',
+  public function testEmptyNet(): void {
+    $msg = [
+      'gateway' => 'adyen',
+      'payment_method' => 'cc',
       'payment_submethod' => 'visa',
-            'first_name' => 'blah',
-            'last_name' => 'wah',
-            'country' => 'US',
-            'currency' => 'USD',
-            'gross' => '1.00',
-            'net' => '',
-            'fee' => '0.21',
-        );
-        $normalized = wmf_civicrm_normalize_msg( $msg );
-        $this->assertEquals( 0.79, $normalized['net'] );
-    }
+      'first_name' => 'blah',
+      'last_name' => 'wah',
+      'country' => 'US',
+      'currency' => 'USD',
+      'gross' => '1.00',
+      'net' => '',
+      'fee' => '0.21',
+    ];
+    $message = new DonationMessage($msg);
+    $normalized = $message->normalize();
+    $this->assertEquals(0.79, $normalized['net']);
+  }
+
 }
