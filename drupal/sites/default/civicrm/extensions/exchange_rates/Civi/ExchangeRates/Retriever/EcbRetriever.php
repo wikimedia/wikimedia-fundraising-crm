b662dd853ad02b311627f5ad1ba6addab8cbd092
Elliott Eggleston <ejegg@ejegg.com>
2024-05-02 10:20:39 -0400

    Small code reformat for exchange rates
    Bug: T270666
    Change-Id: I1a707bfd8d91eb3924089c29cd395a00fd170a8a

--- drupal/sites/default/civicrm/extensions/exchange_rates/Civi/ExchangeRates/Retriever/EcbRetriever.php
+++ drupal/sites/default/civicrm/extensions/exchange_rates/Civi/ExchangeRates/Retriever/EcbRetriever.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Civi\ExchangeRates\Retriever;
 
 use Civi\ExchangeException\ExchangeRateUpdateException;
@@ -6,55 +7,55 @@ use Civi\ExchangeRates\UpdateResult;
 
 class EcbRetriever extends ExchangeRateRetriever {
 
-	public function updateRates( $currencies, $date = null ) {
-		if ( $date !== null ) {
-			throw new ExchangeRateUpdateException( 'ECB does not provide historical exchange rates' );
-		}
+  public function updateRates($currencies, $date = null) {
+    if ($date !== null) {
+      throw new ExchangeRateUpdateException('ECB does not provide historical exchange rates');
+    }
 
-		$url = 'http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml';
+    $url = 'http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml';
 
-		// Retrieve and parse the XML results
-		$request = call_user_func( $this->httpRequester, $url );
-		$xml = $request->data;
-		$p = xml_parser_create();
-		$results = array();
-		$index = array();
-		xml_parse_into_struct( $p, $xml, $results, $index );
-		xml_parser_free( $p );
+    // Retrieve and parse the XML results
+    $request = call_user_func($this->httpRequester, $url);
+    $xml = $request->data;
+    $p = xml_parser_create();
+    $results = array();
+    $index = array();
+    xml_parse_into_struct($p, $xml, $results, $index);
+    xml_parser_free($p);
 
-		// Get date and base USD rate
-		$usdBase = 0;
-		$date = '';
-		foreach ( $index['CUBE'] as $valIndex ) {
-			$current = $results[$valIndex];
-			if ( $current['attributes']['CURRENCY'] == 'USD' && isset( $current['attributes']['RATE'] ) ) {
-				$usdBase = $current['attributes']['RATE'];
-			}
-			if ( isset( $current['attributes']['TIME'] ) ) {
-				$date = $current['attributes']['TIME'];
-			}
-		}
-		$bankUpdateTimestamp = strtotime( $date . ' 00:00:00 GMT' );
-		$result = new UpdateResult();
+    // Get date and base USD rate
+    $usdBase = 0;
+    $date = '';
+    foreach ($index['CUBE'] as $valIndex) {
+      $current = $results[$valIndex];
+      if ($current['attributes']['CURRENCY'] == 'USD' && isset($current['attributes']['RATE'])) {
+        $usdBase = $current['attributes']['RATE'];
+      }
+      if (isset($current['attributes']['TIME'])) {
+        $date = $current['attributes']['TIME'];
+      }
+    }
+    $bankUpdateTimestamp = strtotime($date . ' 00:00:00 GMT');
+    $result = new UpdateResult();
 
-		// Table is based on EUR, so must insert manually if we actually got anything
-        if ( $usdBase !== 0 ) {
-			$result->rates['EUR'] = array(
-				'value' => $usdBase,
-				'date' => $bankUpdateTimestamp
-			);
+    // Table is based on EUR, so must insert manually if we actually got anything
+    if ($usdBase !== 0) {
+      $result->rates['EUR'] = array(
+        'value' => $usdBase,
+        'date' => $bankUpdateTimestamp
+      );
 
-            // Calculate and insert remaining rates
-            foreach ( $index['CUBE'] as $valIndex ) {
-                $current = $results[$valIndex];
-                if ( isset( $current['attributes']['CURRENCY'] ) && isset( $current['attributes']['RATE'] ) ) {
-                    $result->rates[$current['attributes']['CURRENCY']] = array(
-						'value' => $usdBase / $current['attributes']['RATE'],
-						'date' => $bankUpdateTimestamp
-					);
-                }
-            }
+      // Calculate and insert remaining rates
+      foreach ($index['CUBE'] as $valIndex) {
+        $current = $results[$valIndex];
+        if (isset($current['attributes']['CURRENCY']) && isset($current['attributes']['RATE'])) {
+          $result->rates[$current['attributes']['CURRENCY']] = array(
+            'value' => $usdBase / $current['attributes']['RATE'],
+            'date' => $bankUpdateTimestamp
+          );
         }
-		return $result;
-	}
+      }
+    }
+    return $result;
+  }
 }
--- drupal/sites/default/civicrm/extensions/exchange_rates/Civi/ExchangeRates/Retriever/ExchangeRateRetriever.php
+++ drupal/sites/default/civicrm/extensions/exchange_rates/Civi/ExchangeRates/Retriever/ExchangeRateRetriever.php
@@ -1,27 +1,28 @@
 <?php
+
 namespace Civi\ExchangeRates\Retriever;
 
 use InvalidArgumentException;
 
 abstract class ExchangeRateRetriever {
-	protected $httpRequester;
+  protected $httpRequester;
 
-	/**
-	 * @param callable $httpRequester - either drupal_http_request or a fake
-	 * @throws InvalidArgumentException
-	 */
-	public function __construct( $httpRequester ) {
-		if ( !is_callable( $httpRequester ) ) {
-			throw new InvalidArgumentException( 'httpRequester should be callable' );
-		}
-		$this->httpRequester = $httpRequester;
-	}
+  /**
+   * @param callable $httpRequester - either drupal_http_request or a fake
+   * @throws InvalidArgumentException
+   */
+  public function __construct($httpRequester) {
+    if (!is_callable($httpRequester)) {
+      throw new InvalidArgumentException('httpRequester should be callable');
+    }
+    $this->httpRequester = $httpRequester;
+  }
 
-	/**
-	 * Retrieve updated rates using $this->httpRequester
-	 * @param array $currencies - list of currency codes to update
-	 * @param \DateTime $date - retrieve rates for this date.  If omitted, get latest rates.
-	 * @return \Civi\ExchangeRates\UpdateResult
-	 */
-	abstract function updateRates( $currencies, $date = null );
+  /**
+   * Retrieve updated rates using $this->httpRequester
+   * @param array $currencies - list of currency codes to update
+   * @param \DateTime $date - retrieve rates for this date.  If omitted, get latest rates.
+   * @return \Civi\ExchangeRates\UpdateResult
+   */
+  abstract function updateRates($currencies, $date = null);
 }
