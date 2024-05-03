fb019e97f27642ed3b4fccd0cdd8e86ba5bd5173
Elliott Eggleston <ejegg@ejegg.com>
2024-05-03 13:51:18 -0400

    Create ExchangeRate entity in CiviCRM
    Bug: T270666
    Change-Id: I9197d87f74a2b922fe9a4c509fe0afd60dcda1c1

---
+++ drupal/sites/default/civicrm/extensions/exchange_rates/Civi/Api4/ExchangeRate.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * ExchangeRate entity.
+ *
+ * Provided by the Exchange Rates extension.
+ *
+ * @package Civi\Api4
+ */
+class ExchangeRate extends Generic\DAOEntity {
+
+}
