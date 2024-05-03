a2f1a4636c1e497f9fbf166fff67809268d8a11c
Elliott Eggleston <ejegg@ejegg.com>
2024-05-03 16:16:32 -0400

    ExchangeRate::updateAll API4 action
    To replace the drush exchange rate update.
    
    Bug: T270666
    Change-Id: I30a4b193ba04b43caaf4388f92f2f5ba5458344a

--- drupal/sites/default/civicrm/extensions/exchange_rates/Civi/Api4/ExchangeRate.php
+++ drupal/sites/default/civicrm/extensions/exchange_rates/Civi/Api4/ExchangeRate.php
@@ -10,4 +10,8 @@ namespace Civi\Api4;
  */
 class ExchangeRate extends Generic\DAOEntity {
 
+  public static function updateAll($checkPermissions = TRUE): Action\ExchangeRate\UpdateAll {
+    return (new Action\ExchangeRate\UpdateAll(__CLASS__, __FUNCTION__))
+      ->setCheckPermissions($checkPermissions);
+  }
 }
