3d3b335e9c9de41f01fca1266b1c7280d73eafa7
eileen <emcnaughton@wikimedia.org>
2024-05-06 16:36:10 +1200

    Fix Omnimail DAO declaration
    We got away with this being wrong up til now
    but not in 5.73 - Coleman has been tinkering with DAOs so...
    
    Change-Id: I4ebd088e964e2a38209cbf2562e0b3e317cb6b74

--- drupal/sites/default/civicrm/extensions/org.wikimedia.omnimail/xml/schema/CRM/Omnimail/OmnimailJobProgress.entityType.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.omnimail/xml/schema/CRM/Omnimail/OmnimailJobProgress.entityType.php
@@ -1,11 +1,10 @@
 <?php
 // This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
 // http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
-return array (
-  0 => 
-  array (
+return [
+  [
     'name' => 'OmnimailJobProgress',
-    'class' => '_OmnimailJobProgress',
+    'class' => 'CRM_Omnimail_DAO_OmnimailJobProgress',
     'table' => 'civicrm_omnimail_job_progress',
-  ),
-);
+  ],
+];
