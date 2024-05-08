c1585aae3c5df7f5c7a195505d0888ade6562f55
eileen <emcnaughton@wikimedia.org>
2024-05-08 13:31:12 +1200

    Extract getting of the temp table name
    Bug: T344215
    
    Change-Id: Ide5c93208ad4bd8df5f251700de284ff2b4742a9

--- drupal/sites/default/civicrm/extensions/wmf-civicrm/Civi/Api4/Action/WMFDonor/SelectTrait.php
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/Civi/Api4/Action/WMFDonor/SelectTrait.php
@@ -16,6 +16,16 @@ use Civi\Api4\Query\Api4SelectQuery;
 
 trait SelectTrait {
 
+  /**
+   * The name of the temporary table crated to track selected contacts.
+   *
+   * It is important to use a temporary table rather than the contact table
+   * for joins to avoid deadlocks and to avoid a self-referencing query
+   * when the trigger updates the modified_date.
+   *
+   * @var string
+   */
+  protected $temporaryTableName;
 
   /**
    * This entity name is for the WHERE clause & joins, but not the select.
@@ -29,27 +39,38 @@ trait SelectTrait {
   /**
    * Create a temp table from our where criteria and return as an sql subquery.
    *
-   * Our where criteria relate to the contact entity so this we load that to create the where
-   * to build our temp table.
+   * Our where criteria relate to the contact entity so this we load that to
+   * create the where to build our temp table.
    *
    * @throws \CRM_Core_Exception
    */
   protected function getTemporaryTableSelectClause(): string {
-    //
-    /* @var \Civi\Api4\Generic\DAOGetAction $getAction */
-    $getAction = Request::create('Contact', 'get', [
-      'version' => 4,
-      'select' => ['id'],
-      'where' => $this->getWhere(),
-      'limit' => $this->getLimit(),
-      'offset' => $this->getOffset(),
-      'orderBy' => $this->getOrderBy(),
-      'checkPermissions' => $this->getCheckPermissions(),
-    ]);
-    $query = new Api4SelectQuery($getAction);
-    $sql = $query->getSql();
-    $tempTable = \CRM_Utils_SQL_TempTable::build()->createWithQuery($sql);
-    return 'contact_id IN (SELECT id FROM ' . $tempTable->getName() . ')';
+    return 'contact_id IN (SELECT id FROM ' . $this->getTemporaryTableName() . ')';
+  }
+
+  /**
+   * @return string
+   * @throws \CRM_Core_Exception
+   */
+  protected function getTemporaryTableName(): string {
+    if (!isset($this->temporaryTableName)) {
+      /* @var \Civi\Api4\Generic\DAOGetAction $getAction */
+      $getAction = Request::create('Contact', 'get', [
+        'version' => 4,
+        'select' => ['id'],
+        'where' => $this->getWhere(),
+        'limit' => $this->getLimit(),
+        'offset' => $this->getOffset(),
+        'orderBy' => $this->getOrderBy(),
+        'checkPermissions' => $this->getCheckPermissions(),
+      ]);
+      $query = new Api4SelectQuery($getAction);
+      $sql = $query->getSql();
+      $this->temporaryTableName = \CRM_Utils_SQL_TempTable::build()
+        ->createWithQuery($sql)
+        ->getName();
+    }
+    return $this->temporaryTableName;
   }
 
 }
