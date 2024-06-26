e6cdee7a8e8b34746268edc9c822b59fa92a39b2
Jack Gleeson <jgleeson@wikimedia.org>
2024-06-26 13:12:26 -0700

    Add Zendesk tab to contact summary screen
    To Test:
    - Flush cache `./scripts/civicrm-drush.sh -v cvapi system.flush`
    - Set Zendesk config
    -- ./scripts/civicrm-drush.sh -v cvapi setting.create zendesk_api_user=test_user
    -- ./scripts/civicrm-drush.sh -v cvapi setting.create zendesk_api_password=test_pw
    -- ./scripts/civicrm-drush.sh -v cvapi setting.create zendesk_url=test_url
    - Submit donation using email of contact on Zendesk (you can use me@jackgleeson.co.uk)
    - Import contribution and Check 'Zendesk Tickets' Tab on Contact Summary page
    
    Bug: T368689
    Change-Id: Ifdb21ff417e3ead267eddaff877ead178748ba19

---
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/CRM/Wmf/Page/Zendesk.php
@@ -0,0 +1,57 @@
+<?php
+
+use CRM_Wmf_ExtensionUtil as E;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+class CRM_Wmf_Page_Zendesk extends CRM_Core_Page {
+
+  public function run() {
+    $zendesk_api_user = Civi::settings()->get('zendesk_api_user');
+    $zendesk_api_password = Civi::settings()->get('zendesk_api_password');
+    $zendeskURL = Civi::settings()->get('zendesk_url');
+    $ticketURLPrefix = "{$zendeskURL}/agent/tickets/";
+    $this->assign('ticketURLPrefix', $ticketURLPrefix);
+
+    // retrieve contact email
+    $contact_id = CRM_Utils_Request::retrieve('cid', 'Integer');
+    list($displayName, $contactEmail) = CRM_Contact_BAO_Contact_Location::getEmailDetails($contact_id);
+
+    // set up API client
+    $zendeskApiClient = new Client();
+    $requestAuthHeaders = $this->getApiAuthHeaders($zendesk_api_user, $zendesk_api_password);
+
+    // fetch Zendesk open ticket data via API
+    $openTicketSearchParams = "submitter:{$contactEmail} status<solved";
+    $openTicketsRequest = new Request('GET', "{$zendeskURL}/api/v2/search.json?query=$openTicketSearchParams", $requestAuthHeaders);
+    $openTicketsResponse = $zendeskApiClient->sendAsync($openTicketsRequest)->wait();
+    $openTickets = json_decode($openTicketsResponse->getBody(), TRUE);
+    if ($openTickets['count'] > 0) {
+      $this->assign('openTickets', $openTickets['results']);
+    }
+
+    // fetch Zendesk closed ticket data via API
+    $closedTicketSearchParams = "submitter:{$contactEmail} status:solved";
+    $closedTicketsRequest = new Request('GET', "{$zendeskURL}/api/v2/search.json?query=$closedTicketSearchParams", $requestAuthHeaders);
+    $closedTicketsResponse = $zendeskApiClient->sendAsync($closedTicketsRequest)->wait();
+    $closedTickets = json_decode($closedTicketsResponse->getBody(), TRUE);
+    if ($closedTickets['count'] > 0) {
+      $this->assign('closedTickets', $closedTickets['results']);
+    }
+
+    parent::run();
+  }
+
+  /**
+   * @param string $zendesk_api_user
+   * @param string $zendesk_api_password
+   *
+   * @return array
+   */
+  protected function getApiAuthHeaders(string $zendesk_api_user, string $zendesk_api_password): array {
+    return [
+      'Authorization' => 'Basic ' . base64_encode("$zendesk_api_user:$zendesk_api_password"),
+    ];
+  }
+
+}
---
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/templates/CRM/Wmf/Page/Zendesk.tpl
@@ -0,0 +1,21 @@
+<h3 style="margin: 0px 0px 10px 0px;">Zendesk Tickets (Open)</h3>
+{if isset($openTickets) && count($openTickets) > 0}
+<ul>
+{foreach from=$openTickets item='ticket'}
+  <li><a href="{$ticketURLPrefix}{$ticket.id}" target="_blank">{$ticket.subject|nl2br}</a></li>
+{/foreach}
+</ul>
+{else}
+  <p>No open tickets</p>
+{/if}
+
+<h3 style="margin: 0px 0px 10px 0px;">Zendesk Tickets (Resolved)</h3>
+{if isset($closedTickets) && count($closedTickets) > 0}
+<ul>
+{foreach from=$closedTickets item='ticket'}
+  <li><a href="{$ticketURLPrefix}{$ticket.id}" target="_blank">{$ticket.subject|nl2br}</a></li>
+{/foreach}
+</ul>
+{else}
+  <p>No resolved tickets</p>
+{/if}
