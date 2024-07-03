73db4ec3d2bc8bb97d35a8e40b24d6eb5754f417
eileen <emcnaughton@wikimedia.org>
2024-07-03 13:34:04 +1200

    Move wmf_civicrm_get_direct_mail_field_option_id to only caller
    Caller in in a class we expect to delete
    
    Bug: T365415
    Change-Id: I57c56b2cd5e632525aaab03ecf2e11bf1e6b56e4

--- drupal/sites/all/modules/wmf_campaigns/tests/WmfCampaignTest.php
+++ drupal/sites/all/modules/wmf_campaigns/tests/WmfCampaignTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Civi\Api4\CustomField;
+
 /**
  * @group WmfCampaigns
  */
@@ -18,7 +20,7 @@ class WmfCampaignTest extends BaseWmfDrupalPhpUnitTestCase {
   public function setUp(): void {
     parent::setUp();
     civicrm_initialize();
-    unset (Civi::$statics['wmf_campaigns']['campaigns']);
+    unset(Civi::$statics['wmf_campaigns']['campaigns']);
     $this->campaign_custom_field_name = wmf_civicrm_get_custom_field_name('Appeal');
 
     $this->campaign_key = 'fooCamp' . mt_rand();
@@ -39,7 +41,7 @@ class WmfCampaignTest extends BaseWmfDrupalPhpUnitTestCase {
       ->execute();
 
     $result = civicrm_api3('OptionValue', 'create', [
-      'option_group_id' => wmf_civicrm_get_direct_mail_field_option_id(),
+      'option_group_id' => $this->wmf_civicrm_get_direct_mail_field_option_id(),
       'label' => $this->campaign_key,
       'value' => $this->campaign_key,
     ]);
@@ -48,13 +50,39 @@ class WmfCampaignTest extends BaseWmfDrupalPhpUnitTestCase {
 
   public function tearDown(): void {
     civicrm_api3('OptionValue', 'delete', [
-      'option_group_id' => wmf_civicrm_get_direct_mail_field_option_id(),
+      'option_group_id' => $this->wmf_civicrm_get_direct_mail_field_option_id(),
       'id' => $this->option_value_id,
     ]);
     db_delete('wmf_campaigns_campaign')
       ->condition('campaign_key', $this->campaign_key)
       ->execute();
     parent::tearDown();
+  }
+
+
+  /**
+   * Get the name value for the option group for direct mail.
+   *
+   * Note that this function should become obsolete - I wanted to use
+   * the code below in the calling function instead but it didn't work and
+   * I want to check with Coleman first.
+   *
+   * Contribution::getFields(FALSE)
+   * ->setLoadOptions(TRUE)
+   * ->addWhere('field_name', '=', 'Gift_Data:Campaign')
+   * ->execute();
+   *}
+   *
+   * @throws \CRM_Core_Exception
+   */
+  private function wmf_civicrm_get_direct_mail_field_option_id() {
+    if (!isset(\Civi::$statics['wmf-civicrm']['appeal_field_id'])) {
+      \Civi::$statics['wmf-civicrm']['appeal_field_id'] = CustomField::get(FALSE)
+        ->addWhere('name', '=', 'Appeal')
+        ->setSelect(['option_group_id'])
+        ->execute()->first()['option_group_id'];
+    }
+    return \Civi::$statics['wmf-civicrm']['appeal_field_id'];
   }
 
   function testMatchingDonation() {
