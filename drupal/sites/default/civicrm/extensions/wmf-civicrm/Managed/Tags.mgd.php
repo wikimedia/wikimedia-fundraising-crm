bf00c76e905750475d16934f85559af18a018a31
eileen <emcnaughton@wikimedia.org>
2024-10-15 11:33:44 +1300

    Add preference_tags, per prod
    We might yet decommission these tags in favour of custom fields due to
    feedback by Nora - but at this stage the tags exist & adding them
    to the Acoustic export is the best short-term fix as changing tack would
    take a bit longer to work through
    
    Bug: T366212
    Change-Id: Id0a12e0c947bf8c4c4fd218e5ac6d6cdb474197b

--- drupal/sites/default/civicrm/extensions/wmf-civicrm/Managed/Tags.mgd.php
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/Managed/Tags.mgd.php
@@ -2,6 +2,7 @@
 
 use Civi\Api4\OptionValue;
 use Civi\Api4\Tag;
+use CRM_Wmf_ExtensionUtil as E;
 
 // First ensure that the option values exist so that we can add tags
 // against contributions and recurring contributions.
@@ -92,6 +93,91 @@ return [
       'description' => 'Used for contributions where the original assigned invoice id was a duplicate with another already in the database',
       'is_reserved' => 1,
       'selectable' => 0,
+    ],
+  ],
+
+  [
+    'name' => 'Tag_Preference_Spring_Campaign_Only',
+    'entity' => 'Tag',
+    'cleanup' => 'unused',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'Preference_Spring_Campaign_Only',
+        'label' => E::ts('Preference: exclude-from-spring-campaigns'),
+        'description' => E::ts('Donor does not want to participate in the campaign '),
+        'used_for' => [
+          'civicrm_contact',
+        ],
+        'color' => '#1b4a50',
+      ],
+      'match' => [
+        'name',
+      ],
+    ],
+  ],
+  [
+    'name' => 'Tag_Preference_End_Of_The_Year_Campaign_Only',
+    'entity' => 'Tag',
+    'cleanup' => 'unused',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'Preference_End_Of_The_Year_Campaign_Only',
+        'label' => E::ts('Preference: exclude-from-6C-annual-campaigns '),
+        'description' => E::ts('Donor does not want to participate in the campaign '),
+        'used_for' => [
+          'civicrm_contact',
+        ],
+        'color' => '#153f66',
+      ],
+      'match' => [
+        'name',
+      ],
+    ],
+  ],
+  [
+    'name' => 'Tag_Preference_Exclude_from_Direct_Mail_Campaigns',
+    'entity' => 'Tag',
+    'cleanup' => 'unused',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'Preference_Exclude_from_Direct_Mail_Campaigns',
+        'label' => E::ts('Preference: exclude-from-direct-mail-campaigns'),
+        'description' => E::ts('Donor does not want to participate in Direct Mail Campaign'),
+        'used_for' => [
+          'civicrm_contact',
+        ],
+        'color' => '#367d9b',
+      ],
+      'match' => [
+        'name',
+      ],
+    ],
+  ],
+  [
+    'name' => 'Tag_Preference_Exclude_from_SMS_campaigns',
+    'entity' => 'Tag',
+    'cleanup' => 'unused',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'Preference_Exclude_from_SMS_campaigns',
+        'label' => E::ts('Preference: exclude-from-sms-campaigns'),
+        'description' => E::ts('Donor does not want to participate in the campaign'),
+        'used_for' => [
+          'civicrm_contact',
+        ],
+        'color' => '#227b81',
+      ],
+      'match' => [
+        'name',
+      ],
     ],
   ],
 ];
