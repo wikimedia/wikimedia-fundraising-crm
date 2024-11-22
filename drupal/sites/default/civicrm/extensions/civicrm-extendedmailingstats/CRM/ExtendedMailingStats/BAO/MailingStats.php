f41b8e140494e680cc15d0a012c6b0d55a31aa8d
eileen <emcnaughton@wikimedia.org>
2024-11-23 12:23:18 +1300

    Remove commented out boilerplate
    Bug: T361621
    Change-Id: Ibc586c76111745a76dd2340050150d9af08563b8

--- drupal/sites/default/civicrm/extensions/civicrm-extendedmailingstats/CRM/ExtendedMailingStats/BAO/MailingStats.php
+++ drupal/sites/default/civicrm/extensions/civicrm-extendedmailingstats/CRM/ExtendedMailingStats/BAO/MailingStats.php
@@ -2,24 +2,4 @@
 
 class CRM_ExtendedMailingStats_BAO_MailingStats extends CRM_ExtendedMailingStats_DAO_MailingStats {
 
-  /**
-   * Create a new MailingStats based on array-data
-   *
-   * @param array $params key-value pairs
-   * @return CRM_ExtendedMailingStats_DAO_MailingStats|NULL
-   *
-  public static function create($params) {
-    $className = 'CRM_ExtendedMailingStats_DAO_MailingStats';
-    $entityName = 'MailingStats';
-    $hook = empty($params['id']) ? 'create' : 'edit';
-
-    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
-    $instance = new $className();
-    $instance->copyValues($params);
-    $instance->save();
-    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
-
-    return $instance;
-  } */
-
 }
