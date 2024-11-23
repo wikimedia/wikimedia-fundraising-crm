2752084627f1e345a472cd4b2e6fcca4be860cfa
eileen <emcnaughton@wikimedia.org>
2024-11-23 13:51:58 +1300

    Reformat code
    Change-Id: I6fccfc6b332d192b30f1190264f2481e9843aaf0

--- drupal/sites/default/civicrm/extensions/org.wikimedia.omnimail/tests/phpunit/OmnimailingLoadTest.php
+++ drupal/sites/default/civicrm/extensions/org.wikimedia.omnimail/tests/phpunit/OmnimailingLoadTest.php
@@ -6,13 +6,15 @@ require_once __DIR__ . '/OmnimailBaseTestClass.php';
  * FIXME - Add test description.
  *
  * Tips:
- *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
- *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
- *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
- *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
- *    If this test needs to manipulate schema or truncate tables, then either:
- *       a. Do all that using setupHeadless() and Civi\Test.
- *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ *  - With HookInterface, you may implement CiviCRM hooks directly in the test
+ * class. Simply create corresponding functions (e.g. "hook_civicrm_post(...)"
+ * or similar).
+ *  - With TransactionalInterface, any data changes made by setUp() or
+ * test****() functions will rollback automatically -- as long as you don't
+ * manipulate schema or truncate tables. If this test needs to manipulate
+ * schema or truncate tables, then either: a. Do all that using setupHeadless()
+ * and Civi\Test. b. Disable TransactionalInterface, and handle all
+ * setup/teardown yourself.
  *
  * @group headless
  */
@@ -26,14 +28,14 @@ class OmnimailingLoadTest extends OmnimailBaseTestClass {
   public function testOmnimailingLoad(): void {
     $mailings = $this->loadMailings();
     $this->assertEquals(2, $mailings['count']);
-    $mailing = $this->callAPISuccess('Mailing', 'getsingle', array('hash' => 'sp7877'));
+    $mailing = $this->callAPISuccess('Mailing', 'getsingle', ['hash' => 'sp7877']);
     $this->assertEquals(1, $mailing['is_completed']);
 
     $this->loadMailings();
 
-    $mailingReloaded = $this->callAPISuccess('Mailing', 'getsingle', array('hash' => 'sp7877'));
+    $mailingReloaded = $this->callAPISuccess('Mailing', 'getsingle', ['hash' => 'sp7877']);
     $this->assertEquals($mailingReloaded['id'], $mailing['id']);
-    $mailingJobs = $this->callAPISuccess('MailingJob', 'get', array('mailing_id' => $mailing['id']));
+    $mailingJobs = $this->callAPISuccess('MailingJob', 'get', ['mailing_id' => $mailing['id']]);
     $this->assertEquals(0, $mailingJobs['count']);
   }
 
@@ -46,12 +48,12 @@ class OmnimailingLoadTest extends OmnimailBaseTestClass {
    */
   protected function loadMailings(): array {
     $responses = $this->getResponses();
-    $mailings = $this->callAPISuccess('Omnimailing', 'load', array(
+    $mailings = $this->callAPISuccess('Omnimailing', 'load', [
       'mail_provider' => 'Silverpop',
       'client' => $this->getMockRequest($responses),
       'username' => 'Donald',
       'password' => 'quack',
-    ));
+    ]);
     return $mailings;
   }
 
