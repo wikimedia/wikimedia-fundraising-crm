ffcba044fe831b61e13872bae37e6abf807a071e
eileen <emcnaughton@wikimedia.org>
2025-03-13 19:40:49 +1300

    Move last test from wmf_common
    Bug: T366635
    Change-Id: I6c6e1156a552f13aa8e978d56821137fa315b112

--- drupal/sites/default/civicrm/extensions/wmf-civicrm/tests/phpunit/Civi/WMFTransactionTest.php
+++ drupal/sites/default/civicrm/extensions/wmf-civicrm/tests/phpunit/Civi/WMFTransactionTest.php
@@ -1,25 +1,29 @@
 <?php
 
+namespace Civi;
+
+use Civi\Api4\Contact;
+use Civi\Test\EntityTrait;
 use Civi\WMFException\WMFException;
-use Civi\WMFException\NonUniqueTransaction;
-use Civi\WMFTransaction;
+use Civi\WMFHelper\Database;
+use PHPUnit\Framework\TestCase;
 
 /**
- * @group WmfCommon
  */
-class WmfTransactionTestCase extends BaseWmfDrupalPhpUnitTestCase {
+class WMFTransactionTest extends TestCase {
+  use WMFEnvironmentTrait;
+  use EntityTrait;
 
+  /**
+   * @throws \Civi\WMFException\WMFException
+   */
   public function testParseUniqueId() {
     $transaction = WMFTransaction::from_unique_id("RFD RECURRING GLOBALCOLLECT 1234 432");
     $this->assertEquals(
-      $transaction->gateway_txn_id, "1234",
+      "1234", $transaction->gateway_txn_id,
       "5-argument form gateway_txn_id is parsed correctly.");
-    $this->assertEquals(
-      TRUE, $transaction->is_refund,
-      "refund flag parsed");
-    $this->assertEquals(
-      TRUE, $transaction->is_recurring,
-      "recurring flag parsed");
+    $this->assertTrue($transaction->is_refund, "refund flag parsed");
+    $this->assertTrue($transaction->is_recurring, "recurring flag parsed");
     $this->assertEquals(
       "globalcollect", strtolower($transaction->gateway),
       "gateway is correctly parsed");
@@ -27,34 +31,32 @@ class WmfTransactionTestCase extends BaseWmfDrupalPhpUnitTestCase {
       "432", $transaction->timestamp,
       "timestamp is correctly parsed");
     $this->assertEquals(
-      $transaction->get_unique_id(), "RFD RECURRING GLOBALCOLLECT 1234",
+      "RFD RECURRING GLOBALCOLLECT 1234", $transaction->get_unique_id(),
       "5-argument form is renormalized to 4-form");
 
     $transaction = WMFTransaction::from_unique_id("RFD GLOBALCOLLECT 1234 432");
     $this->assertEquals(
-      $transaction->gateway_txn_id, "1234",
+      "1234", $transaction->gateway_txn_id,
       "4-argument form gateway_txn_id is parsed correctly.");
-    $this->assertEquals(
-      TRUE, $transaction->is_refund,
-      "refund flag parsed");
+    $this->assertTrue($transaction->is_refund, "refund flag parsed");
     $this->assertEquals(
       "432", $transaction->timestamp,
       "timestamp is correctly parsed");
     $this->assertEquals(
-      $transaction->get_unique_id(), "RFD GLOBALCOLLECT 1234",
+      "RFD GLOBALCOLLECT 1234", $transaction->get_unique_id(),
       "4-argument form is renormalized correctly");
 
     $transaction = WMFTransaction::from_unique_id("GLOBALCOLLECT 1234x 432");
     $this->assertEquals(
-      $transaction->gateway_txn_id, "1234x",
+      "1234x", $transaction->gateway_txn_id,
       "3-argument form gateway_txn_id is parsed correctly.");
     $this->assertEquals(
-      $transaction->get_unique_id(),"GLOBALCOLLECT 1234x",
+      "GLOBALCOLLECT 1234x", $transaction->get_unique_id(),
       "3-argument form is renormalized correctly");
 
     $transaction = WMFTransaction::from_unique_id("GLOBALCOLLECT 1234");
     $this->assertEquals(
-      $transaction->gateway_txn_id, "1234",
+      "1234", $transaction->gateway_txn_id,
       "2-argument form gateway_txn_id is parsed correctly.");
     $this->assertNull($transaction->timestamp,
       "timestamp is not unnecessarily invented");
@@ -110,54 +112,61 @@ class WmfTransactionTestCase extends BaseWmfDrupalPhpUnitTestCase {
    * rollback happens.
    *
    * (this is really just the 'control' for the following test.
+   *
+   * @throws \CRM_Core_Exception
    */
   public function testNoRollBack() {
-    CRM_Core_DAO::executeQuery("UPDATE civicrm_domain SET description = 'WMF'");
+    \CRM_Core_DAO::executeQuery("UPDATE civicrm_domain SET description = 'WMF'");
 
     $this->callbackFunction(1);
 
-    $this->assertEquals('Cool planet', CRM_Core_DAO::singleValueQuery('SELECT description FROM civicrm_domain LIMIT 1'));
-    $contact = $this->callAPISuccess('Contact', 'get', ['external_identifier' => 'oh so strange']);
-    $this->assertEquals(1, $contact['count']);
-
-    // Cleanup
-    $this->callAPISuccess('Contact', 'delete', ['id' => $contact['id']]);
-    CRM_Core_DAO::executeQuery("UPDATE civicrm_domain SET description = 'WMF'");
+    $this->assertEquals('Cool planet', \CRM_Core_DAO::singleValueQuery('SELECT description FROM civicrm_domain LIMIT 1'));
+    Contact::get(FALSE)
+      ->addWhere('external_identifier', '=', 'oh so strange')
+      ->execute()->single();
+    \CRM_Core_DAO::executeQuery("UPDATE civicrm_domain SET description = 'WMF'");
   }
 
   /**
    * Test that when an exception is thrown with our wrapper the whole lot rolls
    * back.
+   *
+   * @throws \Exception
    */
   public function testFullRollBack() {
-    CRM_Core_DAO::executeQuery("UPDATE civicrm_domain SET description = 'WMF'");
+    \CRM_Core_DAO::executeQuery("UPDATE civicrm_domain SET description = 'WMF'");
 
     try {
-      \Civi\WMFHelper\Database::transactionalCall([$this, 'callbackFunction'], []);
+      Database::transactionalCall([$this, 'callbackFunction'], []);
     }
-    catch (RuntimeException $e) {
+    catch (\RuntimeException $e) {
       // We were expecting this :-)
       unset($this->ids['Contact']);
     }
 
-    $this->assertEquals('WMF', CRM_Core_DAO::singleValueQuery('SELECT description FROM civicrm_domain LIMIT 1'));
-    $count = $this->callAPISuccess('Contact', 'getcount', ['external_identifier' => 'oh so strange']);
+    $this->assertEquals('WMF', \CRM_Core_DAO::singleValueQuery('SELECT description FROM civicrm_domain LIMIT 1'));
+    $count = Contact::get(FALSE)
+      ->addWhere('external_identifier', '=', 'oh so strange')
+      ->execute()->count();
     $this->assertEquals(0, $count);
   }
 
-  public function callbackFunction() {
-    CRM_Core_DAO::executeQuery("UPDATE civicrm_domain SET description = 'Cool planet'");
+  /**
+   * @throws \Civi\Core\Exception\DBQueryException
+   */
+  public function callbackFunction(): void {
+    \CRM_Core_DAO::executeQuery("UPDATE civicrm_domain SET description = 'Cool planet'");
     $contact = [
       'contact_type' => 'Individual',
       'first_name' => 'Dr',
-      'last_name' => 'Strange',
+      'last_name' => 'Mouse',
       'external_identifier' => 'oh so strange',
     ];
-    $this->createTestContact($contact);
+    $this->createIndividual($contact);
     try {
       civicrm_api3('Contact', 'create', $contact);
     }
-    catch (Exception $e) {
+    catch (\Exception $e) {
       // We have done nothing to roll back.
       return;
     }
