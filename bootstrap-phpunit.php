be9063b187d341f1b12971e2912e72d7a873d898
eileen <emcnaughton@wikimedia.org>
2025-05-25 23:40:30 -0700

    Adjust bootstrap to run with standalone
    Change-Id: I95d0ba4676484d003cc2ea9efffec4ac52272dd7

--- bootstrap-phpunit.php
+++ bootstrap-phpunit.php
@@ -13,30 +13,86 @@ require_once(__DIR__ . "/vendor/mrmarkfrench/silverpop-php-connector/test/tests/
 // Argh.  Crib from _drush_bootstrap_drupal_site_validate
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 
-chdir(__DIR__ . '/drupal');
-require_once("includes/bootstrap.inc");
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-
-// Drupal just usurped PHPUnit's error handler.  Kick it off the throne.
-restore_error_handler();
+if (file_exists(__DIR__ . "/drupal/sites/default/settings.php")) {
+  chdir(__DIR__ . '/drupal');
+  require_once("includes/bootstrap.inc");
+  drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+  // Drupal just usurped PHPUnit's error handler.  Kick it off the throne.
+  restore_error_handler();
+}
 
 // Load contrib libs so tests can inherit from them.
 require_once(__DIR__ . '/vendor/autoload.php');
 
-putenv('CIVICRM_SETTINGS=' . DRUPAL_ROOT . '/sites/default/civicrm.settings.php');
-require_once __DIR__ . '/ext/wmf-civicrm/tests/phpunit/bootstrap.php';
-civicrm_initialize();
-// This causes errors to be thrown rather than the user-oriented html being presented on a fatal error.
-// Note that the CRM_Core_TemporaryErrorScope reverts the scope on _deconstruct so
-// the scope lasts until the variable is unset (by the script finishing)
-// or some other process alters it (which it shouldn't). Even though the $errorScope variable
-// is unused it needs to be set or the _deconstruct will take place instantly.
-$errorScope = \CRM_Core_TemporaryErrorScope::useException();
-// Uncomment this if you would like to see all of the
-// watchdog messages when a test fails. Can be useful
-// to debug tests in CI where you can't see the syslog.
-/*
-if (!defined('PRINT_WATCHDOG_ON_TEST_FAIL')) {
-  define('PRINT_WATCHDOG_ON_TEST_FAIL', TRUE);
+if (file_exists(__DIR__ . '/private/civicrm.settings.php')) {
+  putenv('CIVICRM_SETTINGS=' . __DIR__ . '/private/civicrm.settings.php');
 }
-*/
+else {
+  putenv('CIVICRM_SETTINGS=' . DRUPAL_ROOT . '/sites/default/civicrm.settings.php');
+}
+ini_set('memory_limit', '2G');
+
+eval(cv('php:boot --level=classloader', 'phpcode'));
+$baseDirs = (array) glob(__DIR__ . '/ext/*/tests/phpunit');
+$baseDirs[] = __DIR__ . '/ext/wmf-civicrm';
+
+foreach ($baseDirs as $directory) {
+  $loader = new \Composer\Autoload\ClassLoader();
+  $loader->add('CRM_', $directory);
+  $loader->add('Civi\\', $directory);
+  $loader->add('api_', $directory);
+  $loader->add('api\\', $directory);
+  $loader->register();
+}
+
+require_once getenv('CIVICRM_SETTINGS');
+
+/**
+ * Call the "cv" command.
+ *
+ * @param string $cmd
+ *   The rest of the command to send.
+ * @param string $decode
+ *   Ex: 'json' or 'phpcode'.
+ * @return mixed
+ *   Response output (if the command executed normally).
+ *   For 'raw' or 'phpcode', this will be a string. For 'json', it could be any JSON value.
+ * @throws \RuntimeException
+ *   If the command terminates abnormally.
+ */
+function cv(string $cmd, string $decode = 'json') {
+  $cmd = 'cv ' . $cmd;
+  $descriptorSpec = [0 => ['pipe', 'r'], 1 => ['pipe', 'w'], 2 => STDERR];
+  $oldOutput = getenv('CV_OUTPUT');
+  putenv('CV_OUTPUT=json');
+
+  // Execute `cv` in the original folder. This is a work-around for
+  // phpunit/codeception, which seem to manipulate PWD.
+  $cmd = sprintf('cd %s; %s', escapeshellarg(getenv('PWD')), $cmd);
+
+  $process = proc_open($cmd, $descriptorSpec, $pipes, __DIR__);
+  putenv("CV_OUTPUT=$oldOutput");
+  fclose($pipes[0]);
+  $result = stream_get_contents($pipes[1]);
+  fclose($pipes[1]);
+  if (proc_close($process) !== 0) {
+    throw new RuntimeException("Command failed ($cmd):\n$result");
+  }
+  switch ($decode) {
+    case 'raw':
+      return $result;
+
+    case 'phpcode':
+      // If the last output is /*PHPCODE*/, then we managed to complete execution.
+      if (substr(trim($result), 0, 12) !== '/*BEGINPHP*/' || substr(trim($result), -10) !== '/*ENDPHP*/') {
+        throw new \RuntimeException("Command failed ($cmd):\n$result");
+      }
+      return $result;
+
+    case 'json':
+      return json_decode($result, 1);
+
+    default:
+      throw new RuntimeException("Bad decoder format ($decode)");
+  }
+}
