1ab1b9013d2573a328afcbab3e249876f7e220f9
eileen <emcnaughton@wikimedia.org>
2025-09-06 11:30:45 +1200

    Don't send contribution receipts when importing contributions
    Bug: T403676
    Change-Id: Ib9463df978bf3d003c146e87433fcf9225efa709

--- core/ext/civiimport/Civi/Import/ContributionParser.php
+++ core/ext/civiimport/Civi/Import/ContributionParser.php
@@ -387,6 +387,7 @@ class ContributionParser extends ImportParser {
               ->addWhere('id', '=', $contributionID)
               ->execute()->single();
             Payment::create()
+              ->setnotificationForCompleteOrder(FALSE)
               ->setValues(($params['Payment'] ?? []) + [
                 'contribution_id' => $contributionID,
                 'check_number' => $contribution['check_number'],
