1d201c65b989ab6f6e5e624744306ec072ce80ea
Elliott Eggleston <ejegg@ejegg.com>
2026-01-29 12:30:41 -0500

    Move recurUpgrade send to helper
    So it can be called from an action.
    
    Change-Id: I41a2948feb535ab32d7836e1de139ea409ff6411

--- ext/wmf-civicrm/Civi/WMFQueue/RecurringQueueConsumer.php
+++ ext/wmf-civicrm/Civi/WMFQueue/RecurringQueueConsumer.php
@@ -383,7 +383,7 @@ class RecurringQueueConsumer extends TransactionalQueueConsumer {
       throw new WMFException(WMFException::IMPORT_CONTRIB, 'Failed inserting subscriber signup for subscriber id: ' . print_r($msg['subscr_id'], TRUE) . ': ' . $e->getMessage());
     }
 
-    $this->sendSuccessThankYouMail($newContributionRecur, $ctRecord, $msg, $contactId, $contact);
+    RecurHelper::sendSuccessThankYouMail($newContributionRecur, $ctRecord, $msg, $contactId, $contact);
     Civi::log('wmf')->notice('recurring: Successfully inserted subscription signup for subscriber id: {subscriber_id}', ['subscriber_id' => $msg['subscr_id']]);
   }
 
@@ -438,83 +438,6 @@ class RecurringQueueConsumer extends TransactionalQueueConsumer {
       ->setValues($params)
       ->execute()
       ->first();
-  }
-
-  protected function sendSuccessThankYouMail($contributionRecur, $ctRecord, $msg, $contactId, $contact) {
-    // Send an email that the recurring donation has been created
-    // @todo - it' not clear that there is any benefit in loading the contact
-    // here as it will be loaded in the ThankYou.send if not present.
-    if (isset($msg['recurring_payment_token']) && isset($contributionRecur['id'])) {
-      // Get the contact information if not already there
-      if (empty($contact)) {
-        $contact = civicrm_api3('Contact', 'getsingle', ['id' => $contactId]);
-      }
-      else {
-        $contact = $contact['values'][$contactId];
-        $contact['email'] = $msg['email'];
-      }
-
-      // Set up the language for the email
-      $locale = $contact['preferred_language'];
-      if (!$locale) {
-        Civi::log('wmf')->info('monthly_convert: Donor language unknown.  Defaulting to English...');
-        $locale = 'en';
-      }
-      $locale = strtolower(substr($locale, 0, 2));
-
-      // Using the same params sent through in thank_you.module thank_you_for_contribution
-      $start_date = $contributionRecur['start_date'];
-
-      // Get the day of the month
-      $day_of_month = \DateTime::createFromFormat('YmdHis', $start_date, new \DateTimeZone('UTC'))
-        ->format('j');
-
-      // Format the day of month
-      // TODO: This should probably be in the TwigLocalization logic
-      $ordinal = new \NumberFormatter($locale, \NumberFormatter::ORDINAL);
-      $day_of_month = $ordinal->format($day_of_month);
-
-      $params = [
-        'amount' => $msg['original_gross'],
-        'contact_id' => $contactId,
-        'currency' => $msg['original_currency'],
-        'first_name' => $contact['first_name'],
-        'last_name' => $contact['last_name'],
-        // Locale is the mediawiki variant - either 'en' or 'en-US'.
-        // Where 'preferred_language' is known then locale should generally
-        // be ignored, except where required for constructing urls.
-        'locale' => $locale,
-        // Preferred language is as stored in the civicrm database - eg. 'en_US'.
-        'language' => $contact['preferred_language'],
-        'receive_date' => $start_date,
-        'day_of_month' => $day_of_month,
-        'recipient_address' => $contact['email'],
-        'recurring' => TRUE,
-        'transaction_id' => "CNTCT-{$contactId}",
-        // shown in the body of the text
-        'contribution_id' => $ctRecord['contribution_id'],
-      ];
-
-      $success = ThankYou::send(FALSE)
-        ->setDisplayName($contact['display_name'])
-        ->setLanguage($params['language'])
-        ->setTemplateName('monthly_convert')
-        ->setContributionID($ctRecord['contribution_id'])
-        ->setParameters($params)
-        ->setActivityType('Recurring convert email')
-        ->execute()->first()['is_success'];
-
-      $context = [
-        'contribution_recur_id' => $contributionRecur['id'],
-        'recipient_address' => $params['recipient_address'],
-      ];
-      if ($success) {
-        Civi::log('wmf')->info('monthly_convert: Monthly convert sent successfully for recurring contribution id: {contribution_recur_id} to {recipient_address}', $context);
-      }
-      else {
-        Civi::log('wmf')->error('monthly_convert: Monthly convert mail failed for recurring contribution id: {contribution_recur_id} to {recipient_address}', $context);
-      }
-    }
   }
 
   /**
