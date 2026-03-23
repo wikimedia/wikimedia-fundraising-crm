b1b1c12eacbce94739950d5b14b12573306e5859
Damilare Adedoyin <dadedoyin@wikimedia.org>
2026-03-23 18:38:12 +0000

    Create civiproxy role to match production
    BuildKit PR: https://github.com/civicrm/civicrm-buildkit/pull/1035/changes
    
    Bug: T420778
    Change-Id: Ib2c628ca6ca3a239ab4dc47e7da8f7f244a57193

--- build/wmf_roles.sh
+++ build/wmf_roles.sh
@@ -1,3 +1,5 @@
 cv api4 Role.create +v name=donor_services +v 'label=Donor Services' +v 'permissions=translate CiviCRM'
 cv api4 Role.create +v name=civicrm_admin +v 'label=CiviCRM admin' +v 'permissions=access export menu,translate CiviCRM,allow Move Contribution'
+cv api4 Role.create '{"values":{"permissions":["access CiviContribute","access AJAX API","view all contacts","access CiviCRM"],"name":"civiproxy_role","label":"CiviProxy"}}'
+cv api4 Role.create '{"values":{"permissions":["view debug output","view public CiviMail content"],"name":"authenticated user","label":"authenticated_user"}}'
 cv api4 RolePermission.update +v granted_staff=1 +w 'name IN ["access export menu","access pdf menu","access print menu","access mailing labels menu"]'
