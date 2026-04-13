74b5fb7affa26db15ca441fba2bde7d1a9950630
Elliott Eggleston <ejegg@ejegg.com>
2026-04-13 12:13:50 -0400

    BlankLocation datacheck: fix location_type_id=0
    Not just NULL
    
    Bug: T423142
    Change-Id: Idea0f0d003b5c56a6e668585bc6dc3b505afcc5b

--- ext/org.wikimedia.datachecks/CRM/Datachecks/BlankLocation.php
+++ ext/org.wikimedia.datachecks/CRM/Datachecks/BlankLocation.php
@@ -13,12 +13,13 @@ class CRM_Datachecks_BlankLocation extends  CRM_Datachecks_LocationBase {
   public function check() {
     $result = [];
     foreach ($this->entities as $entity) {
-      $count = CRM_Core_DAO::singleValueQuery("SELECT count(*) FROM civicrm_{$entity} WHERE location_type_id IS NULL");
+      $count = CRM_Core_DAO::singleValueQuery("SELECT count(*) FROM civicrm_{$entity}
+                WHERE location_type_id IS NULL OR location_type_id = 0");
       if ($count) {
         $examples = [];
         $exampleContactsDAO = CRM_Core_DAO::executeQuery(
-          "SELECT contact_id FROM civicrm_{$entity} WHERE location_type_id IS NULL ORDER BY id DESC LIMIT 5
-        ");
+          "SELECT contact_id FROM civicrm_{$entity}
+                  WHERE location_type_id IS NULL OR location_type_id = 0 ORDER BY id DESC LIMIT 5");
 
         while ($exampleContactsDAO->fetch()) {
           $examples[] = $exampleContactsDAO->contact_id;
@@ -47,7 +48,7 @@ class CRM_Datachecks_BlankLocation extends  CRM_Datachecks_LocationBase {
              t1.id <> t2.id
              AND t1.contact_id = t2.contact_id
              AND t2.location_type_id = $locationTypeID
-           WHERE t1.location_type_Id IS NULL AND t2.id IS NULL;
+           WHERE (t1.location_type_id IS NULL OR t1.location_type_id=0) AND t2.id IS NULL;
         ")->getName();
         CRM_Core_DAO::executeQuery("
          UPDATE civicrm_{$entity}
