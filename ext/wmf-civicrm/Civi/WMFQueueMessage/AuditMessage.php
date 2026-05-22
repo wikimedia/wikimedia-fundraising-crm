976fe76ab1d6eeb6dd9a255b74a316246323bd29
eileen <emcnaughton@wikimedia.org>
2026-05-22 13:36:05 +1200

    Extend repair date
    Bug: T427028
    Change-Id: Ia125b8a7b65ec9b0b29739a86ec9a999b85ddeb9

--- ext/wmf-civicrm/Civi/WMFQueueMessage/AuditMessage.php
+++ ext/wmf-civicrm/Civi/WMFQueueMessage/AuditMessage.php
@@ -839,7 +839,7 @@ class AuditMessage extends DonationMessage {
     ) {
       return FALSE;
     }
-    if ($this->getSettlementTimeStamp() > strtotime('2026-05-20')) {
+    if ($this->getSettlementTimeStamp() > strtotime('2026-05-27')) {
       return FALSE;
     }
     if ($this->getPaymentOrchestratorReconciliationReference() && (
