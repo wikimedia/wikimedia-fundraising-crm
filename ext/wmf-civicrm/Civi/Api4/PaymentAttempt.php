9cb24a0c29e3614c9c70d5edc58bc4cbfe96f69a
Elliott Eggleston <ejegg@ejegg.com>
2026-06-22 12:01:31 -0400

    Add API4 entities for PaymentAttempt + Label
    After cv flush they should be available in searchkit.
    
    Bug: T429607
    Change-Id: Id9ddc01d9217b4779a377fe3a4fedf0a478ce90b

---
+++ ext/wmf-civicrm/Civi/Api4/PaymentAttempt.php
@@ -0,0 +1,26 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * PaymentAttempt entity.
+ *
+ * Provided by the WMF CiviCRM extension.
+ * @searchable primary
+ * @searchFields order_id,currency,country,utm_medium,utm_campaign
+ * @package Civi\Api4
+ */
+class PaymentAttempt extends Generic\DAOEntity {
+    /**
+     * Get permissions.
+     *
+     * It may be that we don't need a permission check on this api at all at there is a check on the entity
+     * retrieved.
+     *
+     * @return array
+     */
+    public static function permissions(): array {
+        $permissions = parent::permissions();
+        $permissions['get'] = ['access CiviCRM'];
+        return $permissions;
+    }
+}
